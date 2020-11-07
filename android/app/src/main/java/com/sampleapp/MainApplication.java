07fb20f4985b8d856067d366c85e11d6b31b5c40
James Ebert <jamesebert.k@gmail.com>
2020-11-06 17:59:18 -0700

    feat: initial ama-rn sample app config
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    feat: finished project structure, removed dependencies
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    feat: library typescript usage
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    feat: initial builder pattern
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    feat: added library validation
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    feat: agent creation/open & wallet interface
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    chore: removed ama-rn refactor specific references
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- android/app/src/main/java/com/sampleapp/MainApplication.java
+++ android/app/src/main/java/com/sampleapp/MainApplication.java
@@ -4,7 +4,6 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
-import com.ariesmobileagentreactnative.AriesPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
@@ -44,6 +43,5 @@ public class MainApplication extends Application implements ReactApplication {
   public void onCreate() {
     super.onCreate();
     SoLoader.init(this, /* native exopackage */ false);
-    
   }
 }
