34f4fe0b80a0cefdaf8a2d0be164bede1fef3726
James Ebert <jamesebert.k@gmail.com>
2021-02-16 13:43:18 -0700

    chore: changed name to reflect project name
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- android/app/src/main/java/com/ariesbifold/MainActivity.java
+++ android/app/src/main/java/com/ariesbifold/MainActivity.java
@@ -1,4 +1,4 @@
-package com.sampleapp;
+package com.ariesbifold;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "SampleApp";
+    return "aries-bifold";
   }
 }
