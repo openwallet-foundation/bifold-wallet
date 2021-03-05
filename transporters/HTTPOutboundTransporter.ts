d3bb55b8de2bf3e26ca826084743146305832e80
seajensen <seajensen@gmail.com>
2021-03-05 11:33:19 -0700

    feat: added genesis, transporters, rn-indy-sdk, and aries framework javascript
    Signed-off-by: seajensen <seajensen@gmail.com>

---
+++ transporters/HTTPOutboundTransporter.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { OutboundTransporter } from 'aries-framework-javascript'
+
+class HttpOutboundTransporter implements OutboundTransporter {
+  public async sendMessage(outboundPackage: any, receiveReply: boolean): Promise<void> {
+    const { payload, endpoint } = outboundPackage
+
+    if (!endpoint) {
+      throw new Error(`Missing endpoint. I don't know how and where to send the message.`)
+    }
+
+    try {
+      if (receiveReply) {
+        const response = await fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/ssi-agent-wire',
+          },
+          body: JSON.stringify(payload),
+        })
+
+        const data = await response.text()
+        const wireMessage = JSON.parse(data)
+        return wireMessage
+      } else {
+        await fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/ssi-agent-wire',
+          },
+          body: JSON.stringify(payload),
+        })
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error('error sending message', e)
+      throw e
+    }
+  }
+}
+
+export { HttpOutboundTransporter }
