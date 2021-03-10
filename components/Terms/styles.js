0a10849562255e100f38a556439f82ef9b3d8d6e
James Ebert <jamesebert.k@gmail.com>
2021-03-09 21:43:06 -0700

    chore: ran prettier
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- components/Terms/styles.js
+++ components/Terms/styles.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   checkmark: {
     width: '140%',
     height: '140%',
-    bottom: '20%'
+    bottom: '20%',
   },
   checktext: {
     justifyContent: 'center',
