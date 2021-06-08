d7750a753a9b0b195b3af0ffe22741f4824448c7
Karim Stekelenburg <karim@animo.id>
2021-06-08 14:54:17 +0200

    Ensure ConnectionRecord is only added once
    When a new ConnectionRecord is created it will be added to the store by
    the EventListener. We previously also added it in de reducer of the
    ConnectionThunks.createConnection() thunk, resulting in duplicate
    ConnectionRecords in the store. This has now been removed, so only the EventListener
    is adding it to the store.
    
    Signed-off-by: Karim Stekelenburg <karim@animo.id>

--- aries-components/src/slices/connections/connectionsListener.ts
+++ aries-components/src/slices/connections/connectionsListener.ts
@@ -1,19 +1,19 @@
-import { EnhancedStore } from "@reduxjs/toolkit"
-import { ConnectionStateChangedEvent } from "aries-framework"
-import { Agent, ConnectionEventTypes } from "aries-framework"
+import { EnhancedStore } from '@reduxjs/toolkit'
+import { ConnectionStateChangedEvent } from 'aries-framework'
+import { Agent, ConnectionEventTypes } from 'aries-framework'
 import { connectionsSlice } from './connectionsSlice'
 
 const startConnectionListener = (agent: Agent, store: EnhancedStore) => {
-    const listener = ((event: ConnectionStateChangedEvent) => {
-        const record = event.payload.connectionRecord
-        store.dispatch(connectionsSlice.actions.updateOrAdd(record))
-    })
+  const listener = (event: ConnectionStateChangedEvent) => {
+    const record = event.payload.connectionRecord
+    store.dispatch(connectionsSlice.actions.updateOrAdd(record))
+  }
 
-    agent.events.on(ConnectionEventTypes.ConnectionStateChanged, listener)
+  agent.events.on(ConnectionEventTypes.ConnectionStateChanged, listener)
 
-    return () => {
-        agent.events.off(ConnectionEventTypes.ConnectionStateChanged, listener)
-    }
+  return () => {
+    agent.events.off(ConnectionEventTypes.ConnectionStateChanged, listener)
+  }
 }
 
 export { startConnectionListener }
