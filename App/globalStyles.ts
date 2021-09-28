cd9cec14546886c42d1fa08792be3b04eccad07a
NeilSMyers <mmyersneil@gmail.com>
2021-09-28 14:04:53 -0600

    added Intl polyfills
    Signed-off-by: NeilSMyers <mmyersneil@gmail.com>

--- App/globalStyles.ts
+++ App/globalStyles.ts
@@ -1,5 +1,5 @@
 export const mainColor = '#35823f'
 export const textColor = '#fff'
-export const backgroundColor = '#111411'
-export const shadow = '#1b1f1c'
+export const backgroundColor = '#000'
+export const shadow = '#1c1c1e'
 export const borderRadius = 5
--- index.js
+++ index.js
@@ -2,6 +2,23 @@
  * @format
  */
 import 'react-native-gesture-handler'
+
+import '@formatjs/intl-getcanonicallocales/polyfill'
+import '@formatjs/intl-locale/polyfill'
+import '@formatjs/intl-pluralrules/polyfill'
+import '@formatjs/intl-pluralrules/locale-data/en' // locale-data for en
+import '@formatjs/intl-displaynames/polyfill'
+import '@formatjs/intl-displaynames/locale-data/en' // locale-data for en
+import '@formatjs/intl-listformat/polyfill'
+import '@formatjs/intl-listformat/locale-data/en' // locale-data for en
+import '@formatjs/intl-numberformat/polyfill'
+import '@formatjs/intl-numberformat/locale-data/en' // locale-data for en
+import '@formatjs/intl-relativetimeformat/polyfill'
+import '@formatjs/intl-relativetimeformat/locale-data/en' // locale-data for en
+import '@formatjs/intl-datetimeformat/polyfill'
+import '@formatjs/intl-datetimeformat/locale-data/en' // locale-data for en
+import '@formatjs/intl-datetimeformat/add-all-tz' // Add ALL tz data
+
 import { AppRegistry } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
