674bd6efb5536aaa36b389fd5992e61aa169209a
James Ebert <JamesEbert.k@gmail.com>
2021-10-11 12:54:17 -0600

    Fix/Update install process (#81)
    * updated afj to new version and updated method names
    
    Signed-off-by: NeilSMyers <mmyersneil@gmail.com>
    
    * checkFirstLogin double checks the pin
    
    Signed-off-by: NeilSMyers <mmyersneil@gmail.com>
    
    * fix: node_modules install & multiple login fix
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    * chore: updated readme with install and setup docs, fixed re-login flow
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    * chore: deleted obsolete .md file
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    * chore: alter structure from feedback
    
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>
    
    Co-authored-by: NeilSMyers <mmyersneil@gmail.com>

--- App/navigators/AuthenticateStack.tsx
+++ App/navigators/AuthenticateStack.tsx
@@ -21,14 +21,17 @@ const AuthenticateStack: React.FC<Props> = ({ setAuthenticated }) => {
   const checkFirstLogin = async () => {
     try {
       const firstLaunch = await AsyncStorage.getItem('firstLaunch')
+      const pin = await Keychain.getGenericPassword({ service: 'passcode' })
       if (firstLaunch == null) {
         await AsyncStorage.setItem('firstLaunch', 'false')
         await Keychain.resetGenericPassword({ service: 'passcode' })
       } else {
-        setFirstLogin(false)
+        if (pin) {
+          setFirstLogin(false)
+        }
       }
     } catch (e) {
-      //error
+      //TODO: error
     }
   }
 
