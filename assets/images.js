850b625bfbd069121d6e5431f53921ce979173b1
NeilSMyers <mmyersneil@gmail.com>
2021-10-20 14:12:57 -0600

    updated to RN66, removed unused files
    Signed-off-by: NeilSMyers <mmyersneil@gmail.com>

--- assets/images.js
+++ assets/images.js
@@ -1,22 +1,5 @@
 const images = {
-  arrow: require('./img/arrow.png'),
-  arrowDown: require('./img/arrow-down.png'),
-  checkmark: require('./img/checkmark.png'),
-  infoBlue: require('./img/info-blue.png'),
-  infoGray: require('./img/info-gray.png'),
-  infoWhite: require('./img/info-white.png'),
-  logo: require('./img/logo.png'),
   logoLarge: require('./img/logo-large.png'),
-  logoText: require('./img/logo-text.png'),
-  navConnect: require('./img/nav-connect.png'),
-  navContacts: require('./img/nav-contacts.png'),
-  navCredentials: require('./img/nav-credentials.png'),
-  navHome: require('./img/nav-home.png'),
-  navSettings: require('./img/nav-settings.png'),
-  receive: require('./img/receive.png'),
-  send: require('./img/send.png'),
-  waiting: require('./img/waiting.png'),
-  whiteHexCheck: require('./img/credential-large.png'),
 }
 
 export default images
