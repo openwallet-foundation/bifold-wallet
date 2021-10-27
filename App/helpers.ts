799c14ce19892f42574c97fe10824d9fb499e61d
Jason C. Leach <jason.leach@fullboar.ca>
2021-10-27 14:46:34 -0700

    Fix lint in workflow yaml
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/helpers.ts
+++ App/helpers.ts
@@ -2,7 +2,7 @@ export function parseSchema(schemaId?: string): string {
   if (schemaId) {
     const schemaIdRegex = /(.*?):([0-9]):([a-zA-Z .\-_0-9]+):([a-z0-9._-]+)$/
     const schemaIdParts = schemaId.match(schemaIdRegex)
-
+    // test
     if (schemaIdParts!.length === 5) {
       const prettyName = `${schemaIdParts![3].replace(/_/g, ' ')} V${schemaIdParts![4]}`
       return prettyName
