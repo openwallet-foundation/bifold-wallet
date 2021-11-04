67d08fc0105504e19c646a0bfd4d5ea56b882e82
Akiff Manji <akiff.manji@gmail.com>
2021-11-04 11:05:58 -0700

    Adds instructions to scanner screen
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- App/components/inputs/QRScanner.tsx
+++ App/components/inputs/QRScanner.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View, StyleSheet, Text, Dimensions } from 'react-native'
 
 import { BarCodeReadEvent, RNCamera } from 'react-native-camera'
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler'
@@ -11,6 +11,8 @@ interface Props {
   handleCodeScan: (event: BarCodeReadEvent) => Promise<void>
 }
 
+const { width } = Dimensions.get('window')
+
 const styles = StyleSheet.create({
   camera: {
     backgroundColor: 'black',
@@ -20,14 +22,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   viewFinder: {
-    height: 250,
-    width: 250,
-    padding: 100,
-    marginBottom: 100 - 48,
+    height: width - 0.25 * width,
+    width: width - 0.25 * width,
+    marginVertical: 24,
     backgroundColor: 'rgba(255,255,255,0.3)',
     borderRadius: 24,
     borderWidth: 2,
     borderColor: mainColor,
+    alignSelf: 'center',
   },
   torchButton: {
     borderRadius: 24,
@@ -45,6 +47,19 @@ const styles = StyleSheet.create({
   torchButtonIcon: {
     marginLeft: 2,
     marginTop: 2,
+  },
+  event: {
+    left: 30 + 1,
+    width: 20,
+    height: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'white',
+  },
+  activeEvent: {
+    color: 'black',
+    backgroundColor: 'white',
+    overflow: 'hidden',
   },
 })
 
@@ -79,18 +94,68 @@ const QRScanner: React.FC<Props> = ({ handleCodeScan }) => {
           }}
         >
           <View>
-            <View style={styles.viewFinder} />
-            <TouchableWithoutFeedback
-              style={[styles.torchButton, torch && styles.torchButtonOn]}
-              onPress={() => setTorch(!torch)}
-            >
-              {torch ? (
-                <Icon name="flash-on" color="black" size={24} style={[styles.torchButtonIcon]} />
-              ) : (
-                <Icon name="flash-off" color="white" size={24} style={styles.torchButtonIcon} />
-              )}
-            </TouchableWithoutFeedback>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', maxWidth: '100%' }}>
+              <View style={[styles.event, styles.activeEvent]}></View>
+              <View
+                style={{
+                  borderLeftWidth: 2,
+                  borderLeftColor: 'white',
+                  marginLeft: 20,
+                  paddingLeft: 20,
+                  marginTop: 20,
+                  zIndex: -1,
+                }}
+              >
+                <Text style={{ color: 'white', paddingLeft: 10, paddingRight: 30, marginTop: -20, paddingBottom: 20 }}>
+                  Scan the QR code issued by an organization to add a credential
+                </Text>
+              </View>
+            </View>
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', maxWidth: '100%' }}>
+              <View style={styles.event}></View>
+              <View
+                style={{
+                  borderLeftWidth: 2,
+                  borderLeftColor: 'white',
+                  marginLeft: 20,
+                  paddingLeft: 20,
+                  marginTop: 20,
+                  zIndex: -1,
+                }}
+              >
+                <Text style={{ color: 'white', paddingLeft: 10, paddingRight: 30, marginTop: -20, paddingBottom: 20 }}>
+                  Accept the offered credential
+                </Text>
+              </View>
+            </View>
+
+            <View style={{ flexDirection: 'row', justifyContent: 'flex-start', maxWidth: '100%' }}>
+              <View style={styles.event}></View>
+              <View
+                style={{
+                  marginLeft: 20,
+                  paddingLeft: 20,
+                  marginTop: 20,
+                  zIndex: -1,
+                }}
+              >
+                <Text style={{ color: 'white', paddingLeft: 10, paddingRight: 30, marginTop: -20 }}>
+                  Done! Your new credential is added to your wallet
+                </Text>
+              </View>
+            </View>
           </View>
+          <View style={styles.viewFinder} />
+          <TouchableWithoutFeedback
+            style={[styles.torchButton, torch && styles.torchButtonOn]}
+            onPress={() => setTorch(!torch)}
+          >
+            {torch ? (
+              <Icon name="flash-on" color="black" size={24} style={[styles.torchButtonIcon]} />
+            ) : (
+              <Icon name="flash-off" color="white" size={24} style={styles.torchButtonIcon} />
+            )}
+          </TouchableWithoutFeedback>
         </RNCamera>
       )}
     </View>
