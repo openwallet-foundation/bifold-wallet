f593929b62f28edaf04d61eb7c1fcb0b1fe69772
Jason C. Leach <jason.leach@fullboar.ca>
2021-11-05 16:03:13 -0700

    Update navigation packages
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- index.js
+++ index.js
@@ -20,12 +20,25 @@ import '@formatjs/intl-datetimeformat/polyfill'
 import '@formatjs/intl-datetimeformat/locale-data/en' // locale-data for en
 import '@formatjs/intl-datetimeformat/add-all-tz' // Add ALL tz data
 
-import { NavigationContainer } from '@react-navigation/native'
+import { NavigationContainer, Theme as NavigationTheme } from '@react-navigation/native'
 import React, { useEffect } from 'react'
 import { AppRegistry, LogBox } from 'react-native'
 
 import App from './App'
+import myTheme from './App/theme'
 import { name as appName } from './app.json'
+
+const navigationTheme = {
+  dark: false,
+  colors: {
+    primary: myTheme.colors.mainColor,
+    background: myTheme.colors.backgroundColor,
+    card: myTheme.colors.mainColor,
+    text: myTheme.colors.white,
+    border: myTheme.colors.white,
+    notification: myTheme.colors.white,
+  },
+}
 
 const Base = () => {
   useEffect(() => {
@@ -33,7 +46,7 @@ const Base = () => {
   }, [])
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navigationTheme}>
       <App />
     </NavigationContainer>
   )
