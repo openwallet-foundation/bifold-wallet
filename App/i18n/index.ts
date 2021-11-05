0d732e39f7fd0e7386f96642b2750214f3cb39d5
Jason C. Leach <jason.leach@fullboar.ca>
2021-11-05 08:35:57 -0700

    Fix lint issues
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/i18n/index.ts
+++ App/i18n/index.ts
@@ -1,7 +1,9 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import * as RNLocalize from 'react-native-localize'
+
 import { defaultLanguage } from '../constants'
+
 import en from './en'
 
 export type Translation = typeof en
