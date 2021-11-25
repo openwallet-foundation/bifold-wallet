280c12ca978395d660576dc7498242480f8d2694
Jason C. Leach <jason.leach@fullboar.ca>
2021-11-25 11:59:28 -0800

    Work on onboarding as a component
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/screens/Onboarding.tsx
+++ App/screens/Onboarding.tsx
@@ -4,23 +4,41 @@ import { Animated, BackHandler, Dimensions, FlatList, StyleSheet, Text, Touchabl
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { SvgProps } from 'react-native-svg'
 
-// @ts-ignore
-import WelcomeOne from '../assets/img/welcome-1.svg'
-// @ts-ignore
-import WelcomeTwo from '../assets/img/welcome-2.svg'
 import { Pagination } from '../components/shared/Pagination'
 import { Colors } from '../globalStyles'
-import { setTutorialCompletionStatus } from '../utils/storage'
 
 const { width } = Dimensions.get('window')
 
-const styles = StyleSheet.create({
+interface IOnboardingStyleSheet {
+  container: Record<string, any>
+  controlsContainer: Record<string, any>
+  headerText: Record<string, any>
+  bodyText: Record<string, any>
+  primaryButton: Record<string, any>
+  primaryButtonText: Record<string, any>
+}
+
+interface IOnboardingCarouselData {
+  image: React.FC<SvgProps>
+  text: string
+}
+
+interface IOnboardingProps {
+  title: string
+  pages: Array<IOnboardingCarouselData>
+  onOnboardingDismissed: () => Promise<void>
+  style?: IOnboardingStyleSheet
+}
+
+const defaultStyle: IOnboardingStyleSheet = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
   },
-  headerSize: {
-    width: '123',
+  controlsContainer: {
+    alignItems: 'center',
+    marginTop: 16,
+    marginBottom: 32,
   },
   headerText: {
     fontWeight: 'bold',
@@ -34,13 +52,8 @@ const styles = StyleSheet.create({
     margin: 16,
     color: Colors.textColor,
   },
-  blarb: {
-    alignItems: 'center',
-    marginTop: 16,
-    marginBottom: 32,
-  },
   primaryButton: {
-    padding: 16, // was 16 32
+    padding: 16,
     borderRadius: 4,
     backgroundColor: Colors.mainColor,
   },
@@ -50,38 +63,33 @@ const styles = StyleSheet.create({
   },
 })
 
-const Onboarding: React.FC<any> = ({ navigation }) => {
+const Onboarding: React.FC<IOnboardingProps> = ({ title, pages, onOnboardingDismissed, style }) => {
   const [activeIndex, setActiveIndex] = useState(0)
   const flatList: Ref<FlatList> = useRef(null)
   const scrollX = useRef(new Animated.Value(0)).current
+  const myStyle = style ? style : defaultStyle
+
   const onViewableItemsChangedRef = useRef(({ viewableItems }: any) => {
     setActiveIndex(viewableItems[0].index)
   })
+
   const viewabilityConfigRef = useRef({
-    viewAreaCoveragePercentThreshold: 50,
+    viewAreaCoveragePercentThreshold: 60,
   })
 
   const onScroll = Animated.event([{ nativeEvent: { contentOffset: { x: scrollX } } }], {
     useNativeDriver: false,
   })
-  const data: { image: React.FC<SvgProps>; text: string }[] = [
-    {
-      image: WelcomeOne,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-    {
-      image: WelcomeTwo,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    },
-  ]
+
   const next = () => {
-    if (activeIndex + 1 < data.length) {
+    if (activeIndex + 1 < pages.length) {
       flatList?.current?.scrollToIndex({
         index: activeIndex + 1,
         animated: true,
       })
     }
   }
+
   const previous = () => {
     if (activeIndex !== 0) {
       flatList?.current?.scrollToIndex({
@@ -99,7 +107,7 @@ const Onboarding: React.FC<any> = ({ navigation }) => {
           height: 180,
           width: 180,
         })}
-        <Text style={styles.bodyText}>{item.text}</Text>
+        <Text style={myStyle.bodyText}>{item.text}</Text>
       </View>
     ),
     []
@@ -117,32 +125,29 @@ const Onboarding: React.FC<any> = ({ navigation }) => {
   )
 
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={[styles.headerText]}>BC Wallet</Text>
+    <SafeAreaView style={myStyle.container}>
+      <Text style={myStyle.headerText}>{title}</Text>
       <FlatList
         ref={flatList}
         horizontal
         pagingEnabled
         showsHorizontalScrollIndicator={false}
         style={[{ width }]}
-        data={data}
+        data={pages}
         renderItem={renderItem}
         viewabilityConfig={viewabilityConfigRef.current}
         onViewableItemsChanged={onViewableItemsChangedRef.current}
         onScroll={onScroll}
         scrollEventThrottle={16}
       />
-      <Pagination data={data} scrollX={scrollX} next={next} previous={previous} />
-      <View style={styles.blarb}>
+      <Pagination data={pages} scrollX={scrollX} next={next} previous={previous} />
+      <View style={myStyle.controlsContainer}>
         <TouchableHighlight
-          style={[styles.primaryButton]}
+          style={myStyle.primaryButton}
           underlayColor={Colors.activeMain}
-          onPress={async () => {
-            await setTutorialCompletionStatus(true)
-            navigation.navigate('Home')
-          }}
+          onPress={onOnboardingDismissed}
         >
-          <Text style={[styles.primaryButtonText, { fontWeight: 'bold' }]}>Get started</Text>
+          <Text style={myStyle.primaryButtonText}>Get Started!</Text>
         </TouchableHighlight>
       </View>
     </SafeAreaView>
