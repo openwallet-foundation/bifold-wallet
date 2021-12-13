f9c1f4f25cb7353527981077b78db282d66188ef
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-13 09:12:38 -0800

    Work on testing
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- __tests__/Onboarding.test.tsx
+++ __tests__/Onboarding.test.tsx
@@ -1,21 +1,50 @@
 import React from 'react'
-import { Text } from 'react-native'
-import { SvgProps } from 'react-native-svg'
+import { StyleSheet, Text } from 'react-native'
 import renderer from 'react-test-renderer'
 
-import Onboarding from '../App/screens/Onboarding'
+import { Colors } from '../App/Theme'
+import Onboarding, { IOnboardingStyleSheet } from '../App/screens/Onboarding'
 
-const title = 'My Wallet'
 const markTutorialFin = jest.fn()
-const pages: { image: React.FC<SvgProps> | null; text: string }[] = [
-  {
-    image: jest.fn(),
-    text: 'Lorem 1 ipsum dolor sit amet, consectetur adipiscing elit.',
+
+export const carousel: IOnboardingStyleSheet = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: Colors.backgroundColor,
   },
-  {
-    image: jest.fn(),
-    text: 'Lorem 2 ipsum dolor sit amet, consectetur adipiscing elit.',
+  carouselContainer: {
+    flexDirection: 'column',
+    backgroundColor: Colors.backgroundColor,
   },
+  pagerContainer: {
+    flexShrink: 2,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  pagerDot: {
+    borderWidth: 1,
+    borderStyle: 'solid',
+    borderColor: Colors.mainColor,
+  },
+  pagerPosition: {
+    position: 'relative',
+    top: 0,
+  },
+  pagerNavigationButton: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: Colors.mainColor,
+  },
+})
+
+const pages = [
+  <>
+    <Text testID={'bodyText'}>Hello</Text>
+  </>,
+  <>
+    <Text testID={'bodyText'}>World</Text>
+  </>,
 ]
 
 jest.mock('@react-navigation/native', () => {
@@ -28,23 +57,23 @@ describe('Onboarding', () => {
   it('Renders correctly', () => {
     const tree = renderer
       // @ts-ignore
-      .create(<Onboarding title={title} pages={pages} onOnboardingDismissed={markTutorialFin} />)
+      .create(<Onboarding pages={pages} onOnboardingDismissed={markTutorialFin} style={carousel} />)
       .toJSON()
 
     expect(tree).toMatchSnapshot()
   })
 
-  it('Dismisses correctly', () => {
-    // @ts-ignore
-    const tree = renderer.create(<Onboarding title={title} pages={pages} onOnboardingDismissed={markTutorialFin} />)
-    tree.root!.findByProps({ testID: 'dismissButton' }).props.onPress()
+  // it('Next works correctly', () => {
+  //   // @ts-ignore
+  //   const tree = renderer.create(<Onboarding pages={pages} onOnboardingDismissed={markTutorialFin} style={carousel} />)
+  //   const b = tree.root!.findByProps({ testID: 'nextButton' })
 
-    expect(markTutorialFin).toBeCalledTimes(1)
-  })
+  //   expect(markTutorialFin).toBeCalledTimes(1)
+  // })
 
   it('Pages exist', () => {
     // @ts-ignore
-    const tree = renderer.create(<Onboarding title={title} pages={pages} onOnboardingDismissed={markTutorialFin} />)
+    const tree = renderer.create(<Onboarding pages={pages} onOnboardingDismissed={markTutorialFin} style={carousel} />)
     const foundPages = tree.root!.findAllByType(Text).filter((e: any) => e.props.testID === 'bodyText')
 
     expect(foundPages.length).toBe(2)
--- __tests__/Splash.test.tsx
+++ __tests__/Splash.test.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-// import { Text } from 'react-native'
-// import { SvgProps } from 'react-native-svg'
 import renderer from 'react-test-renderer'
 
 import Splash from '../App/screens/Splash'
 
-// jest.mock('@react-navigation/native', () => {
-//   return {
-//     useFocusEffect: jest.fn(),
-//   }
-// })
+jest.mock('@react-navigation/native', () => {
+  return {
+    useFocusEffect: jest.fn(),
+  }
+})
+
+jest.mock('@react-navigation/core', () => {
+  return {
+    useNavigation: jest.fn(),
+  }
+})
 
 describe('Splash Screen', () => {
   it('Renders correctly', () => {
