b4331e6d9b8eef56d2652c6ab84a3b9bb7941adb
Akiff Manji <akiff.manji@gmail.com>
2021-12-15 08:53:41 -0800

    Adds close button to QR scanner
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

---
+++ App/components/misc/QRScannerClose.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { StyleSheet, TouchableOpacity, View } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons'
+
+interface Props {
+  onPress?: () => void
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    padding: 20,
+  },
+})
+
+const CloseButton: React.FC<Props> = ({ onPress }) => {
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <Icon name="close" size={24} color="#ffffff"></Icon>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const QRScannerClose: React.FC<Props> = ({ onPress }) => {
+  return <CloseButton onPress={onPress}></CloseButton>
+}
+
+export default QRScannerClose
