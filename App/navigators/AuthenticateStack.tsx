52a8f3121657fd79ed2ca0a86588c1047fe63a6e
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-15 11:38:33 -0800

    Cleanup auth stack
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/navigators/AuthenticateStack.tsx
+++ App/navigators/AuthenticateStack.tsx
@@ -1,16 +1,11 @@
-import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createStackNavigator } from '@react-navigation/stack'
-import React, { useEffect, useState } from 'react'
-// eslint-disable-next-line import/namespace
-import * as Keychain from 'react-native-keychain'
+import React from 'react'
 
-import PinCreate from '../screens/PinCreate'
 import PinEnter from '../screens/PinEnter'
 
 import defaultStackOptions from './defaultStackOptions'
 
 export type AuthenticateStackParams = {
-  'Create 6-Digit Pin': { setAuthenticated: (auth: boolean) => void } | undefined
   'Enter Pin': { setAuthenticated: (auth: boolean) => void }
 }
 
@@ -21,40 +16,9 @@ interface Props {
 }
 
 const AuthenticateStack: React.FC<Props> = ({ setAuthenticated }) => {
-  const [firstLogin, setFirstLogin] = useState(true)
-
-  const checkFirstLogin = async () => {
-    // try {
-    //   const firstLaunch = await AsyncStorage.getItem('firstLaunch')
-    //   const pin = await Keychain.getGenericPassword({ service: 'passcode' })
-    //   if (firstLaunch == null) {
-    //     await AsyncStorage.setItem('firstLaunch', 'false')
-    //     await Keychain.resetGenericPassword({ service: 'passcode' })
-    //   } else {
-    //     if (pin) {
-    //       setFirstLogin(false)
-    //     }
-    //   }
-    // } catch (e) {
-    //   //TODO: error
-    // }
-  }
-
-  useEffect(() => {
-    checkFirstLogin()
-  }, [])
-
   return (
     <Stack.Navigator screenOptions={{ ...defaultStackOptions, presentation: 'transparentModal', headerShown: false }}>
-      {firstLogin ? (
-        // <Stack.Group>
-        <Stack.Screen name="Create 6-Digit Pin" component={PinCreate} initialParams={{ setAuthenticated }} />
-      ) : (
-        // </Stack.Group>
-        // <Stack.Group>
-        <Stack.Screen name="Enter Pin" component={PinEnter} initialParams={{ setAuthenticated }} />
-        // </Stack.Group>
-      )}
+      <Stack.Screen name="Enter Pin" component={PinEnter} initialParams={{ setAuthenticated }} />
     </Stack.Navigator>
   )
 }
