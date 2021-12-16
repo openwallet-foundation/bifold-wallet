77e91db80f6eecdc345e0245dfd89f37e56d118f
Jason C. Leach <jason.leach@fullboar.ca>
2021-12-16 11:37:40 -0800

    Cleanup nav stack params
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/navigators/AuthenticateStack.tsx
+++ App/navigators/AuthenticateStack.tsx
@@ -9,13 +9,13 @@ export type AuthenticateStackParams = {
   'Enter Pin': { setAuthenticated: (auth: boolean) => void }
 }
 
-const Stack = createStackNavigator<AuthenticateStackParams>()
-
-interface Props {
+interface AuthenticateStackProps {
   setAuthenticated: (auth: boolean) => void
 }
 
-const AuthenticateStack: React.FC<Props> = ({ setAuthenticated }) => {
+const AuthenticateStack: React.FC<AuthenticateStackProps> = ({ setAuthenticated }) => {
+  const Stack = createStackNavigator<AuthenticateStackParams>()
+
   return (
     <Stack.Navigator screenOptions={{ ...defaultStackOptions, presentation: 'transparentModal', headerShown: false }}>
       <Stack.Screen name="Enter Pin" component={PinEnter} initialParams={{ setAuthenticated }} />
