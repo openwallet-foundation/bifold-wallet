9fc61aad556082fe9f9e1bfaa05d52f33509ee62
Akiff Manji <akiff.manji@gmail.com>
2022-01-24 08:52:12 -0800

    Fill test scenarios and updte UI accordingly
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- __tests__/screens/CredentialOffer.test.tsx
+++ __tests__/screens/CredentialOffer.test.tsx
@@ -1,6 +1,58 @@
-import { cleanup } from '@testing-library/react-native'
+import { CredentialRecord, CredentialState } from '@aries-framework/core'
+import { cleanup, fireEvent, render } from '@testing-library/react-native'
+import React from 'react'
+
+import CredentialOffer from '../../App/screens/CredentialOffer'
+
+import Button from 'components/buttons/Button'
+import NotificationModal from 'components/modals/NotificationModal'
+
+const mockT = jest.fn((key: string) => key)
+jest.mock('react-i18next', () => ({
+  useTranslation: () => ({ t: mockT }),
+}))
+
+const mockNavigate = jest.fn()
+const mockGoBack = jest.fn()
+jest.mock('@react-navigation/core', () => {
+  const module = jest.requireActual('@react-navigation/core')
+  return {
+    ...module,
+    useNavigation: () => ({
+      navigate: mockNavigate,
+      goBack: mockGoBack,
+    }),
+  }
+})
+
+const mockAgent = {
+  acceptOffer: jest.fn(),
+  declineOffer: jest.fn(),
+}
+const mockTestCredentialRecords: CredentialRecord[] = [
+  new CredentialRecord({
+    threadId: '1',
+    state: CredentialState.OfferReceived,
+  }),
+]
+jest.mock('@aries-framework/react-hooks', () => {
+  const module = jest.requireActual('@aries-framework/react-hooks')
+  return {
+    ...module,
+    useAgent: () => ({
+      agent: {
+        credentials: mockAgent,
+      },
+    }),
+    useCredentialById: () => mockTestCredentialRecords[0],
+  }
+})
 
 describe('displays a credential offer screen', () => {
+  beforeEach(() => {
+    mockTestCredentialRecords[0].state = CredentialState.OfferReceived
+  })
+
   afterEach(() => {
     cleanup()
   })
@@ -12,8 +64,25 @@ describe('displays a credential offer screen', () => {
      * When the holder accepts the credential offer
      * Then the holder will be taken to a loading screen that informs them that their credential is coming
      */
-    test('a loading screen is displayed when the user accepts the credential offer', () => {
-      // TODO:
+    test('a loading screen is displayed when the user accepts the credential offer', async () => {
+      const { findByText, UNSAFE_getByProps } = render(
+        <CredentialOffer
+          navigation={{ navigate: mockNavigate, goBack: mockGoBack } as any}
+          route={{ params: { credentialId: mockTestCredentialRecords[0].id } } as any}
+        />
+      )
+
+      const acceptButtonInstance = await findByText('Global.Accept')
+
+      fireEvent(acceptButtonInstance, 'press')
+
+      const notificationModalInstance = await UNSAFE_getByProps({
+        title: 'CredentialOffer.CredentialOnTheWay',
+      })
+
+      expect(notificationModalInstance).not.toBe(null)
+      expect(notificationModalInstance.type).toBe(NotificationModal)
+      expect(notificationModalInstance.props.visible).toBeTruthy()
     })
 
     /**
@@ -21,8 +90,27 @@ describe('displays a credential offer screen', () => {
      * When the credential arrives in the wallet
      * Then the screen will change from the loading screen to a success screen informing the holder that the credential has arrived
      */
-    test('a success screen is displayed when the credential arrives', () => {
-      // TODO:
+    test('a success screen is displayed when the credential arrives', async () => {
+      mockTestCredentialRecords[0].state = CredentialState.CredentialReceived
+
+      const { findByText, UNSAFE_getByProps } = render(
+        <CredentialOffer
+          navigation={{ navigate: mockNavigate, goBack: mockGoBack } as any}
+          route={{ params: { credentialId: mockTestCredentialRecords[0].id } } as any}
+        />
+      )
+
+      const acceptButtonInstance = await findByText('Global.Accept')
+
+      fireEvent(acceptButtonInstance, 'press')
+
+      const notificationModalInstance = await UNSAFE_getByProps({
+        title: 'CredentialOffer.CredentialAddedToYourWallet',
+      })
+
+      expect(notificationModalInstance).not.toBe(null)
+      expect(notificationModalInstance.type).toBe(NotificationModal)
+      expect(notificationModalInstance.props.visible).toBeTruthy()
     })
 
     /**
@@ -32,8 +120,31 @@ describe('displays a credential offer screen', () => {
      * When the user presses the continue button
      * Then the holder will be taken to the credential list with the offered credential at the top of the list
      */
-    test('pressing the continue button on the success screen takes the holder to the credential list screen', () => {
-      // TODO:
+    test('pressing the continue button on the success screen takes the holder to the credential list screen', async () => {
+      mockTestCredentialRecords[0].state = CredentialState.CredentialReceived
+
+      const { findByText, UNSAFE_getByProps } = render(
+        <CredentialOffer
+          navigation={{ navigate: mockNavigate, goBack: mockGoBack } as any}
+          route={{ params: { credentialId: mockTestCredentialRecords[0].id } } as any}
+        />
+      )
+
+      const acceptButtonInstance = await findByText('Global.Accept')
+
+      fireEvent(acceptButtonInstance, 'press')
+
+      const notificationModalInstance = await UNSAFE_getByProps({
+        title: 'CredentialOffer.CredentialAddedToYourWallet',
+      })
+      const notificationModalDoneButtonInstance = notificationModalInstance.findByProps({ title: 'Global.Done' })
+
+      expect(notificationModalDoneButtonInstance).not.toBe(null)
+      expect(notificationModalDoneButtonInstance.type).toBe(Button)
+
+      fireEvent(notificationModalDoneButtonInstance, 'press')
+
+      expect(mockNavigate).toBeCalledWith('Credentials')
     })
   })
 })
