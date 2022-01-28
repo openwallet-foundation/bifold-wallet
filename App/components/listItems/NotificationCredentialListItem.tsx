92a3f66076b73ae2b96124568832192c6c2bae76
Akiff Manji <akiff.manji@gmail.com>
2022-01-28 10:50:04 -0800

    Update credential offer and notification screen with updated helper functions
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- App/components/listItems/NotificationCredentialListItem.tsx
+++ App/components/listItems/NotificationCredentialListItem.tsx
@@ -1,15 +1,16 @@
 import type { CredentialRecord } from '@aries-framework/core'
 
-import { useConnectionById } from '@aries-framework/react-hooks'
 import { useNavigation } from '@react-navigation/core'
 import { StackNavigationProp } from '@react-navigation/stack'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { TouchableOpacity, StyleSheet, View } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import { Colors, borderRadius, TextTheme, CredentialOfferTheme } from '../../theme'
-import { parseSchema } from '../../utils/helpers'
+import { parsedSchema } from '../../utils/helpers'
 import Text from '../texts/Text'
+import Title from '../texts/Title'
 
 import { HomeStackParams } from 'types/navigators'
 
@@ -32,19 +33,22 @@ const styles = StyleSheet.create({
 
 const NotificationCredentialListItem: React.FC<Props> = ({ notification }) => {
   const navigation = useNavigation<StackNavigationProp<HomeStackParams>>()
+  const { t } = useTranslation()
 
-  const { metadata, connectionId, id } = notification
+  // TODO: Reincorporate according to UI wireframes
+  // const connection = connectionRecordFromId(notification.connectionId)
 
-  const connection = useConnectionById(connectionId)
+  const { name: schemaName, version: schemaVersion } = parsedSchema(notification)
 
   return (
     <TouchableOpacity
       style={styles.container}
-      onPress={() => navigation.navigate('Credential Offer', { credentialId: id })}
+      onPress={() => navigation.navigate('Credential Offer', { credentialId: notification.id })}
     >
       <View>
-        <Text style={TextTheme.headingFour}>{parseSchema(metadata?.schemaId)}</Text>
-        <Text style={TextTheme.normal}>{connection?.alias || connection?.invitation?.label}</Text>
+        <Title>{t('CredentialOffer.CredentialOffer')}</Title>
+        <Text style={TextTheme.normal}>{schemaName + (schemaVersion ? ` V${schemaVersion}` : '')}</Text>
+        {/* {!!connection && <Text style={TextTheme.normal}>{connection?.alias || connection?.invitation?.label}</Text>} */}
       </View>
       <Icon name="chevron-right" color={Colors.text} size={30} />
     </TouchableOpacity>
