764b19c752d539f85919c9daf961824eccdc8f79
Jason C. Leach <jason.leach@fullboar.ca>
2022-01-28 14:52:49 -0800

    Cleaup tests
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- __tests__/components/Button.test.tsx
+++ __tests__/components/Button.test.tsx
@@ -1,41 +1,35 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { create } from 'react-test-renderer'
 
 import Button, { ButtonType } from '../../App/components/buttons/Button'
 
 describe('Button Component', () => {
   it('Primary renders correctly', () => {
-    const tree = renderer
-      // @ts-ignore
-      .create(
-        <Button
-          title={'Hello Primary'}
-          accessibilityLabel={'primary'}
-          onPress={() => {
-            return
-          }}
-          buttonType={ButtonType.Primary}
-        />
-      )
-      .toJSON()
+    const tree = create(
+      <Button
+        title={'Hello Primary'}
+        accessibilityLabel={'primary'}
+        onPress={() => {
+          return
+        }}
+        buttonType={ButtonType.Primary}
+      />
+    ).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
 
   it('Secondary renders correctly', () => {
-    const tree = renderer
-      // @ts-ignore
-      .create(
-        <Button
-          title={'Hello Secondary'}
-          accessibilityLabel={'secondary'}
-          onPress={() => {
-            return
-          }}
-          buttonType={ButtonType.Secondary}
-        />
-      )
-      .toJSON()
+    const tree = create(
+      <Button
+        title={'Hello Secondary'}
+        accessibilityLabel={'secondary'}
+        onPress={() => {
+          return
+        }}
+        buttonType={ButtonType.Secondary}
+      />
+    ).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
