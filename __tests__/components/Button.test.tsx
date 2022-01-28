87b490e6c88e50ce8999393d5b0af24e0db79007
Jason C. Leach <jason.leach@fullboar.ca>
2022-01-28 13:39:43 -0800

    Update button tests
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ __tests__/components/Button.test.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import Button, { ButtonType } from '../../App/components/buttons/Button'
+
+// jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon')
+
+describe('Button Component', () => {
+  it('Primary renders correctly', () => {
+    const tree = renderer
+      // @ts-ignore
+      .create(
+        <Button
+          title={'Hello Primary'}
+          accessibilityLabel={'primary'}
+          onPress={() => {
+            return
+          }}
+          buttonType={ButtonType.Primary}
+        />
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Secondary renders correctly', () => {
+    const tree = renderer
+      // @ts-ignore
+      .create(
+        <Button
+          title={'Hello Secondary'}
+          accessibilityLabel={'secondary'}
+          onPress={() => {
+            return
+          }}
+          buttonType={ButtonType.Secondary}
+        />
+      )
+      .toJSON()
+
+    expect(tree).toMatchSnapshot()
+  })
+})
