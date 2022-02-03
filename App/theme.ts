0c36c0dc929f2805434d9f1c50d9f824c2585210
Akiff Manji <akiff.manji@gmail.com>
2022-02-03 08:31:48 -0800

    Remove redundant theme types
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- App/theme.ts
+++ App/theme.ts
@@ -165,22 +165,6 @@ interface ColorTheme extends BaseColor {
   text: string
 }
 
-interface FontAttributes {
-  fontSize: number
-  fontWeight: 'normal' | 'bold' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900'
-  color: string
-}
-
-interface TextTheme {
-  headingOne: FontAttributes
-  headingTwo: FontAttributes
-  headingThree: FontAttributes
-  headingFour: FontAttributes
-  normal: FontAttributes
-  label: FontAttributes
-  caption: FontAttributes
-}
-
 interface CredentialOfferTheme {
   background: string
 }
