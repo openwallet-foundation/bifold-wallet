1d4dbf4781cf837dc4777a37697a583112f78d76
Jason C. Leach <jason.leach@fullboar.ca>
2022-02-09 11:17:16 -0800

    Improve accessability
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/navigators/RootStack.tsx
+++ App/navigators/RootStack.tsx
@@ -1,6 +1,7 @@
 import { useNavigation } from '@react-navigation/core'
 import { createStackNavigator } from '@react-navigation/stack'
 import React, { useContext, useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Text, TouchableOpacity, View } from 'react-native'
 
 import Arrow from '../assets/img/large-arrow.svg'
@@ -21,90 +22,95 @@ import defaultStackOptions from './defaultStackOptions'
 
 import { GenericFn, StateFn } from 'types/fn'
 
-const authStack = (setAuthenticated: StateFn) => {
-  const Stack = createStackNavigator()
-
-  return (
-    <Stack.Navigator initialRouteName={Screens.Splash} screenOptions={{ ...defaultStackOptions, headerShown: false }}>
-      <Stack.Screen name={Screens.EnterPin}>
-        {(props) => <PinEnter {...props} setAuthenticated={setAuthenticated} />}
-      </Stack.Screen>
-    </Stack.Navigator>
-  )
-}
-
-const mainStack = () => {
-  const Stack = createStackNavigator()
-
-  return (
-    <Stack.Navigator initialRouteName={Screens.Splash} screenOptions={{ ...defaultStackOptions, headerShown: false }}>
-      <Stack.Screen name="Tabs">{() => <TabStack />}</Stack.Screen>
-      <Stack.Screen name="Connect" options={{ presentation: 'modal' }}>
-        {() => <ScanStack />}
-      </Stack.Screen>
-    </Stack.Navigator>
-  )
-}
-
-const onboardingStack = (onSkipTouched: GenericFn, setAuthenticated: StateFn) => {
-  const Stack = createStackNavigator()
-
-  return (
-    <Stack.Navigator initialRouteName={Screens.Splash} screenOptions={{ ...defaultStackOptions, headerShown: false }}>
-      <Stack.Screen name={Screens.Splash} component={Splash} />
-      <Stack.Screen
-        name={Screens.Onboarding}
-        options={() => ({
-          title: 'Onboarding',
-          headerTintColor: Colors.white,
-          headerShown: true,
-          gestureEnabled: false,
-          headerLeft: () => false,
-          headerRight: () => {
-            return (
-              // TODO:(jl) Create new type of button component
-              <TouchableOpacity onPress={onSkipTouched} style={{ marginRight: 14 }}>
-                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                  <Text style={{ color: Colors.white, fontWeight: 'bold', marginRight: 4 }}>Skip</Text>
-                  <Arrow height={15} width={15} fill={Colors.white} style={{ transform: [{ rotate: '180deg' }] }} />
-                </View>
-              </TouchableOpacity>
-            )
-          },
-        })}
-      >
-        {(props) => (
-          <Onboarding
-            {...props}
-            nextButtonText={'Next'}
-            previousButtonText={'Back'}
-            pages={pages(onSkipTouched)}
-            style={carousel}
-          />
-        )}
-      </Stack.Screen>
-      <Stack.Screen
-        name={Screens.Terms}
-        options={() => ({
-          title: 'Terms & Conditions',
-          headerTintColor: Colors.white,
-          headerShown: true,
-          headerLeft: () => false,
-          rightLeft: () => false,
-        })}
-        component={Terms}
-      />
-      <Stack.Screen name={Screens.CreatePin}>
-        {(props) => <PinCreate {...props} setAuthenticated={setAuthenticated} />}
-      </Stack.Screen>
-    </Stack.Navigator>
-  )
-}
-
 const RootStack: React.FC = () => {
   const [authenticated, setAuthenticated] = useState(false)
   const [state, dispatch] = useContext(Context)
   const nav = useNavigation()
+  const { t } = useTranslation()
+
+  const authStack = (setAuthenticated: StateFn) => {
+    const Stack = createStackNavigator()
+
+    return (
+      <Stack.Navigator initialRouteName={Screens.Splash} screenOptions={{ ...defaultStackOptions, headerShown: false }}>
+        <Stack.Screen name={Screens.EnterPin}>
+          {(props) => <PinEnter {...props} setAuthenticated={setAuthenticated} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    )
+  }
+
+  const mainStack = () => {
+    const Stack = createStackNavigator()
+
+    return (
+      <Stack.Navigator initialRouteName={Screens.Splash} screenOptions={{ ...defaultStackOptions, headerShown: false }}>
+        <Stack.Screen name="Tabs">{() => <TabStack />}</Stack.Screen>
+        <Stack.Screen name="Connect" options={{ presentation: 'modal' }}>
+          {() => <ScanStack />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    )
+  }
+
+  const onboardingStack = (onSkipTouched: GenericFn, setAuthenticated: StateFn) => {
+    const Stack = createStackNavigator()
+
+    return (
+      <Stack.Navigator initialRouteName={Screens.Splash} screenOptions={{ ...defaultStackOptions, headerShown: false }}>
+        <Stack.Screen name={Screens.Splash} component={Splash} />
+        <Stack.Screen
+          name={Screens.Onboarding}
+          options={() => ({
+            title: 'Onboarding',
+            headerTintColor: Colors.white,
+            headerShown: true,
+            gestureEnabled: false,
+            headerLeft: () => false,
+            headerRight: () => {
+              return (
+                // TODO:(jl) Create new type of button component
+                <TouchableOpacity
+                  accessibilityLabel={t('Global.Skip')}
+                  onPress={onSkipTouched}
+                  style={{ marginRight: 14 }}
+                >
+                  <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                    <Text style={{ color: Colors.white, fontWeight: 'bold', marginRight: 4 }}>Skip</Text>
+                    <Arrow height={15} width={15} fill={Colors.white} style={{ transform: [{ rotate: '180deg' }] }} />
+                  </View>
+                </TouchableOpacity>
+              )
+            },
+          })}
+        >
+          {(props) => (
+            <Onboarding
+              {...props}
+              nextButtonText={'Next'}
+              previousButtonText={'Back'}
+              pages={pages(onSkipTouched)}
+              style={carousel}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen
+          name={Screens.Terms}
+          options={() => ({
+            title: 'Terms & Conditions',
+            headerTintColor: Colors.white,
+            headerShown: true,
+            headerLeft: () => false,
+            rightLeft: () => false,
+          })}
+          component={Terms}
+        />
+        <Stack.Screen name={Screens.CreatePin}>
+          {(props) => <PinCreate {...props} setAuthenticated={setAuthenticated} />}
+        </Stack.Screen>
+      </Stack.Navigator>
+    )
+  }
 
   const onSkipTouched = () => {
     dispatch({
