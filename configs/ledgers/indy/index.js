e8487d9202a74c8f8dd759a0ea93a187fecc23a0
James Ebert <jamesebert.k@gmail.com>
2022-02-09 09:00:34 -0700

    chore: add sovrin buildernet
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- configs/ledgers/indy/index.js
+++ configs/ledgers/indy/index.js
@@ -1,6 +1,7 @@
 import BCovrinTest from './bcovrin-test/pool-config'
 import CandyDev from './candy-dev/pool-config'
 import IndicioTestNet from './indicio-test-net/pool-config'
+import SovrinBuilderNet from './sovrin-builder-net/pool-config'
 import SovrinStagingNet from './sovrin-staging-net/pool-config'
 
-export default [IndicioTestNet, SovrinStagingNet, CandyDev, BCovrinTest]
+export default [SovrinStagingNet, IndicioTestNet, CandyDev, BCovrinTest, SovrinBuilderNet]
