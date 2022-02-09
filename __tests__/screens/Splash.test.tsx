0ec9f48be16327a25239429767579b88cc7c9680
Jason C. Leach <jason.leach@fullboar.ca>
2022-02-09 10:38:58 -0800

    Work on mock cleanup
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- __tests__/screens/Splash.test.tsx
+++ __tests__/screens/Splash.test.tsx
@@ -1,26 +1,11 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
+import { create } from 'react-test-renderer'
 
 import Splash from '../../App/screens/Splash'
 
-jest.mock('@react-navigation/native', () => {
-  return {
-    useFocusEffect: jest.fn(),
-  }
-})
-
-jest.mock('@react-navigation/core', () => {
-  return {
-    useNavigation: jest.fn(),
-  }
-})
-
 describe('Splash Screen', () => {
   it('Renders correctly', () => {
-    const tree = renderer
-      // @ts-ignore
-      .create(<Splash />)
-      .toJSON()
+    const tree = create(<Splash />).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
