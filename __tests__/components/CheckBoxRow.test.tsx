ce0c241caa2484cff7cd640e7411ca54e2ddaf33
Jason C. Leach <jason.leach@fullboar.ca>
2022-02-11 12:39:36 -0800

    Update tests
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- __tests__/components/CheckBoxRow.test.tsx
+++ __tests__/components/CheckBoxRow.test.tsx
@@ -3,13 +3,11 @@ import { create } from 'react-test-renderer'
 
 import CheckBoxRow from '../../App/components/inputs/CheckBoxRow'
 
-jest.mock('react-native-vector-icons/MaterialIcons', () => 'Icon')
-
 describe('CheckBoxRow Component', () => {
   it('Renders correctly', () => {
     const tree = create(
       // eslint-disable-next-line
-      <CheckBoxRow title={'Hello Fried'} accessibilityLabel={'Hey'} checked={true} onPress={() => {}} />
+      <CheckBoxRow title={'Hello Friend'} accessibilityLabel={'Hey'} checked={true} onPress={() => {}} />
     ).toJSON()
 
     expect(tree).toMatchSnapshot()
