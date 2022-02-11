b8c75efe14d8def849730595c2380dd8a6bd9e98
Akiff Manji <akiff.manji@gmail.com>
2022-02-10 16:57:36 -0800

    Cleans up mocks in existing tests
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- __tests__/screens/Home.test.tsx
+++ __tests__/screens/Home.test.tsx
@@ -1,7 +1,6 @@
 import { CredentialRecord, CredentialState, ProofRecord } from '@aries-framework/core'
 import { useCredentialByState, useProofByState } from '@aries-framework/react-hooks'
 import { useNavigation } from '@react-navigation/core'
-import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
 import { FlatList, TouchableOpacity } from 'react-native'
 import { create } from 'react-test-renderer'
@@ -12,14 +11,14 @@ import { InfoTextBox, NotificationListItem } from 'components'
 
 describe('displays a home screen', () => {
   it('renders correctly', () => {
-    const tree = create(<Home />).toJSON()
+    const tree = create(<Home navigation={useNavigation()} />).toJSON()
 
     expect(tree).toMatchSnapshot()
   })
 
   // TODO:(jl) Good enough to be captured by the snapshot?
   it('defaults to no notifications', () => {
-    const tree = create(<Home />)
+    const tree = create(<Home navigation={useNavigation()} />)
     const root = tree.root
     const results = root.findByType(InfoTextBox)
 
@@ -51,7 +50,7 @@ describe('with a notifications module, when an issuer sends a credential offer',
    * Then the credential offer will arrive in the form of a notification in the home screen
    */
   it('notifications are displayed', () => {
-    const tree = create(<Home />)
+    const tree = create(<Home navigation={useNavigation()} />)
     const root = tree.root
     const flatListInstance = root.findByType(FlatList)
 
@@ -65,7 +64,7 @@ describe('with a notifications module, when an issuer sends a credential offer',
    * When the holder is taken to the credential offer screen/flow
    */
   it('touch notification triggers navigation correctly', async () => {
-    const tree = create(<Home />)
+    const tree = create(<Home navigation={useNavigation()} />)
     const root = tree.root
     const touchable = root.findByType(NotificationListItem).findByType(TouchableOpacity)
     const navigation = useNavigation()
