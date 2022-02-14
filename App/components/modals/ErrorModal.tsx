937b1bd13a8c67421113deced69db8a2f0c7ffa4
Jason C. Leach <jason.leach@fullboar.ca>
2022-02-14 14:08:00 -0800

    Continue work on error modal
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/components/modals/ErrorModal.tsx
+++ App/components/modals/ErrorModal.tsx
@@ -1,38 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Dimensions, Modal, StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Modal, StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { Context } from '../../store/Store'
-import { ColorPallet, Colors, TextTheme } from '../../theme'
-import Button, { ButtonType } from '../buttons/Button'
+import { ColorPallet } from '../../theme'
+
+import BifoldErrorX from './BifoldErrorX'
 
 const { height } = Dimensions.get('window')
-
+const genericErrorCode = 1024
 const styles = StyleSheet.create({
   container: {
     minHeight: height,
     flexDirection: 'column',
-    paddingHorizontal: 25,
-    backgroundColor: 'red',
-  },
-  image: {
-    marginTop: 40,
-  },
-  messageContainer: {
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 54,
-  },
-  messageText: {
-    fontWeight: 'normal',
-    textAlign: 'center',
-  },
-  delayMessageContainer: {
-    marginTop: 30,
-  },
-  delayMessageText: {
-    textAlign: 'center',
-    marginBottom: 30,
+    backgroundColor: ColorPallet.brand.primary,
   },
 })
 
@@ -40,30 +24,29 @@ const ErrorModal: React.FC = () => {
   const { t } = useTranslation()
   const [modalVisible, setModalVisible] = useState<boolean>(false)
   const [state] = useContext(Context)
-
-  // const onDismissModalTouched = () => {
-  //   setModalVisible(false)
-  // }
+  const onDismissModalTouched = () => {
+    setModalVisible(false)
+  }
 
   useEffect(() => {
-    if (state.error) {
-      // console.log(state.error.title)
-      // console.log(state.error.message)
-      // console.log(state.error.code)
-
+    if (state.error && state.error.title && state.error.message && state.error.code) {
       setModalVisible(true)
+
       return
     }
 
-    setModalVisible(false)
+    setModalVisible(true)
   }, [state])
 
   return (
     <Modal visible={modalVisible} transparent={true}>
       <SafeAreaView style={[styles.container]}>
-        <View style={[styles.messageContainer]}>
-          <Text style={[TextTheme.headingThree, styles.messageText]}>{t('Connection.JustAMoment')}</Text>
-        </View>
+        <BifoldErrorX
+          title={state.error ? state.error.title : t('Error.Unknown')}
+          message={state.error ? state.error.message : t('Error.Problem')}
+          code={state.error ? state.error.code : genericErrorCode}
+          onPress={onDismissModalTouched}
+        />
       </SafeAreaView>
     </Modal>
   )
