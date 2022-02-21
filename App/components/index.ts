38f13855ce59c4248e900cb120ad461db29f455b
Akiff Manji <akiff.manji@gmail.com>
2022-02-21 14:30:21 -0800

    Reorganize component directories
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- App/components/index.ts
+++ App/components/index.ts
@@ -2,11 +2,11 @@
 
 import Button from './buttons/Button'
 import CheckBoxRow from './inputs/CheckBoxRow'
-import QRScanner from './inputs/QRScanner'
 import TextInput from './inputs/TextInput'
 import ContactListItem from './listItems/ContactListItem'
 import CredentialListItem from './listItems/CredentialListItem'
 import NotificationListItem from './listItems/NotificationListItem'
+import QRScanner from './misc/QRScanner'
 import HighlightTextBox from './texts/HighlightTextBox'
 import InfoTextBox from './texts/InfoTextBox'
 import Label from './texts/Label'
