45a2ae02e1b0ec0676c715b1d44ec6ae81db29c7
Jason C. Leach <jason.leach@fullboar.ca>
2022-03-08 16:50:57 -0800

    Cleanup style
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- App/components/buttons/HeaderRight.tsx
+++ App/components/buttons/HeaderRight.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
 import Arrow from '../../assets/icons/large-arrow.svg'
-import { Colors } from '../../theme'
+import { ColorPallet, Colors, TextTheme } from '../../theme'
 
 interface HeaderButtonProps {
   title: string
@@ -11,17 +11,39 @@ interface HeaderButtonProps {
   disabled?: boolean
 }
 
+const style = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  touchableArea: {
+    marginRight: 14,
+  },
+  title: {
+    ...TextTheme.label,
+    marginRight: 4,
+  },
+  icon: {
+    transform: [{ rotate: '180deg' }],
+  },
+})
+
 const HeaderRight: React.FC<HeaderButtonProps> = ({ title, accessibilityLabel, onPress, disabled = false }) => {
   return (
     <TouchableOpacity
       accessibilityLabel={accessibilityLabel}
       onPress={onPress}
-      style={{ marginRight: 14 }}
+      style={[style.touchableArea]}
       disabled={disabled}
     >
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Text style={{ color: Colors.white, fontWeight: 'bold', marginRight: 4 }}>{title}</Text>
-        <Arrow height={15} width={15} fill={Colors.white} style={{ transform: [{ rotate: '180deg' }] }} />
+      <View style={style.container}>
+        <Text style={[style.title]}>{title}</Text>
+        <Arrow
+          height={TextTheme.label.fontSize}
+          width={TextTheme.label.fontSize}
+          fill={TextTheme.label.color}
+          style={[style.icon]}
+        />
       </View>
     </TouchableOpacity>
   )
