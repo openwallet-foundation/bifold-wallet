9869575aea85357955c054db40656f25d46c4153
Akiff Manji <akiff.manji@gmail.com>
2022-03-16 10:06:58 -0700

    Adds back title to credential offer and proof request headers
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- App/navigators/NotificationStack.tsx
+++ App/navigators/NotificationStack.tsx
@@ -1,5 +1,6 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 
 import CredentialOffer from '../screens/CredentialOffer'
 import ProofRequest from '../screens/ProofRequest'
@@ -10,11 +11,20 @@ import defaultStackOptions from './defaultStackOptions'
 
 const NotificationStack: React.FC = () => {
   const Stack = createStackNavigator<NotificationStackParams>()
+  const { t } = useTranslation()
 
   return (
     <Stack.Navigator screenOptions={{ ...defaultStackOptions }}>
-      <Stack.Screen name={Screens.CredentialOffer} component={CredentialOffer} />
-      <Stack.Screen name={Screens.ProofRequest} component={ProofRequest} />
+      <Stack.Screen
+        name={Screens.CredentialOffer}
+        component={CredentialOffer}
+        options={{ headerBackTitle: t('Global.Back') }}
+      />
+      <Stack.Screen
+        name={Screens.ProofRequest}
+        component={ProofRequest}
+        options={{ headerBackTitle: t('Global.Back') }}
+      />
       <Stack.Screen name={Screens.ProofRequestAttributeDetails} component={ProofRequestAttributeDetails} />
     </Stack.Navigator>
   )
