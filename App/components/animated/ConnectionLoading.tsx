ea582427288dd4b76d1f32287952ebb92368a46d
Jason C. Leach <jason.leach@fullboar.ca>
2022-04-12 14:43:25 -0700

    Animate connection graphic
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ App/components/animated/ConnectionLoading.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useRef } from 'react'
+import { View, StyleSheet, Animated } from 'react-native'
+
+import ActivityIndicator from '../../assets/img/activity-indicator-circle.svg'
+import Wallet from '../../assets/img/wallet.svg'
+import { useThemeContext } from '../../utils/themeContext'
+
+const ConnectionLoading: React.FC = () => {
+  const { ColorPallet } = useThemeContext()
+  const rotationAnim = useRef(new Animated.Value(0)).current
+  const timing: Animated.TimingAnimationConfig = {
+    toValue: 1,
+    duration: 2000,
+    useNativeDriver: true,
+  }
+  const rotation = rotationAnim.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '360deg'],
+  })
+  const style = StyleSheet.create({
+    animation: {
+      position: 'absolute',
+    },
+  })
+  const imageDisplayOptions = {
+    fill: ColorPallet.notification.infoText,
+    height: 250,
+    width: 250,
+  }
+
+  useEffect(() => {
+    Animated.loop(Animated.timing(rotationAnim, timing)).start()
+  }, [rotationAnim])
+
+  return (
+    <View>
+      <Wallet {...imageDisplayOptions} />
+      <Animated.View style={[style.animation, { transform: [{ rotate: rotation }] }]}>
+        <ActivityIndicator {...imageDisplayOptions} />
+      </Animated.View>
+    </View>
+  )
+}
+
+export default ConnectionLoading
