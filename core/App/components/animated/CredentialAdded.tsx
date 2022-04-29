feeab9272a3d68956cb196cd07171a1cbfcdc77c
Jason C. Leach <jason.leach@fullboar.ca>
2022-04-29 11:17:12 -0700

    Shore up animations
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- core/App/components/animated/CredentialAdded.tsx
+++ core/App/components/animated/CredentialAdded.tsx
@@ -6,12 +6,13 @@ import CredentialCard from '../../assets/img/credential-card.svg'
 import WalletBack from '../../assets/img/wallet-back.svg'
 import WalletFront from '../../assets/img/wallet-front.svg'
 
-const ConnectionLoading: React.FC = () => {
-  const fadeAnim = useRef(new Animated.Value(0)).current
-  const tranAnim = useRef(new Animated.Value(-105)).current
+const CredentialAdded: React.FC = () => {
+  const cardFadeAnim = useRef(new Animated.Value(0)).current
+  const checkFadeAnim = useRef(new Animated.Value(0)).current
+  const tranAnim = useRef(new Animated.Value(-90)).current
   const slideTiming: Animated.TimingAnimationConfig = {
     toValue: -15,
-    duration: 2000,
+    duration: 1200,
     useNativeDriver: true,
   }
   const fadeTiming: Animated.TimingAnimationConfig = {
@@ -23,44 +24,51 @@ const ConnectionLoading: React.FC = () => {
     container: {
       flexDirection: 'column',
     },
-    wallet: {
+    back: {
       backgroundColor: 'transparent',
       position: 'absolute',
+      marginTop: -30,
+    },
+    front: {
+      backgroundColor: 'transparent',
     },
     card: {
       backgroundColor: 'transparent',
+      position: 'absolute',
+      marginLeft: 10,
     },
     check: {
       alignItems: 'center',
+      marginBottom: 10,
     },
   })
-  const imageDisplayOptions = {
-    height: 140,
-    width: 140,
-  }
 
   useEffect(() => {
     const animationDelay = 300
 
     setTimeout(() => {
-      Animated.sequence([Animated.timing(tranAnim, slideTiming), Animated.timing(fadeAnim, fadeTiming)]).start()
+      Animated.sequence([
+        Animated.timing(cardFadeAnim, fadeTiming),
+        Animated.timing(tranAnim, slideTiming),
+        Animated.timing(checkFadeAnim, fadeTiming),
+      ]).start()
     }, animationDelay)
   }, [])
 
   return (
     <View style={[style.container]}>
-      <Animated.View style={[{ opacity: fadeAnim }, style.check]}>
-        <CheckInCircle />
+      <Animated.View style={[{ opacity: checkFadeAnim }, style.check]}>
+        <CheckInCircle {...{ height: 45, width: 45 }} />
       </Animated.View>
       <View>
-        <WalletBack style={[style.wallet]} {...imageDisplayOptions} />
-        <Animated.View style={[{ transform: [{ translateY: tranAnim }] }]}>
-          <CredentialCard style={[style.card]} {...imageDisplayOptions} />
+        <WalletBack style={[style.back]} {...{ height: 110, width: 110 }} />
+        <Animated.View style={[{ opacity: cardFadeAnim, transform: [{ translateY: tranAnim }] }]}>
+          <CredentialCard style={[style.card]} {...{ height: 110, width: 110 }} />
         </Animated.View>
-        <WalletFront style={[style.wallet]} {...imageDisplayOptions} />
+        <WalletFront style={[style.front]} {...{ height: 140, width: 140 }} />
       </View>
     </View>
   )
 }
 
-export default ConnectionLoading
+export default CredentialAdded
