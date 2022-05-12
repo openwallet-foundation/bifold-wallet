a5f4c70e631a851d79be3efef72d8f0d08ef44cb
Jason C. Leach <jason.leach@fullboar.ca>
2022-05-12 14:42:15 -0700

    Work on status bar colour
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- core/App/screens/CredentialOfferDecline.tsx
+++ core/App/screens/CredentialOfferDecline.tsx
@@ -2,7 +2,7 @@ import { useCredentialById } from '@aries-framework/react-hooks'
 import { useNavigation } from '@react-navigation/core'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Modal, StyleSheet, Text, View } from 'react-native'
+import { Modal, StatusBar, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import CredentialDeclined from '../assets/img/credential-declined.svg'
@@ -73,6 +73,7 @@ const CredentialOfferDecline: React.FC<CredentialOfferDeclineProps> = ({
 
   return (
     <Modal visible={modalVisible} transparent={true} animationType={'none'}>
+      <StatusBar barStyle="light-content" hidden={false} translucent={false} />
       <SafeAreaView style={[styles.container]}>
         <View style={[{ marginTop: 25 }]}>
           {!didDeclineOffer && (
