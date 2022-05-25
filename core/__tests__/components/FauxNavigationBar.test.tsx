4b2a943a8dd1eccc95975853acf23c916b375ff6
Jason C. Leach <jason.leach@fullboar.ca>
2022-05-25 14:21:22 -0700

    Add nav bar to offer and proof modal
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ core/__tests__/components/FauxNavigationBar.test.tsx
@@ -0,0 +1,31 @@
+import { render, fireEvent } from '@testing-library/react-native'
+import React from 'react'
+
+import FauxNavigationBar from '../../App/components/views/FauxNavigationBar'
+import { testIdWithKey } from '../../App/utils/testable'
+
+describe('Faux Navigation Bar Component', () => {
+  it('Renders without home icon', () => {
+    const tree = render(<FauxNavigationBar title={'Hello'} />)
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Renders with home icon', () => {
+    const cb = jest.fn()
+    const tree = render(<FauxNavigationBar title={'Hello'} onHomeTouched={cb} />)
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('Home button triggers callback as expected', () => {
+    const cb = jest.fn()
+    const tree = render(<FauxNavigationBar title={'Hello'} onHomeTouched={cb} />)
+    const homeButton = tree.getByTestId(testIdWithKey('HomeButton'))
+
+    fireEvent(homeButton, 'press')
+
+    expect(homeButton).not.toBeNull()
+    expect(cb).toBeCalledTimes(1)
+  })
+})
