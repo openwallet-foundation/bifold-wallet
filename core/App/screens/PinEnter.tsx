c06b629740817823c8b15389fe247547efec9b36
Akiff Manji <akiff.manji@gmail.com>
2022-05-27 15:59:41 -0700

    Componentize pin input
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- core/App/screens/PinEnter.tsx
+++ core/App/screens/PinEnter.tsx
@@ -1,61 +1,27 @@
 import React, { useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Alert, Keyboard, StyleSheet, Text, View, Image } from 'react-native'
-import { CodeField, useClearByFocusCell } from 'react-native-confirmation-code-field'
-import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Alert, Keyboard, StyleSheet, Text, Image } from 'react-native'
 import * as Keychain from 'react-native-keychain'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Button, { ButtonType } from '../components/buttons/Button'
+import PinInput from '../components/inputs/PinInput'
 import { useTheme } from '../contexts/theme'
 import { testIdWithKey } from '../utils/testable'
-
-const CELL_COUNT = 6
 
 interface PinEnterProps {
   setAuthenticated: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 const PinEnter: React.FC<PinEnterProps> = ({ setAuthenticated }) => {
-  const [pin, setPin] = useState('')
-  const [showPin, setShowPin] = useState(false)
   const { t } = useTranslation()
+  const [pin, setPin] = useState('')
+
   const { ColorPallet, TextTheme, Assets } = useTheme()
-  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
-    value: pin,
-    setValue: setPin,
-  })
   const style = StyleSheet.create({
     container: {
       backgroundColor: ColorPallet.brand.primaryBackground,
       margin: 20,
-    },
-    codeField: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-    },
-    codeFieldRoot: {
-      marginVertical: 40,
-    },
-    cell: {
-      width: 40,
-      height: 48,
-      backgroundColor: ColorPallet.grayscale.darkGrey,
-      borderWidth: 2,
-      borderRadius: 5,
-      borderColor: ColorPallet.grayscale.darkGrey,
-      marginRight: 8,
-    },
-    focusedCell: {
-      borderColor: ColorPallet.grayscale.lightGrey,
-    },
-    cellText: {
-      ...TextTheme.headingThree,
-      lineHeight: 46,
-      color: ColorPallet.grayscale.white,
-      textAlign: 'center',
     },
   })
 
@@ -80,44 +46,8 @@ const PinEnter: React.FC<PinEnterProps> = ({ setAuthenticated }) => {
           marginBottom: 20,
         }}
       />
-
       <Text style={[TextTheme.normal, { alignSelf: 'center' }]}>Please enter your PIN</Text>
-      <View style={[style.codeField]}>
-        <CodeField
-          {...props}
-          value={pin}
-          onChangeText={(v: string) => {
-            setPin(v)
-            if (v.length === 6) {
-              Keyboard.dismiss()
-            }
-          }}
-          cellCount={CELL_COUNT}
-          keyboardType="numeric"
-          textContentType="password"
-          rootStyle={style.codeFieldRoot}
-          renderCell={({ index, symbol, isFocused }) => {
-            let child = ''
-            if (symbol) {
-              child = showPin ? symbol : '\u00B7'
-            }
-            return (
-              <View
-                key={index}
-                style={[style.cell, isFocused && style.focusedCell]}
-                onLayout={getCellOnLayoutHandler(index)}
-              >
-                <Text style={[style.cellText, !showPin && { fontSize: 72, lineHeight: 60 }]}>{child}</Text>
-              </View>
-            )
-          }}
-          autoFocus
-        />
-        <TouchableOpacity onPress={() => setShowPin(!showPin)} style={[{ marginRight: 8 }]}>
-          <Icon color="white" name={showPin ? 'visibility-off' : 'visibility'} size={30}></Icon>
-        </TouchableOpacity>
-      </View>
-
+      <PinInput onPinChanged={setPin} />
       <Button
         title={t('Global.Submit')}
         buttonType={ButtonType.Primary}
