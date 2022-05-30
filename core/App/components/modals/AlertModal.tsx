41fd346f4422ad03f8c74c82e0d11a8a5f226681
Akiff Manji <akiff.manji@gmail.com>
2022-05-30 13:26:31 -0700

    Adds modal alerts to create and enter pin screens
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

---
+++ core/App/components/modals/AlertModal.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { Dimensions, Modal, SafeAreaView, StyleSheet } from 'react-native'
+
+import InfoBox, { InfoBoxType } from '../../components/misc/InfoBox'
+
+const { height } = Dimensions.get('window')
+
+interface AlertModalProps {
+  title: string
+  message: string
+  submit?: () => void
+}
+
+const AlertModal: React.FC<AlertModalProps> = ({ title, message, submit }) => {
+  const { t } = useTranslation()
+
+  const styles = StyleSheet.create({
+    container: {
+      minHeight: height,
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  })
+
+  return (
+    <Modal visible={true} transparent={true}>
+      <SafeAreaView style={[styles.container]}>
+        <InfoBox
+          notificationType={InfoBoxType.Info}
+          title={title}
+          message={message}
+          onCallToActionLabel={t('Global.Okay')}
+          onCallToActionPressed={submit ? () => submit() : () => undefined}
+        />
+      </SafeAreaView>
+    </Modal>
+  )
+}
+
+export default AlertModal
