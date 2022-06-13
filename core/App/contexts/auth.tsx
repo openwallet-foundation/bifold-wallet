abd9e203b12ce46e1778f10844a507d7ffc30dd3
Mostafa <mostafa.youssef@ontario.ca>
2022-06-13 08:23:27 -0400

    Update auth provider to reflect new service layer changes
    Signed-off-by: Mostafa <mostafa.youssef@ontario.ca>

--- core/App/contexts/auth.tsx
+++ core/App/contexts/auth.tsx
@@ -1,11 +1,14 @@
 import React, { createContext, useContext, useState } from 'react'
 
-import { getWalletKey, setGenericPassword } from '../services/keychain.service'
+import { generateKeyForPin, getStoredWalletId, getWalletKey, setGenericPassword } from '../services/keychain.service'
 import { WalletSecret } from '../types/security'
 
 export interface AuthContext {
   getWalletSecret: () => Promise<WalletSecret | undefined>
+  getWalletID: () => Promise<string | undefined>
+  getKeyForPIN: (pin: string, providedSalt?: string) => Promise<any>
   setAppPIN: (pin: string) => Promise<void>
+  comparePIN: (newPin: string) => Promise<boolean>
 }
 
 export const AuthContext = createContext<AuthContext>(null as unknown as AuthContext)
@@ -16,14 +19,24 @@ export const AuthProvider: React.FC = ({ children }) => {
   const getWalletSecret = async (): Promise<WalletSecret | undefined> => {
     try {
       if (!walletSecret) {
-        const walletKey = await getWalletKey()
+        const walletKey = await getWalletKey('Confirm your biometrics to open the wallet', '')
         if (!walletKey) {
           throw new Error(`[79]Cannot get wallet key`)
         }
 
-        setWalletSecret(walletKey)
+        const walletID = await getStoredWalletId()
+        if (!walletID) {
+          throw new Error(`[119]:wallet id undefined`)
+        }
 
-        return walletKey
+        const secret: WalletSecret = {
+          walletId: walletID,
+          walletKey: walletKey,
+        }
+
+        setWalletSecret(secret)
+
+        return secret
       } else {
         return walletSecret
       }
@@ -42,11 +55,43 @@ export const AuthProvider: React.FC = ({ children }) => {
     }
   }
 
+  const getWalletID = async (): Promise<string | undefined> => {
+    try {
+      const walletID = await getStoredWalletId()
+      if (!walletID) {
+        return undefined
+      }
+
+      return walletID
+    } catch (e) {
+      throw new Error(`${e}`)
+    }
+  }
+
+  const getKeyForPIN = (pin: string, providedSalt?: string): Promise<any> => {
+    return generateKeyForPin(pin, providedSalt)
+  }
+
+  const comparePIN = async (newPin: string): Promise<boolean> => {
+    const keyforPin = await getKeyForPIN(newPin)
+    const walletSecret = await getWalletSecret()
+    const walletKey = walletSecret?.walletKey
+
+    if (keyforPin === walletKey) {
+      return true
+    } else {
+      return false
+    }
+  }
+
   return (
     <AuthContext.Provider
       value={{
         getWalletSecret,
+        getWalletID: getWalletID,
+        getKeyForPIN: getKeyForPIN,
         setAppPIN,
+        comparePIN: comparePIN,
       }}
     >
       {children}
