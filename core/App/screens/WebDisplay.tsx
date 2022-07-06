246a6c0569b68bb38e0f6fa5b95d64ec3219c7da
Jason C. Leach <jason.leach@fullboar.ca>
2022-07-06 15:00:08 -0700

    Display web content and override home content
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ core/App/screens/WebDisplay.tsx
@@ -0,0 +1,45 @@
+import { StackScreenProps } from '@react-navigation/stack'
+import React from 'react'
+import { StyleSheet, SafeAreaView } from 'react-native'
+import { WebView, WebViewNavigation } from 'react-native-webview'
+
+import { HomeStackParams, Screens } from '../types/navigators'
+
+type WebDisplayProps = StackScreenProps<HomeStackParams, Screens.WebDisplay>
+
+const WebDisplay: React.FC<WebDisplayProps> = ({ navigation, route }) => {
+  if (!route?.params) {
+    throw new Error('WebDisplay route prams were not set properly')
+  }
+
+  const { destUrl, exitUrl } = route?.params
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    container2: {
+      flex: 1,
+    },
+  })
+
+  return (
+    <SafeAreaView style={styles.container2}>
+      <WebView
+        style={styles.container}
+        source={{ uri: destUrl }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        onNavigationStateChange={(nav: WebViewNavigation) => {
+          if (exitUrl && nav.url.includes(exitUrl)) {
+            navigation.pop()
+          }
+        }}
+      />
+    </SafeAreaView>
+  )
+}
+
+export default WebDisplay
