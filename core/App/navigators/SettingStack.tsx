b77f0588145b99b8f263108872c31a0d69463d4a
wadeking98 <wkingnumber2@gmail.com>
2022-07-27 13:03:31 -0700

    removed text from back button
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- core/App/navigators/SettingStack.tsx
+++ core/App/navigators/SettingStack.tsx
@@ -1,6 +1,5 @@
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
-import { useTranslation } from 'react-i18next'
 
 import { useTheme } from '../contexts/theme'
 import Language from '../screens/Language'
@@ -11,12 +10,11 @@ import { createDefaultStackOptions } from './defaultStackOptions'
 
 const SettingStack: React.FC = () => {
   const Stack = createStackNavigator<SettingStackParams>()
-  const { t } = useTranslation()
   const theme = useTheme()
   const defaultStackOptions = createDefaultStackOptions(theme)
   return (
     <Stack.Navigator screenOptions={{ ...defaultStackOptions }}>
-      <Stack.Screen name={Screens.Settings} component={Settings} options={{ headerBackTitle: t('Global.Back') }} />
+      <Stack.Screen name={Screens.Settings} component={Settings} options={{ headerBackTitleVisible: false }} />
       <Stack.Screen name={Screens.Language} component={Language} />
     </Stack.Navigator>
   )
