c8eabf74de3a615de35c882d671c39aff7fb0f2b
ryankoch13 <ryan.koch13@gmail.com>
2022-09-26 13:06:04 -0400

    fix ui config
    Signed-off-by: ryankoch13 <ryan.koch13@gmail.com>

--- core/configs/uiConfig.tsx
+++ core/configs/uiConfig.tsx
@@ -10,11 +10,11 @@ export const uiConfig: uiConfigType = {
   // Determines if the scan tab will display a text label
   showTabLabel: true,
   // Determines if the tab bar will display 5 tabs or only 3
-  fiveTabDisplay: true,
+  fiveTabDisplay: false,
   // Determines if the app will navigate to the chat screen upon connection or wait for a notification
-  navigateOnConnection: true,
+  navigateOnConnection: false,
   // If true, allows the user to display their own invite QR code
-  allowQrDisplay: true,
+  allowQrDisplay: false,
   // Determines if the scan tab will have a highlight circle around it
-  focusScanTab: false,
+  focusScanTab: true,
 }
