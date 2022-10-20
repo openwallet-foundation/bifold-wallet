f101186b6c836dface8f0180d9152d7770b598cb
Akiff Manji <akiff.manji@gmail.com>
2022-10-20 15:21:49 -0700

    Update language screen with new UI/UX
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- core/App/screens/Language.tsx
+++ core/App/screens/Language.tsx
@@ -1,24 +1,49 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import BouncyCheckbox from 'react-native-bouncy-checkbox'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
-import SingleSelectBlock, { BlockSelection } from '../components/inputs/SingleSelectBlock'
-import SafeAreaScrollView from '../components/views/SafeAreaScrollView'
+import { useTheme } from '../contexts/theme'
 import { Locales, storeLanguage } from '../localization'
+interface Language {
+  id: Locales
+  value: string
+}
 
 const Language = () => {
   const { t, i18n } = useTranslation()
-
+  const { ColorPallet, TextTheme, SettingsTheme } = useTheme()
   // List of available languages into the localization directory
   const languages = [
-    { id: Locales.en, value: t('Language.English') },
-    { id: Locales.fr, value: t('Language.French') },
-    { id: Locales.ptBr, value: t('Language.Portuguese') },
+    { id: Locales.en, value: t('Language.English', { lng: Locales.en }) },
+    { id: Locales.fr, value: t('Language.French', { lng: Locales.fr }) },
+    { id: Locales.ptBr, value: t('Language.Portuguese', { lng: Locales.ptBr }) },
   ]
 
-  /**
-   * Find current set language
-   */
-  const storedLanguage = languages.find((l) => l.id === i18n.language)
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: ColorPallet.brand.primaryBackground,
+      width: '100%',
+    },
+    sectionContainer: {},
+    section: {
+      backgroundColor: SettingsTheme.groupBackground,
+      paddingHorizontal: 25,
+      paddingVertical: 16,
+    },
+    sectionSeparator: {
+      borderBottomWidth: 1,
+      borderBottomColor: ColorPallet.brand.primaryBackground,
+      marginHorizontal: 25,
+    },
+    sectionRow: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+  })
 
   /**
    * Once user select the particular language from the list,
@@ -26,15 +51,41 @@ const Language = () => {
    *
    * @param {BlockSelection} language
    */
-  const handleLanguageChange = async (language: BlockSelection) => {
+  const handleLanguageChange = async (language: Language) => {
     i18n.changeLanguage(language.id as Locales)
     await storeLanguage(language.id)
   }
 
   return (
-    <SafeAreaScrollView>
-      <SingleSelectBlock initialSelect={storedLanguage} selection={languages} onSelect={handleLanguageChange} />
-    </SafeAreaScrollView>
+    <SafeAreaView style={[styles.container]}>
+      <FlatList
+        data={languages}
+        renderItem={({ item: language }) => {
+          const { id, value }: Language = language
+          return (
+            <View style={[styles.section, styles.sectionRow]}>
+              <Text style={[TextTheme.title]}>{value}</Text>
+              <BouncyCheckbox
+                disableText
+                fillColor="#FFFFFFFF"
+                unfillColor="#FFFFFFFF"
+                size={36}
+                innerIconStyle={{ borderColor: ColorPallet.brand.primary, borderWidth: 2 }}
+                ImageComponent={() => <Icon name="circle" size={18} color={ColorPallet.brand.primary}></Icon>}
+                onPress={() => handleLanguageChange(language)}
+                isChecked={id === i18n.language}
+                disableBuiltInState
+              />
+            </View>
+          )
+        }}
+        ItemSeparatorComponent={() => (
+          <View style={{ backgroundColor: SettingsTheme.groupBackground }}>
+            <View style={[styles.sectionSeparator]}></View>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   )
 }
 
