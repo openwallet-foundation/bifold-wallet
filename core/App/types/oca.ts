f14ab2d8894b48e1e42ca861d990ab3ccdbe5f02
Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>
2022-11-08 10:28:37 -0500

    Small fix
    Signed-off-by: Jean-Christophe <Jean-Christophe.Drouin@mcn.gouv.qc.ca>

--- core/App/types/oca.ts
+++ core/App/types/oca.ts
@@ -162,7 +162,7 @@ export class DefaultOCABundleResolver implements OCABundleResolver {
   }
   public loadDefaultBundles(): OCABundleResolver {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
-    return this.loadBundles(require('../assets/oca-bundles-qc.json'))
+    return this.loadBundles(require('../assets/oca-bundles.json'))
   }
   public async getCredentialPresentationFields(
     credential: CredentialExchangeRecord,
