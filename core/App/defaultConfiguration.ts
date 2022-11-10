c79decef46b696152dace45600b0b2e409e8b36a
Jason C. Leach <jason.leach@fullboar.ca>
2022-11-09 16:09:35 -0800

    feat: developer screen override
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- core/App/defaultConfiguration.ts
+++ core/App/defaultConfiguration.ts
@@ -3,6 +3,7 @@ import defaultIndyLedgers from '../configs/ledgers/indy'
 import Record from './components/record/Record'
 import HomeContentView from './components/views/HomeContentView'
 import { ConfigurationContext } from './contexts/configuration'
+import Developer from './screens/Developer'
 import OnboardingPages from './screens/OnboardingPages'
 import Splash from './screens/Splash'
 import Terms from './screens/Terms'
@@ -13,6 +14,7 @@ export const defaultConfiguration: ConfigurationContext = {
   pages: OnboardingPages,
   splash: Splash,
   terms: Terms,
+  developer: Developer,
   homeContentView: HomeContentView,
   OCABundle: new oca.DefaultOCABundleResolver().loadDefaultBundles(),
   useBiometry: UseBiometry,
