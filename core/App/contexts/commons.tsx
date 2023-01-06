6c20fbc15857ea337ca9a29d52e72268ee901cd4
Mostafa <moscd3@gmail.com>
2023-01-05 22:40:14 -0500

    fix linting in 1 file
    Signed-off-by: Mostafa <moscd3@gmail.com>

--- core/App/contexts/commons.tsx
+++ core/App/contexts/commons.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState } from 'react'
+import React, { createContext, useContext } from 'react'
 
 import { AppConsoleLogger, LogLevel } from '../services/logger'
 
