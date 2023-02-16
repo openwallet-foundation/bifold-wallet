7c92f291196c8311b54e166269942ca46b88e7c7
wadeking98 <wkingnumber2@gmail.com>
2023-02-16 11:43:25 -0800

    proof branding update
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- core/App/components/misc/CredentialCard.tsx
+++ core/App/components/misc/CredentialCard.tsx
@@ -5,24 +5,58 @@ import { ViewStyle } from 'react-native'
 import { useConfiguration } from '../../contexts/configuration'
 import { GenericFn } from '../../types/fn'
 import { CardOverlayType } from '../../types/oca'
+import { Attribute, Predicate } from '../../types/record'
 
 import CredentialCard10 from './CredentialCard10'
 import CredentialCard11 from './CredentialCard11'
+import CredentialProofCard from './CredentialProofCard'
 
 interface CredentialCardProps {
-  credential: CredentialExchangeRecord
+  credential?: CredentialExchangeRecord
+  credDefId?: string
+  schemaId?: string
+  credName?: string
   onPress?: GenericFn
   style?: ViewStyle
+  proof?: boolean
+  proofAttributes?: Attribute[]
+  proofPredicates?: Predicate[]
+  existsInWallet?: boolean
 }
 
-const CredentialCard: React.FC<CredentialCardProps> = ({ credential, style = {}, onPress = undefined }) => {
+const CredentialCard: React.FC<CredentialCardProps> = ({
+  credential,
+  credDefId,
+  schemaId,
+  proof,
+  proofAttributes,
+  proofPredicates,
+  credName,
+  existsInWallet,
+  style = {},
+  onPress = undefined,
+}) => {
+  // add ability to reference credential by ID, allows us to get past react hook restrictions
   const { OCABundleResolver } = useConfiguration()
-
-  return OCABundleResolver.cardOverlayType === CardOverlayType.CardLayout10 ? (
-    <CredentialCard10 credential={credential} style={style} onPress={onPress} />
-  ) : (
-    <CredentialCard11 credential={credential} style={style} onPress={onPress} />
-  )
+  const getCredOverlayType = (type: CardOverlayType) => {
+    if (proof) {
+      return (
+        <CredentialProofCard
+          credDefId={credDefId}
+          schemaId={schemaId}
+          proofAttributes={proofAttributes ?? []}
+          proofPredicates={proofPredicates ?? []}
+          credName={credName ?? ''}
+          existsInWallet={existsInWallet}
+        />
+      )
+    } else if (type === CardOverlayType.CardLayout10) {
+      return <CredentialCard10 credential={credential as CredentialExchangeRecord} style={style} onPress={onPress} />
+    } else {
+      return <CredentialCard11 credential={credential as CredentialExchangeRecord} style={style} onPress={onPress} />
+    }
+  }
+  return getCredOverlayType(OCABundleResolver.cardOverlayType)
 }
 
 export default CredentialCard
