4a256d1c397618ac5d098598ada039a5e83476ae
Artemkaaas <artem.ivanov@dsr-corporation.com>
2023-03-02 17:47:10 +0300

    Added tests
    Signed-off-by: Artemkaaas <artem.ivanov@dsr-corporation.com>

--- core/App/utils/proof-request.ts
+++ core/App/utils/proof-request.ts
@@ -1,14 +1,30 @@
-import { ProofRequestTemplate, ProofRequestType } from '../types/proof-reqeust-template'
+import {
+  IndyRequestedAttribute,
+  IndyRequestedPredicate,
+  ProofRequestTemplate,
+  ProofRequestType,
+} from '../types/proof-reqeust-template'
 
 export const buildProofRequestFromTemplate = (template: ProofRequestTemplate) => {
   if (template.payload.type === ProofRequestType.Indy) {
+    const requestedAttributes: Array<IndyRequestedAttribute> = []
+    const requestedPredicates: Array<IndyRequestedPredicate> = []
+
+    template.payload.data.forEach((data) => {
+      if (data.requestedAttributes?.length) {
+        requestedAttributes.push(...data.requestedAttributes)
+      }
+      if (data.requestedPredicates?.length) {
+        requestedPredicates.push(...data.requestedPredicates)
+      }
+    })
     return {
       indy: {
         name: template.title,
         version: template.version,
         nonce: Date.now(),
-        requestedAttributes: template.payload.data.flatMap((item) => item.requestedAttributes),
-        requestedPredicates: template.payload.data.flatMap((item) => item.requestedPredicates),
+        requestedAttributes,
+        requestedPredicates,
       },
     }
   }
