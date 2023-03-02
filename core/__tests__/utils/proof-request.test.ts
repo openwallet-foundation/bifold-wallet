45fdcdf9ebf832ea1f6a2c6311229a5ed709920d
Artemkaaas <artem.ivanov@dsr-corporation.com>
2023-03-02 17:47:10 +0300

    Added tests
    Signed-off-by: Artemkaaas <artem.ivanov@dsr-corporation.com>

---
+++ core/__tests__/utils/proof-request.test.ts
@@ -0,0 +1,33 @@
+import { defaultProofRequestTemplates } from '../../App/constants'
+import { buildProofRequestFromTemplate } from '../../App/utils/proof-request'
+import SpyInstance = jest.SpyInstance
+
+describe('Helpers', () => {
+  let spy: SpyInstance
+
+  beforeAll(() => {
+    spy = jest.spyOn(Date, 'now').mockImplementation(() => 1677766511505)
+  })
+
+  test('Build indy proof request from template containing two requested attributes', async () => {
+    const template = defaultProofRequestTemplates[0]
+    const proofRequest = buildProofRequestFromTemplate(template)
+    expect(proofRequest).toMatchSnapshot()
+  })
+
+  test('Build indy proof request from template containing two requested attributes and predicate', async () => {
+    const template = defaultProofRequestTemplates[1]
+    const proofRequest = buildProofRequestFromTemplate(template)
+    expect(proofRequest).toMatchSnapshot()
+  })
+
+  test('Build indy proof request from template containing attributes group', async () => {
+    const template = defaultProofRequestTemplates[3]
+    const proofRequest = buildProofRequestFromTemplate(template)
+    expect(proofRequest).toMatchSnapshot()
+  })
+
+  afterAll(() => {
+    spy.mockRestore()
+  })
+})
