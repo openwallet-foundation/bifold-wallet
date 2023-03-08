eb54eb4bfa050598d2635ce9f38e4e9609cf3d4f
Artemkaaas <artem.ivanov@dsr-corporation.com>
2023-03-08 09:33:15 +0300

    Style corrections
    Signed-off-by: Artemkaaas <artem.ivanov@dsr-corporation.com>

--- core/App/screens/ListProofRequests.tsx
+++ core/App/screens/ListProofRequests.tsx
@@ -6,76 +6,69 @@ import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import { defaultProofRequestTemplates } from '../../verifier/constants'
 import { hasPredicates } from '../../verifier/utils/proof-request'
-import { ColorPallet } from '../theme'
+import { useTheme } from '../contexts/theme'
 import { ProofRequestsStackParams, Screens } from '../types/navigators'
 
 interface ListProofRequestsProps {
   navigation: StackNavigationProp<ProofRequestsStackParams>
 }
 
-const style = StyleSheet.create({
-  container: {
-    margin: 24,
-  },
-  proofButton: {
-    flexDirection: 'row',
-    borderRadius: 8,
-    padding: 12,
-    backgroundColor: '#fff',
-    marginBottom: 10,
-  },
-  textContainer: {
-    flex: 1,
-  },
-  proofTitle: {
-    fontWeight: 'bold',
-    marginBottom: 6,
-  },
-  proofCaption: {
-    color: ColorPallet.grayscale.mediumGrey,
-    fontSize: 11,
-  },
-  proofText: {
-    color: ColorPallet.grayscale.black,
-  },
-  proofDetails: {
-    marginBottom: 4,
-  },
-  iconContainer: {
-    alignSelf: 'center',
-  },
-  icon: {
-    color: ColorPallet.grayscale.black,
-  },
-})
-
-const iconSize = 35
-const iconName = 'chevron-right'
-
 const ListProofRequests: React.FC<ListProofRequestsProps> = ({ navigation }) => {
   const { t } = useTranslation()
+  const { ListItems } = useTheme()
+
+  const style = StyleSheet.create({
+    container: {
+      margin: 24,
+    },
+    recordButton: {
+      ...ListItems.requestTemplateBackground,
+      flexDirection: 'row',
+      borderRadius: 8,
+      padding: 12,
+      marginBottom: 10,
+    },
+    textContainer: {
+      flex: 1,
+    },
+    templateTitle: {
+      ...ListItems.requestTemplateTitle,
+      marginBottom: 6,
+    },
+    templateDetails: {
+      ...ListItems.requestTemplateDetails,
+      marginBottom: 4,
+    },
+    templateZkpLabel: {
+      ...ListItems.requestTemplateZkpLabel,
+      fontSize: 11,
+    },
+    iconContainer: {
+      alignSelf: 'center',
+    },
+    icon: {
+      ...ListItems.requestTemplateIcon,
+      fontSize: 36,
+    },
+  })
+
   const records = defaultProofRequestTemplates
 
   return (
     <View style={style.container}>
       {records.map((record) => (
-        <TouchableOpacity
-          style={style.proofButton}
-          onPress={() => navigation.navigate(Screens.ProofRequesting, { templateId: record.id })}
-        >
+        <TouchableOpacity style={style.recordButton} onPress={() => navigation.navigate(Screens.ProofRequestDetails)}>
           <View style={style.textContainer}>
-            <Text style={[style.proofText, style.proofTitle]} numberOfLines={1}>
+            <Text style={style.templateTitle} numberOfLines={1}>
               {record.title}
             </Text>
-            <Text style={[style.proofText, style.proofDetails]} numberOfLines={2}>
+            <Text style={style.templateDetails} numberOfLines={2}>
               {record.details}
             </Text>
-            {hasPredicates(record) && (
-              <Text style={[style.proofText, style.proofCaption]}>{t('Verifier.ZeroKnowledgeProof')}</Text>
-            )}
+            {hasPredicates(record) && <Text style={style.templateZkpLabel}>{t('Verifier.ZeroKnowledgeProof')}</Text>}
           </View>
           <View style={style.iconContainer}>
-            <Icon style={style.icon} size={iconSize} name={iconName} />
+            <Icon style={style.icon} name={'chevron-right'} />
           </View>
         </TouchableOpacity>
       ))}
