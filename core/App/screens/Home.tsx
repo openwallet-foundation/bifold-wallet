7ee53bf1607bdddbbc75c0b8370f7c5a369a795d
Nikita Zakharov <nikita.zakharov@dsr-corporation.com>
2023-03-09 18:50:07 +0300

    eat: add proof request tutorial modal (WIP)
    Signed-off-by: Nikita Zakharov <nikita.zakharov@dsr-corporation.com>

--- core/App/screens/Home.tsx
+++ core/App/screens/Home.tsx
@@ -9,6 +9,7 @@ import { ScrollView } from 'react-native-gesture-handler'
 import { isPresentationReceived } from '../../verifier/utils/proof'
 import NotificationListItem, { NotificationType } from '../components/listItems/NotificationListItem'
 import NoNewUpdates from '../components/misc/NoNewUpdates'
+import ProofRequestTutorialModal from '../components/modals/ProofRequestTutorialModal'
 import { useConfiguration } from '../contexts/configuration'
 import { DispatchAction } from '../contexts/reducers/store'
 import { useStore } from '../contexts/store'
@@ -121,6 +122,7 @@ const Home: React.FC<HomeProps> = ({ navigation }) => {
   return (
     <>
       <ScrollView>
+        <ProofRequestTutorialModal visible={true} />
         <View style={styles.rowContainer}>
           <Text style={[HomeTheme.notificationsHeader, styles.header]}>
             {t('Home.Notifications')}
