57f5519f79cd6c1be833b78138c3a8dc91178466
Artemkaaas <artem.ivanov@dsr-corporation.com>
2023-03-09 11:04:16 +0300

    Integrated With OCA bundle resolver
    Signed-off-by: Artemkaaas <artem.ivanov@dsr-corporation.com>

--- core/verifier/constants.ts
+++ core/verifier/constants.ts
@@ -4,15 +4,15 @@ import { ProofRequestTemplate, ProofRequestType } from './types/proof-reqeust-te
 
 export const defaultProofRequestTemplates: Array<ProofRequestTemplate> = [
   {
-    id: '8a83675e-f864-4e5a-9c4d-9787f1034c04',
-    title: 'Full name',
-    details: 'Verify the full name of a person',
+    id: 'BC:5:FullName:0.0.1:indy',
+    name: 'Full name',
+    description: 'Verify the full name of a person',
     version: '0.0.1',
     payload: {
       type: ProofRequestType.Indy,
       data: [
         {
-          schema: 'Verified Person Schema',
+          schema: 'Trx3R1frdEzbn34Sp1jyX:2:Verified Person:0.0.1',
           requestedAttributes: [
             {
               name: 'given_names',
@@ -26,15 +26,15 @@ export const defaultProofRequestTemplates: Array<ProofRequestTemplate> = [
     },
   },
   {
-    id: '3339c1e1-681f-433c-9f2f-8d0d1af0b3d2',
-    title: '19+ and Full name',
-    details: 'Verify if a person is 19 years end up and full name.',
+    id: 'BC:5:19+AndFullName:0.0.1:indy',
+    name: '19+ and Full name',
+    description: 'Verify if a person is 19 years end up and full name.',
     version: '0.0.1',
     payload: {
       type: ProofRequestType.Indy,
       data: [
         {
-          schema: 'Verified Person Schema',
+          schema: 'Trx3R1frdEzbn34Sp1jyX:2:Verified Person:0.0.1',
           requestedAttributes: [
             {
               name: 'given_names',
@@ -55,15 +55,15 @@ export const defaultProofRequestTemplates: Array<ProofRequestTemplate> = [
     },
   },
   {
-    id: '18183e86-4528-4d10-80f8-aa5c7cbbbfe7',
-    title: 'Over 19 years of age',
-    details: 'Verify if a person is 19 years end up.',
+    id: 'BC:5:Over19YearsOfAge:0.0.1:indy',
+    name: 'Over 19 years of age',
+    description: 'Verify if a person is 19 years end up.',
     version: '0.0.1',
     payload: {
       type: ProofRequestType.Indy,
       data: [
         {
-          schema: 'Verified Person Schema',
+          schema: 'Trx3R1frdEzbn34Sp1jyX:2:Verified Person:0.0.1',
           requestedPredicates: [
             {
               name: 'age',
@@ -76,15 +76,15 @@ export const defaultProofRequestTemplates: Array<ProofRequestTemplate> = [
     },
   },
   {
-    id: 'd91c7aab-af1a-42b6-8f1f-2702ed6e1f98',
-    title: 'Practising lawyer',
-    details: 'Verify if a person`is a practicing lawyer.',
+    id: 'BC:5:PractisingLawyer:0.0.1:indy',
+    name: 'Practising lawyer',
+    description: 'Verify if a person`is a practicing lawyer.',
     version: '0.0.1',
     payload: {
       type: ProofRequestType.Indy,
       data: [
         {
-          schema: 'Practising Lawyer',
+          schema: 'Trx3R1frdEzbn34Sp1jyX:2:Practising Lawyer:0.0.1',
           requestedAttributes: [
             {
               names: ['given_names', 'family_name', 'ppid', 'practicing_status'],
@@ -95,15 +95,15 @@ export const defaultProofRequestTemplates: Array<ProofRequestTemplate> = [
     },
   },
   {
-    id: 'e4cf6f7c-5abe-4e0d-b299-f944fe5ef8b2',
-    title: 'Practising lawyer and full name',
-    details: 'Verify if a person`is a practicing lawyer using two different credentials for extra assuarnce',
+    id: 'BC:5:PractisingLawyerAndFullName:0.0.1:indy',
+    name: 'Practising lawyer and full name',
+    description: 'Verify if a person`is a practicing lawyer using two different credentials for extra assuarnce',
     version: '0.0.1',
     payload: {
       type: ProofRequestType.Indy,
       data: [
         {
-          schema: 'Verified Person Schema',
+          schema: 'Trx3R1frdEzbn34Sp1jyX:2:Verified Person:0.0.1',
           requestedAttributes: [
             {
               name: 'given_names',
@@ -114,7 +114,7 @@ export const defaultProofRequestTemplates: Array<ProofRequestTemplate> = [
           ],
         },
         {
-          schema: 'Practising Lawyer',
+          schema: 'Trx3R1frdEzbn34Sp1jyX:2:Practising Lawyer:0.0.1',
           requestedAttributes: [
             {
               names: ['given_names', 'family_name', 'ppid', 'practicing_status'],
@@ -125,15 +125,15 @@ export const defaultProofRequestTemplates: Array<ProofRequestTemplate> = [
     },
   },
   {
-    id: 'ecf18605-81d0-4073-ac3e-b79dcf4cc97a',
-    title: 'Over some years of age',
-    details: 'Verify if a person is over some years ends up.',
+    id: 'BC:5:OverSomeYearsOfAge:0.0.1:indy',
+    name: 'Over some years of age',
+    description: 'Verify if a person is over some years ends up.',
     version: '0.0.1',
     payload: {
       type: ProofRequestType.Indy,
       data: [
         {
-          schema: 'Verified Person Schema',
+          schema: 'Trx3R1frdEzbn34Sp1jyX:2:Verified Person:0.0.1',
           requestedPredicates: [
             {
               name: 'age',
