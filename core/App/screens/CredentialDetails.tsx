82c451095d566c49048552974ec1d00427c60e44
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2023-03-09 14:49:36 -0800

    chore: add testIDs to cred details screen (#658)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    
    Signed-off-by: Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>

--- core/App/screens/CredentialDetails.tsx
+++ core/App/screens/CredentialDetails.tsx
@@ -325,7 +325,10 @@ const CredentialDetails: React.FC<CredentialDetailsProps> = ({ navigation, route
                 <Text style={[TextTheme.title, isRevoked && { color: ColorPallet.grayscale.mediumGrey }]}>
                   {t('CredentialDetails.IssuedBy') + ' '}
                 </Text>
-                <Text style={[TextTheme.normal, isRevoked && { color: ColorPallet.grayscale.mediumGrey }]}>
+                <Text
+                  style={[TextTheme.normal, isRevoked && { color: ColorPallet.grayscale.mediumGrey }]}
+                  testID={testIdWithKey('IssuerName')}
+                >
                   {credentialConnectionLabel}
                 </Text>
               </Text>
@@ -334,7 +337,10 @@ const CredentialDetails: React.FC<CredentialDetailsProps> = ({ navigation, route
                   <Text style={[TextTheme.title, { color: ColorPallet.notification.errorText }]}>
                     {t('CredentialDetails.Revoked') + ': '}
                   </Text>
-                  <Text style={[TextTheme.normal, { color: ColorPallet.notification.errorText }]}>
+                  <Text
+                    style={[TextTheme.normal, { color: ColorPallet.notification.errorText }]}
+                    testID={testIdWithKey('RevokedDate')}
+                  >
                     {revocationDate}
                   </Text>
                 </Text>
