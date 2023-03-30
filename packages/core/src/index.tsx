ba524c0994832d7a4f5830e29a047316b1a28f5b
James Ebert <jamesebert.k@gmail.com>
2023-03-30 09:25:45 -0700

    feat: basic lerna and compile steps
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -1,10 +1,10 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeModules, Platform } from 'react-native'
 
 const LINKING_ERROR =
   `The package '@aries-bifold/core' doesn't seem to be linked. Make sure: \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
-  '- You are not using Expo Go\n';
+  '- You are not using Expo Go\n'
 
 const Core = NativeModules.Core
   ? NativeModules.Core
@@ -12,11 +12,11 @@ const Core = NativeModules.Core
       {},
       {
         get() {
-          throw new Error(LINKING_ERROR);
+          throw new Error(LINKING_ERROR)
         },
       }
-    );
+    )
 
 export function multiply(a: number, b: number): Promise<number> {
-  return Core.multiply(a, b);
+  return Core.multiply(a, b)
 }
