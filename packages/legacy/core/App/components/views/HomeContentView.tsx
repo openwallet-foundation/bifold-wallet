a65a5068820ec2fe158664eaeca0deeed01e910b
Jason C. Leach <jason.leach@fullboar.ca>
2023-04-19 09:20:36 -0700

    fix: show welcome when notif
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/legacy/core/App/components/views/HomeContentView.tsx
+++ packages/legacy/core/App/components/views/HomeContentView.tsx
@@ -5,6 +5,7 @@ import { useTranslation } from 'react-i18next'
 import { StyleSheet, View, Text } from 'react-native'
 
 import { useTheme } from '../../contexts/theme'
+import { useNotifications } from '../../hooks/notifications'
 
 const offset = 25
 
@@ -17,7 +18,7 @@ const HomeContentView: React.FC<HomeContentViewProps> = ({ children }) => {
     ...useCredentialByState(CredentialState.CredentialReceived),
     ...useCredentialByState(CredentialState.Done),
   ]
-
+  const notifications = useNotifications()
   const { HomeTheme } = useTheme()
   const { t } = useTranslation()
   const styles = StyleSheet.create({
@@ -61,11 +62,13 @@ const HomeContentView: React.FC<HomeContentViewProps> = ({ children }) => {
 
     return (
       <>
-        <View style={[styles.messageContainer]}>
-          <Text adjustsFontSizeToFit style={[HomeTheme.welcomeHeader, { marginTop: offset, marginBottom: 20 }]}>
-            {t('Home.Welcome')}
-          </Text>
-        </View>
+        {notifications.total === 0 && (
+          <View style={[styles.messageContainer]}>
+            <Text adjustsFontSizeToFit style={[HomeTheme.welcomeHeader, { marginTop: offset, marginBottom: 20 }]}>
+              {t('Home.Welcome')}
+            </Text>
+          </View>
+        )}
         <View style={[styles.messageContainer]}>
           <Text style={[HomeTheme.credentialMsg, { marginTop: offset, textAlign: 'center' }]}>{credentialMsg}</Text>
         </View>
