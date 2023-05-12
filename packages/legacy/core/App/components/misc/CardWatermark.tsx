2ee4b898189788d3effb647dd032f4e31e14bc6f
wadeking98 <wkingnumber2@gmail.com>
2023-05-12 10:16:50 -0700

    apply speed up to oca 1.0 style
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- packages/legacy/core/App/components/misc/CardWatermark.tsx
+++ packages/legacy/core/App/components/misc/CardWatermark.tsx
@@ -13,10 +13,12 @@ interface CardWatermarkProps {
 const CardWatermark: React.FC<CardWatermarkProps> = ({ watermark, style, height, width }) => {
   const { TextTheme } = useTheme()
   const fontSize = Number((style as React.CSSProperties)?.fontSize ?? TextTheme.normal.fontSize)
-  const watermarkText = `${watermark} `.repeat(Math.ceil(width / (Math.cos(30) * ((fontSize / 2) * (watermark.length + 1)))))
+  const watermarkText = `${watermark} `.repeat(
+    Math.ceil(width / (Math.cos(30) * ((fontSize / 2) * (watermark.length + 1))))
+  )
   return (
     <View style={{ position: 'absolute', left: '-50%', top: '-100%', width: '200%', height: '200%' }}>
-      {Array.from({ length: Math.ceil((height * 2) / (fontSize) + 1) }).map((_, i) => (
+      {Array.from({ length: Math.ceil((height * 2) / fontSize + 1) }).map((_, i) => (
         <Text key={i} numberOfLines={1} style={style}>
           {watermarkText}
         </Text>
