b6997d283bfba3c196323b79241872d4b6442bdc
wadeking98 <wkingnumber2@gmail.com>
2023-05-29 13:29:19 -0700

    modified style to active secondary buttons
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- packages/legacy/core/App/navigators/defaultStackOptions.tsx
+++ packages/legacy/core/App/navigators/defaultStackOptions.tsx
@@ -1,8 +1,8 @@
+import { t } from 'i18next'
 import React from 'react'
 
 import HeaderTitle from '../components/texts/HeaderTitle'
 import { ITheme } from '../theme'
-import { t } from 'i18next'
 
 export function createDefaultStackOptions({ ColorPallet }: ITheme) {
   return {
@@ -24,6 +24,6 @@ export function createDefaultStackOptions({ ColorPallet }: ITheme) {
     },
     headerTitleAlign: 'center' as 'center' | 'left',
     headerTitle: (props: { children: React.ReactNode }) => <HeaderTitle {...props} />,
-    headerBackAccessibilityLabel: t('Global.Back')
+    headerBackAccessibilityLabel: t('Global.Back'),
   }
 }
