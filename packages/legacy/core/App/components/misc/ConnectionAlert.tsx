68294996de4ac1923f80350703f483598744b1ba
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2023-05-31 13:53:18 -0700

    fix: update a11y label and use proper icon color (#800)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    
    Signed-off-by: Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>

--- packages/legacy/core/App/components/misc/ConnectionAlert.tsx
+++ packages/legacy/core/App/components/misc/ConnectionAlert.tsx
@@ -64,7 +64,7 @@ const ConnectionAlert: React.FC<ConnectionAlertProps> = ({ connectionID }) => {
       marginVertical: 6,
     },
     informationIcon: {
-      color: ColorPallet.brand.icon,
+      color: ColorPallet.notification.infoIcon,
       marginLeft: 10,
     },
   })
@@ -82,7 +82,7 @@ const ConnectionAlert: React.FC<ConnectionAlertProps> = ({ connectionID }) => {
         <Text style={styles.notifyTitle}>{t('ConnectionAlert.AddedContacts')}</Text>
         <TouchableOpacity
           testID={t('Global.Info')}
-          accessibilityLabel={t('Global.Info')}
+          accessibilityLabel={t('ConnectionAlert.WhatAreContacts')}
           accessibilityRole={'button'}
           onPress={toggleInfoCard}
           hitSlop={hitSlop}
