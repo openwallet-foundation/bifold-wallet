fdcffc5cadc7345fa5bf254f1a0675977afdaa27
Akiff Manji <akiff.manji@gmail.com>
2023-06-27 13:16:21 -0700

    fix: oca build path (#860)
    Signed-off-by: Akiff Manji <akiff.manji@gmail.com>

--- packages/oca/src/formatters/index.ts
+++ packages/oca/src/formatters/index.ts
@@ -1,1 +1,5 @@
-export { CredentialFormatter } from './Credential'
+import CredentialFormatter from './credential/CredentialFormatter'
+import DisplayAttribute from './credential/DisplayAttribute'
+import LocalizedCredential from './credential/LocalizedCredential'
+
+export { CredentialFormatter, DisplayAttribute, LocalizedCredential }
