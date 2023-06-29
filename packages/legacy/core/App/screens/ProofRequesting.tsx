a2b070c90e7a56e8e19234f9c8328a2066246bd8
Wade King <wkingnumber2@gmail.com>
2023-06-29 14:06:14 -0700

    feat: verifier QR code density (#863)
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>
    
    Signed-off-by: Wade King <wkingnumber2@gmail.com>

--- packages/legacy/core/App/screens/ProofRequesting.tsx
+++ packages/legacy/core/App/screens/ProofRequesting.tsx
@@ -1,6 +1,6 @@
 import type { StackScreenProps } from '@react-navigation/stack'
 
-import { ProofExchangeRecord } from '@aries-framework/core'
+import { DidExchangeState } from '@aries-framework/core'
 import { useAgent, useProofById } from '@aries-framework/react-hooks'
 import { useIsFocused } from '@react-navigation/core'
 import { useFocusEffect } from '@react-navigation/native'
@@ -9,20 +9,17 @@ import { useTranslation } from 'react-i18next'
 import { BackHandler, DeviceEventEmitter, Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
-import {
-  isPresentationFailed,
-  isPresentationReceived,
-  linkProofWithTemplate,
-  createConnectionlessProofRequestInvitation,
-} from '../../verifier'
+import { isPresentationFailed, isPresentationReceived, linkProofWithTemplate, sendProofRequest } from '../../verifier'
 import LoadingIndicator from '../components/animated/LoadingIndicator'
 import Button, { ButtonType } from '../components/buttons/Button'
 import QRRenderer from '../components/misc/QRRenderer'
 import { EventTypes } from '../constants'
 import { useTheme } from '../contexts/theme'
+import { useConnectionByOutOfBandId } from '../hooks/connections'
 import { useTemplate } from '../hooks/proof-request-templates'
 import { BifoldError } from '../types/error'
 import { ProofRequestsStackParams, Screens } from '../types/navigators'
+import { createTempConnectionInvitation } from '../utils/helpers'
 import { testIdWithKey } from '../utils/testable'
 
 type ProofRequestingProps = StackScreenProps<ProofRequestsStackParams, Screens.ProofRequesting>
@@ -49,8 +46,10 @@ const ProofRequesting: React.FC<ProofRequestingProps> = ({ route, navigation }) 
   const isFocused = useIsFocused()
   const [generating, setGenerating] = useState(true)
   const [message, setMessage] = useState<string | undefined>(undefined)
-  const [recordId, setRecordId] = useState<string | undefined>(undefined)
-  const record: ProofExchangeRecord | undefined = useProofById(recordId ?? '')
+  const [connectionRecordId, setConnectionRecordId] = useState<string | undefined>(undefined)
+  const [proofRecordId, setProofRecordId] = useState<string | undefined>(undefined)
+  const record = useConnectionByOutOfBandId(connectionRecordId ?? '')
+  const proofRecord = useProofById(proofRecordId ?? '')
   const template = useTemplate(templateId)
 
   const styles = StyleSheet.create({
@@ -113,11 +112,10 @@ const ProofRequesting: React.FC<ProofRequestingProps> = ({ route, navigation }) 
     try {
       setMessage(undefined)
       setGenerating(true)
-      const result = await createConnectionlessProofRequestInvitation(agent, template, predicateValues)
+      const result = await createTempConnectionInvitation(agent, 'verify')
       if (result) {
-        setRecordId(result.proofRecord.id)
-        setMessage(JSON.stringify(result.invitation.toJSON()))
-        linkProofWithTemplate(agent, result.proofRecord, templateId)
+        setConnectionRecordId(result.record.id)
+        setMessage(result.invitationUrl)
       }
     } catch (e) {
       const error = new BifoldError(t('Error.Title1038'), t('Error.Message1038'), (e as Error).message, 1038)
@@ -148,10 +146,24 @@ const ProofRequesting: React.FC<ProofRequestingProps> = ({ route, navigation }) 
   }, [isFocused])
 
   useEffect(() => {
-    if (record && (isPresentationReceived(record) || isPresentationFailed(record))) {
-      navigation.navigate(Screens.ProofDetails, { recordId: record.id })
+    const sendAsyncProof = async () => {
+      if (record && record.state === DidExchangeState.Completed) {
+        // send proof logic
+        const result = await sendProofRequest(agent, template, record.id, predicateValues)
+        if (result?.proofRecord) {
+          linkProofWithTemplate(agent, result.proofRecord, templateId)
+        }
+        setProofRecordId(result?.proofRecord.id)
+      }
     }
+    sendAsyncProof()
   }, [record])
+
+  useEffect(() => {
+    if (proofRecord && (isPresentationReceived(proofRecord) || isPresentationFailed(proofRecord))) {
+      navigation.navigate(Screens.ProofDetails, { recordId: proofRecord.id })
+    }
+  }, [proofRecord])
 
   return (
     <SafeAreaView style={styles.container} edges={['left', 'right']}>
