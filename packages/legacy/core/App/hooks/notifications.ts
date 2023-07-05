eea1d579d73cd98d8a6feb7c1aae765c205cdd7e
Wade King <wkingnumber2@gmail.com>
2023-07-04 23:30:36 -0700

    fix: mob ver proof notif (#873)
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>
    
    Signed-off-by: Wade King <wkingnumber2@gmail.com>

--- packages/legacy/core/App/hooks/notifications.ts
+++ packages/legacy/core/App/hooks/notifications.ts
@@ -6,7 +6,6 @@ import {
 } from '@aries-framework/core'
 import { useCredentialByState, useProofByState } from '@aries-framework/react-hooks'
 
-import { ProofCustomMetadata, ProofMetadata } from '../../verifier'
 import { CredentialMetadata, customMetadata } from '../types/metadata'
 
 interface Notifications {
@@ -17,14 +16,6 @@ interface Notifications {
 export const useNotifications = (): Notifications => {
   const offers = useCredentialByState(CredentialState.OfferReceived)
   const proofsRequested = useProofByState(ProofState.RequestReceived)
-  const proofsDone = useProofByState([ProofState.Done, ProofState.PresentationReceived]).filter(
-    (proof: ProofExchangeRecord) => {
-      if (proof.isVerified === undefined) return false
-
-      const metadata = proof.metadata.get(ProofMetadata.customMetadata) as ProofCustomMetadata
-      return !metadata?.details_seen
-    }
-  )
   const revoked = useCredentialByState(CredentialState.Done).filter((cred: CredentialRecord) => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const metadata = cred!.metadata.get(CredentialMetadata.customMetadata) as customMetadata
@@ -33,7 +24,7 @@ export const useNotifications = (): Notifications => {
     }
   })
 
-  const notifications = [...offers, ...proofsRequested, ...proofsDone, ...revoked].sort(
+  const notifications = [...offers, ...proofsRequested, ...revoked].sort(
     (a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
   )
 
