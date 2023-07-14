4d6d99948fcb7618ff3ede1c9d4879505e679fe6
Thiago Romano <112953503+thiagoromanos@users.noreply.github.com>
2023-07-14 14:12:25 -0300

    feat: implement the remote OCA resolver (#883)
    Signed-off-by: Thiago Romano <thiagoromano.s@gmail.co>
    Signed-off-by: Thiago Romano dos Santos <thiagoromano.s@gmail.com>
    Signed-off-by: Thiago Romano <112953503+thiagoromanos@users.noreply.github.com>
    Co-authored-by: Thiago Romano <thiagoromano.s@gmail.co>

--- packages/oca/src/index.ts
+++ packages/oca/src/index.ts
@@ -2,6 +2,4 @@ export * from './formatters'
 export * from './interfaces'
 export * from './types'
 export * from './utils'
-
-// DEPRECATED: refrain from using these exports
 export * as legacy from './legacy'
