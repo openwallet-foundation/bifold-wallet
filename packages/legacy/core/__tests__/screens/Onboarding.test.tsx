a71e974fe356624580845179e823f7b13cbb98b7
Wade King <wkingnumber2@gmail.com>
2023-07-24 14:29:57 -0700

    feat: added developer mode to onboarding (#899)
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- packages/legacy/core/__tests__/screens/Onboarding.test.tsx
+++ packages/legacy/core/__tests__/screens/Onboarding.test.tsx
@@ -1,11 +1,14 @@
-import { render } from '@testing-library/react-native'
+import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
 import { Text } from 'react-native'
 
 import * as themeContext from '../../App/contexts/theme' // note we're importing with a * to import all the exports
 import Onboarding, { OnboardingStyleSheet } from '../../App/screens/Onboarding'
-import { createCarouselStyle } from '../../App/screens/OnboardingPages'
+import { createCarouselStyle, createPageWith } from '../../App/screens/OnboardingPages'
 import { OnboardingTheme, theme } from '../../App/theme'
+import CredentialList from '../../App/assets/img/credential-list.svg'
+import { testIdWithKey } from '../../App/utils/testable'
+
 
 export const carousel: OnboardingStyleSheet = createCarouselStyle(OnboardingTheme)
 
@@ -42,4 +45,18 @@ describe('Onboarding', () => {
 
     expect(foundPages.length).toBe(2)
   })
+
+  test('Onboarding Developer mode', async () => {
+    jest.spyOn(themeContext, 'useTheme').mockImplementation(() => theme)
+    const testFunc = jest.fn();
+    const tree = render(
+      <Onboarding pages={[createPageWith(CredentialList, "test", "body", {}, true, testFunc)]} nextButtonText="Next" previousButtonText="Back" style={carousel} />
+    )
+    expect(tree).toMatchSnapshot()
+
+    const devModeButton = await tree.getByTestId(testIdWithKey('DeveloperModeTouch'))
+    fireEvent(devModeButton, "press")
+    expect(testFunc).toBeCalledTimes(1)
+  })
+
 })
