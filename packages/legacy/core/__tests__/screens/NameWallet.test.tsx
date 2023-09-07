96fbaf4d94ad73616e27cd7cdceac5aecf46cfd5
wadeking98 <wkingnumber2@gmail.com>
2023-09-07 14:23:34 -0700

    updated nameWallet test
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- packages/legacy/core/__tests__/screens/NameWallet.test.tsx
+++ packages/legacy/core/__tests__/screens/NameWallet.test.tsx
@@ -3,6 +3,10 @@ import React from 'react'
 
 import NameWallet from '../../App/screens/NameWallet'
 import { testIdWithKey } from '../../App/utils/testable'
+import configurationContext from '../contexts/configuration'
+import { ConfigurationContext } from '../../App/contexts/configuration'
+import { StoreContext, StoreProvider } from '../../App'
+import { testDefaultState } from '../contexts/store'
 
 jest.mock('@react-navigation/core', () => {
   return require('../../__mocks__/custom/@react-navigation/core')
@@ -21,5 +25,32 @@ describe('NameWallet Screen', () => {
 
     expect(WalletNameInput).not.toBeNull()
     expect(ContinueButton).not.toBeNull()
+  })
+
+  test('Check non-onboarding rendering', async () => {
+    const customState = {
+      ...testDefaultState,
+      onboarding: {
+        didAgreeToTerms: true,
+        didCompleteTutorial: true,
+        didCreatePIN: true,
+        didConsiderBiometry: true,
+        didNameWallet: true,
+      },
+    } 
+    const tree = render(
+      <StoreContext.Provider value={[customState, () => { return }]}>
+        <ConfigurationContext.Provider value={configurationContext}>
+          <NameWallet />
+        </ConfigurationContext.Provider>
+      </StoreContext.Provider>
+    )
+
+
+    const SaveButton = await tree.getByTestId(testIdWithKey('Save'))
+    const CancelButton = await tree.getByTestId(testIdWithKey('Cancel'))
+
+    expect(SaveButton).not.toBeNull()
+    expect(CancelButton).not.toBeNull()
   })
 })
