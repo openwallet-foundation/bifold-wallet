00edcdb6d1ac76ac0db4ce358dab84ab518da529
Clécio Varjão <1348549+cvarjao@users.noreply.github.com>
2023-09-16 17:51:34 -0700

    Fix unit tests
    Signed-off-by: Clécio Varjão <1348549+cvarjao@users.noreply.github.com>

--- packages/legacy/core/__tests__/screens/LoadingModal.test.tsx
+++ packages/legacy/core/__tests__/screens/LoadingModal.test.tsx
@@ -1,21 +1,32 @@
-import { render } from '@testing-library/react-native'
+import { act, render } from '@testing-library/react-native'
 import React from 'react'
 
 import LoadingModal from '../../App/components/modals/LoadingModal'
 import { testIdWithKey } from '../../App/utils/testable'
 
 describe('displays loading screen', () => {
+  beforeAll(()=>{
+    jest.useFakeTimers()
+  })
+  afterAll(()=>{
+    jest.useRealTimers()
+  })
   test('renders correctly', () => {
     const tree = render(<LoadingModal />)
 
     expect(tree).toMatchSnapshot()
   })
 
-  test('contains testIDs', () => {
-    const { getByTestId } = render(<LoadingModal />)
-
-    const loadingActivityIndicatorID = getByTestId(testIdWithKey('LoadingActivityIndicator'))
-
-    expect(loadingActivityIndicatorID).not.toBeNull()
+  test('contains testIDs', async () => {
+    let tree: ReturnType<typeof render>
+    
+    tree = render(<LoadingModal />)
+    await act(()=>{
+      jest.runAllTimers()
+    })
+    await act(async ()=>{
+      const loadingActivityIndicatorID = await tree.findByTestId(testIdWithKey('LoadingActivityIndicator'))
+      expect(loadingActivityIndicatorID).not.toBeNull()
+    })
   })
 })
