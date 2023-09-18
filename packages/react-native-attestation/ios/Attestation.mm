9ef0792e2a9ff1978e4607c715fea13518ced603
Jason C. Leach <jason.leach@fullboar.ca>
2023-09-18 16:49:36 -0700

    fix: worn on attestation pkg
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/react-native-attestation/ios/Attestation.mm
+++ packages/react-native-attestation/ios/Attestation.mm
@@ -1,10 +1,25 @@
 #import "Attestation.h"
 #import <DeviceCheck/DeviceCheck.h>
 #import <CommonCrypto/CommonCrypto.h>
+#import <Security/Security.h>
 
 @implementation Attestation
 RCT_EXPORT_MODULE()
 
+NSMutableDictionary *queryForKeychain(NSString *identifier) {
+    NSMutableDictionary *query = [NSMutableDictionary dictionary];
+    [query setObject:(__bridge id)kSecClassGenericPassword forKey:(__bridge id)kSecClass];
+    [query setObject:identifier forKey:(__bridge id)kSecAttrAccount];
+    
+    return query;
+}
+
+NSString *keychainIdentifier2() {
+    NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
+    NSString *concatenatedString = [bundleID stringByAppendingString:@".AttestationKey"];
+    
+    return concatenatedString;
+}
 
 NSArray<NSNumber *> *sha256Of(NSString *stringToBeHashed) {
     NSData *data = [stringToBeHashed dataUsingEncoding:NSUTF8StringEncoding];
@@ -24,18 +39,69 @@ NSArray<NSNumber *> *dataToBytes(NSData *data) {
     for (NSUInteger i = 0; i < data.length; i++) {
         [array addObject:@(bytes[i])];
     }
-
+    
     return [array copy];
 }
+
+BOOL saveStringToKeychain(NSString *string, NSString *identifier) {
+    NSData *stringData = [string dataUsingEncoding:NSUTF8StringEncoding];
+    
+    NSMutableDictionary *query = queryForKeychain(identifier);
+    [query setObject:stringData forKey:(__bridge id)kSecValueData];
+    [query setObject:(__bridge id)kSecAttrAccessibleWhenUnlocked forKey:(__bridge id)kSecAttrAccessible];
+    
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
+    
+    return status == errSecSuccess;
+}
+
+NSString *getStringFromKeychainWithIdentifier(NSString *identifier) {
+
+    NSMutableDictionary *query = queryForKeychain(identifier);
+    [query setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
+    [query setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
+    
+    CFTypeRef dataRef = NULL;
+    OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &dataRef);
+    if (status == errSecSuccess) {
+        NSData *data = (__bridge_transfer NSData *)dataRef;
+        return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    } else {
+        return nil;
+    }
+}
+
 
 // See // https://reactnative.dev/docs/native-modules-ios
 
 RCT_EXPORT_METHOD(generateKey:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
+    NSLog(@"**************************** XXXX **********************************");
+
     DCAppAttestService *attestService = [DCAppAttestService sharedService];
     
+    NSString *keychainIdentifier = keychainIdentifier2();
+    NSString *keyId = getStringFromKeychainWithIdentifier(keychainIdentifier);
+    NSLog(@"found key %@", keyId);
+
+    if (keyId != nil) {
+        NSLog(@"returning existing key...");
+        resolve(keyId);
+        
+        return;
+    }
+    
     [attestService generateKeyWithCompletionHandler:^(NSString * _Nullable keyId, NSError * _Nullable error) {
+        if (error) {
+            NSLog(@"generateKeyWithCompletionHandler error %@", error);
+            reject(@"error", @"error", error);
+        }
+        
+        NSLog(@"saving key to KC = ", keyId);
+
+        saveStringToKeychain(keyId, keychainIdentifier);
+        
         resolve(keyId);
     }];
 }
@@ -64,6 +130,7 @@ RCT_EXPORT_METHOD(appleAttestationAsBase64:(NSString *)keyId
     [attestService attestKey:keyId clientDataHash:hashData completionHandler:^(NSData * _Nullable attestationObject, NSError * _Nullable error) {
 
         if (error) {
+            NSLog(@"appleAttestationAsBase64 error %@", error);
             reject(@"error", @"error", error);
         } else {
             NSArray<NSNumber *> *result = dataToBytes(attestationObject);
