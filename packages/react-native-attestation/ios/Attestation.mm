6232b572033c4aa04281946f5f69fbc92356cec7
Jason C. Leach <jason.leach@fullboar.ca>
2023-09-19 09:58:27 -0700

    fix: worn on attestation pkg
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/react-native-attestation/ios/Attestation.mm
+++ packages/react-native-attestation/ios/Attestation.mm
@@ -6,13 +6,7 @@
 @implementation Attestation
 RCT_EXPORT_MODULE()
 
-NSMutableDictionary *queryForKeychain(NSString *identifier) {
-    NSMutableDictionary *query = [NSMutableDictionary dictionary];
-    [query setObject:(__bridge id)kSecClassGenericPassword forKey:(__bridge id)kSecClass];
-    [query setObject:identifier forKey:(__bridge id)kSecAttrAccount];
-    
-    return query;
-}
+NSString *KeychainServiceName = @"AriesAttestation";
 
 NSString *keychainIdentifier2() {
     NSString *bundleID = [[NSBundle mainBundle] bundleIdentifier];
@@ -44,23 +38,30 @@ NSArray<NSNumber *> *dataToBytes(NSData *data) {
 }
 
 BOOL saveStringToKeychain(NSString *string, NSString *identifier) {
-    NSData *stringData = [string dataUsingEncoding:NSUTF8StringEncoding];
+    NSData *stringAsData = [string dataUsingEncoding:NSUTF8StringEncoding];
     
-    NSMutableDictionary *query = queryForKeychain(identifier);
-    [query setObject:stringData forKey:(__bridge id)kSecValueData];
-    [query setObject:(__bridge id)kSecAttrAccessibleWhenUnlocked forKey:(__bridge id)kSecAttrAccessible];
+    NSDictionary *attributes = @{
+        (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
+        (__bridge id)kSecAttrService: KeychainServiceName,
+        (__bridge id)kSecAttrAccount: identifier,
+        (__bridge id)kSecValueData: stringAsData
+    };
     
-    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)query, NULL);
+    OSStatus status = SecItemAdd((__bridge CFDictionaryRef)attributes, NULL);
     
     return status == errSecSuccess;
 }
 
-NSString *getStringFromKeychainWithIdentifier(NSString *identifier) {
+NSString *stringFromKeychainWithIdentifier(NSString *identifier) {
 
-    NSMutableDictionary *query = queryForKeychain(identifier);
-    [query setObject:(__bridge id)kCFBooleanTrue forKey:(__bridge id)kSecReturnData];
-    [query setObject:(__bridge id)kSecMatchLimitOne forKey:(__bridge id)kSecMatchLimit];
-    
+    NSDictionary *query = @{
+        (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
+        (__bridge id)kSecAttrService: KeychainServiceName,
+        (__bridge id)kSecAttrAccount: identifier,
+        (__bridge id)kSecReturnData: (__bridge id)kCFBooleanTrue,
+        (__bridge id)kSecMatchLimit: (__bridge id)kSecMatchLimitOne
+    };
+        
     CFTypeRef dataRef = NULL;
     OSStatus status = SecItemCopyMatching((__bridge CFDictionaryRef)query, &dataRef);
     if (status == errSecSuccess) {
@@ -71,6 +72,70 @@ NSString *getStringFromKeychainWithIdentifier(NSString *identifier) {
     }
 }
 
+BOOL clearStoredKeyIfExists(NSString *identifier) {
+    NSDictionary *query = @{
+        (__bridge id)kSecClass: (__bridge id)kSecClassGenericPassword,
+        (__bridge id)kSecAttrService: identifier
+    };
+
+    // Delete the item from the Keychain.
+    OSStatus status = SecItemDelete((__bridge CFDictionaryRef)query);
+
+    if (status == errSecSuccess) {
+        NSLog(@"Item deleted successfully.");
+        return YES;
+    } else if (status == errSecItemNotFound) {
+        NSLog(@"Item not found in Keychain.");
+        return NO;
+    } else {
+        NSLog(@"Error deleting item from Keychain. Status code: %d", (int)status);
+        return NO;
+    }
+}
+
+NSError *errorWithReason(NSString *reason, NSInteger code) {
+    NSString *domain = [[NSBundle mainBundle] bundleIdentifier];
+    NSDictionary *userInfo = @{
+        NSLocalizedDescriptionKey: @"An error occurred.",
+        NSLocalizedFailureReasonErrorKey: reason,
+    };
+    
+    return [NSError errorWithDomain:domain code:code userInfo:userInfo];
+}
+
+NSString *messageAttestationErrorCode(NSInteger code) {
+    NSString *result;
+    
+    switch (code) {
+        case 3:
+            result = @"The provided key ID is invalid or the key is not found.";
+            break;
+        case 4:
+            result = @"The device is not eligible for App Attestation.";
+            break;
+        default:
+            result = @"Unable to generate attestation object.";
+            break;
+    }
+    
+    return result;
+}
+
+DCAppAttestService *sharedService() {
+    NSString *version = [[UIDevice currentDevice] systemVersion];
+    if ([version compare:@"14.0" options:NSNumericSearch] != NSOrderedDescending) {
+        NSLog(@"iOS version is less than 14.0");
+        return nil;
+    }
+    
+    DCAppAttestService *service = [DCAppAttestService sharedService];
+    if (service.isSupported) {
+        return service;
+    }
+    
+    return nil;
+}
+
 
 // See // https://reactnative.dev/docs/native-modules-ios
 
@@ -79,11 +144,16 @@ RCT_EXPORT_METHOD(generateKey:(RCTPromiseResolveBlock)resolve
 {
     NSLog(@"**************************** XXXX **********************************");
 
-    DCAppAttestService *attestService = [DCAppAttestService sharedService];
+    DCAppAttestService *attestService = sharedService();
+    if (attestService == nil) {
+        NSString *message = @"The device is not eligible for App Attestation.";
+        reject(@"error", message, errorWithReason(message, 22));
+
+        return;
+    }
     
     NSString *keychainIdentifier = keychainIdentifier2();
-    NSString *keyId = getStringFromKeychainWithIdentifier(keychainIdentifier);
-    NSLog(@"found key %@", keyId);
+    NSString *keyId = stringFromKeychainWithIdentifier(keychainIdentifier);
 
     if (keyId != nil) {
         NSLog(@"returning existing key...");
@@ -95,7 +165,9 @@ RCT_EXPORT_METHOD(generateKey:(RCTPromiseResolveBlock)resolve
     [attestService generateKeyWithCompletionHandler:^(NSString * _Nullable keyId, NSError * _Nullable error) {
         if (error) {
             NSLog(@"generateKeyWithCompletionHandler error %@", error);
-            reject(@"error", @"error", error);
+            reject(@"error", @"Unable to generate key.", errorWithReason(@"Unable to generate key.", 21));
+            
+            return;
         }
         
         NSLog(@"saving key to KC = ", keyId);
@@ -120,24 +192,37 @@ RCT_EXPORT_METHOD(appleAttestationAsBase64:(NSString *)keyId
                   resolve:(RCTPromiseResolveBlock)resolve
                   reject:(RCTPromiseRejectBlock)reject)
 {
+    NSInteger InvalidKeyErrorCode = 3;
+    DCAppAttestService *attestService = sharedService();
+    if (attestService == nil) {
+        NSString *message = @"The device is not eligible for App Attestation.";
+        reject(@"error", message, errorWithReason(message, 22));
+        
+        return;
+    }
+    
     NSData *data = [challenge dataUsingEncoding:NSUTF8StringEncoding];
     uint8_t hash[CC_SHA256_DIGEST_LENGTH];
     CC_SHA256(data.bytes, (CC_LONG)data.length, hash);
     NSData *hashData = [NSData dataWithBytes:hash length:CC_SHA256_DIGEST_LENGTH];
-    
-    DCAppAttestService *attestService = [DCAppAttestService sharedService];
-    
+        
     [attestService attestKey:keyId clientDataHash:hashData completionHandler:^(NSData * _Nullable attestationObject, NSError * _Nullable error) {
 
         if (error) {
             NSLog(@"appleAttestationAsBase64 error %@", error);
-            reject(@"error", @"error", error);
+            NSString *message = messageAttestationErrorCode(error.code);
+            
+            if (error.code == InvalidKeyErrorCode) {
+                NSString *keychainIdentifier = keychainIdentifier2();
+                clearStoredKeyIfExists(keychainIdentifier);
+            }
+            
+            reject(@"error", message, errorWithReason(message, error.code));
         } else {
             NSArray<NSNumber *> *result = dataToBytes(attestationObject);
             resolve(result);
         }
     }];
-    
 }
 
 // Don't compile this code when we build for the old architecture.
