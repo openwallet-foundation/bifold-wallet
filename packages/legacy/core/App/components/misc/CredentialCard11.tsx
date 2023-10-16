6853844f571e5455c8246123d26aa371479ba490
Caroline Lucas Calheirani <ccalheirani@gmail.com>
2023-10-16 13:16:28 -0300

    line break when it’s a long attribute
    Signed-off-by: Caroline Lucas Calheirani <ccalheirani@gmail.com>

--- packages/legacy/core/App/components/misc/CredentialCard11.tsx
+++ packages/legacy/core/App/components/misc/CredentialCard11.tsx
@@ -131,7 +131,7 @@ const CredentialCard11: React.FC<CredentialCard11Props> = ({
       flexGrow: 1,
       padding,
       marginLeft: -1 * logoHeight + padding,
-      marginRight: logoHeight + (logoHeight - padding) + padding,
+      marginRight: logoHeight * 2,
     },
     imageAttr: {
       height: 150,
