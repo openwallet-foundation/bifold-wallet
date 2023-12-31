b30df5eabf6a18ede88153633fdc181bcb47c248
OPS <ops@Mostafas-MacBook-Pro.local>
2023-12-31 09:59:13 -0500

    inject global screen configuration into core config
    Signed-off-by: OPS <ops@Mostafas-MacBook-Pro.local>

--- packages/legacy/core/App/navigators/defaultStackOptions.tsx
+++ packages/legacy/core/App/navigators/defaultStackOptions.tsx
@@ -1,30 +1,36 @@
+import { StackNavigationOptions } from '@react-navigation/stack'
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 import HeaderTitle from '../components/texts/HeaderTitle'
+import { useConfiguration } from '../contexts/configuration'
 import { ITheme } from '../theme'
 
-export function createDefaultStackOptions({ ColorPallet }: ITheme): any {
+export function createDefaultStackOptions({ ColorPallet }: ITheme): StackNavigationOptions {
   const { t } = useTranslation()
-  return {
-    headerTintColor: ColorPallet.brand.headerIcon,
-    headerShown: true,
-    headerBackTitleVisible: false,
-    headerTitleContainerStyle: {
-      flexShrink: 1,
-      maxWidth: '68%',
-      width: '100%',
-    },
-    headerStyle: {
-      elevation: 0,
-      shadowOffset: { width: 0, height: 6 },
-      shadowRadius: 6,
-      shadowColor: ColorPallet.grayscale.black,
-      shadowOpacity: 0.15,
-      borderBottomWidth: 0,
-    },
-    headerTitleAlign: 'center' as 'center' | 'left',
-    headerTitle: (props: { children: React.ReactNode }) => <HeaderTitle {...props} />,
-    headerBackAccessibilityLabel: t('Global.Back'),
-  }
+  const { globalScreenOptions } = useConfiguration()
+
+  return (
+    globalScreenOptions ?? {
+      headerTintColor: ColorPallet.brand.headerIcon,
+      headerShown: true,
+      headerBackTitleVisible: false,
+      headerTitleContainerStyle: {
+        flexShrink: 1,
+        maxWidth: '68%',
+        width: '100%',
+      },
+      headerStyle: {
+        elevation: 0,
+        shadowOffset: { width: 0, height: 6 },
+        shadowRadius: 6,
+        shadowColor: ColorPallet.grayscale.black,
+        shadowOpacity: 0.15,
+        borderBottomWidth: 0,
+      },
+      headerTitleAlign: 'center' as 'center' | 'left',
+      headerTitle: (props: { children: React.ReactNode }) => <HeaderTitle {...props} />,
+      headerBackAccessibilityLabel: t('Global.Back'),
+    }
+  )
 }
