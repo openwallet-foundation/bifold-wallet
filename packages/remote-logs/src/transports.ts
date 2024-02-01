38f4eaa635a8093f119f40a281fa809b74ef1a95
Jason C. Leach <jason.leach@fullboar.ca>
2024-01-31 16:38:18 -0800

    fix: issue with URL pkg
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/remote-logs/src/transports.ts
+++ packages/remote-logs/src/transports.ts
@@ -1,5 +1,6 @@
-import axios, { AxiosRequestConfig } from 'axios'
+import axios from 'axios'
 import { transportFunctionType } from 'react-native-logs'
+import { Buffer } from 'buffer'
 
 export interface RemoteLoggerOptions {
   lokiUrl?: string
@@ -55,26 +56,36 @@ export const lokiTransport: transportFunctionType = (props: LokiTransportProps) 
     ],
   }
 
-  const [credentials, href] = lokiUrl.split('@')
+  const lokiUrl2 =
+    'https://90e08348:acf0bb5692b8ecea193d9da458dade0c@bcwallet-logstack-proxy-e79518-dev.apps.silver.devops.gov.bc.ca/loki/api/v1/push'
+
+  const [credentials, href] = lokiUrl2.split('@')
   const [username, password] = credentials.split('//')[1].split(':')
   const protocol = credentials.split('//')[0]
-  const axiosConfig: AxiosRequestConfig = {
-    method: 'post',
-    url: lokiUrl,
-    data: payload,
-  }
+  // const axiosConfig: AxiosRequestConfig = {
+  //   method: 'post',
+  //   url: lokiUrl2,
+  //   data: payload,
+  // }
 
-  // If username and password are present
-  // in the URL, use them for auth
-  if (username && password) {
-    axiosConfig.auth = {
-      username,
-      password,
-    }
-    axiosConfig.url = `${protocol}//${href}`
-  }
+  // // If username and password are present
+  // // in the URL, use them for auth
+  // if (username && password) {
+  //   axiosConfig.auth = {
+  //     username,
+  //     password,
+  //   }
+  //   axiosConfig.url = `${protocol}//${href}`
+  // }
 
-  axios(axiosConfig)
+  // console.log(axiosConfig)
+
+  const authHeader = Buffer.from(`Basic ${username}:${password}`).toString('base64')
+
+  axios
+    .post(`${protocol}//${href}`, payload, {
+      headers: { Authorization: authHeader },
+    })
     .then((res) => {
       if (res.status !== 204) {
         throw new Error(`Expected Loki to return 204, received ${res.status}`)
