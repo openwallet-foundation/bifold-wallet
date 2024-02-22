ebdd6b31d9673f1119f0cbbce12eaab0753c64c5
Bryce McMath <bryce.j.mcmath@gmail.com>
2024-02-21 22:34:51 -0800

    feat: verifier credential card preview
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

---
+++ packages/legacy/core/__tests__/components/CredentialCardPreview.test.tsx
@@ -0,0 +1,46 @@
+import { act, render } from '@testing-library/react-native'
+import React from 'react'
+
+import CredentialCardPreview from '../../App/components/misc/CredentialCardPreview'
+import { ConfigurationContext } from '../../App/contexts/configuration'
+import configurationContext from '../contexts/configuration'
+
+const displayItems = [
+  {
+    name: 'given_names',
+    restrictions: [
+      { schema_id: '4eCXHS79ykiMv2PoBxPK23:2:unverified_person:0.1.0', issuer_did: '4eCXHS79ykiMv2PoBxPK23' },
+      { schema_id: 'HTkhhCW1bAXWnxC1u3YVoa:2:unverified_person:0.1.0', issuer_did: 'HTkhhCW1bAXWnxC1u3YVoa' },
+      { schema_id: 'Ui6HA36FvN83cEtmYYHxrn:2:unverified_person:0.1.0', issuer_did: 'Ui6HA36FvN83cEtmYYHxrn' },
+    ],
+    nonRevoked: { to: 1708583231 },
+    value: null,
+  },
+  {
+    name: 'family_name',
+    restrictions: [
+      { schema_id: '4eCXHS79ykiMv2PoBxPK23:2:unverified_person:0.1.0', issuer_did: '4eCXHS79ykiMv2PoBxPK23' },
+      { schema_id: 'HTkhhCW1bAXWnxC1u3YVoa:2:unverified_person:0.1.0', issuer_did: 'HTkhhCW1bAXWnxC1u3YVoa' },
+      { schema_id: 'Ui6HA36FvN83cEtmYYHxrn:2:unverified_person:0.1.0', issuer_did: 'Ui6HA36FvN83cEtmYYHxrn' },
+    ],
+    nonRevoked: { to: 1708583231 },
+    value: null,
+  },
+]
+
+describe('CredentialCardPreview Component', () => {
+  test('Renders correctly', async () => {
+    const tree = render(
+      <ConfigurationContext.Provider value={configurationContext}>
+        <CredentialCardPreview
+          schemaId={'4eCXHS79ykiMv2PoBxPK23:2:unverified_person:0.1.0'}
+          onChangeValue={jest.fn()}
+          displayItems={displayItems}
+          elevated
+        />
+      </ConfigurationContext.Provider>
+    )
+    await act(async () => {})
+    expect(tree).toMatchSnapshot()
+  })
+})
