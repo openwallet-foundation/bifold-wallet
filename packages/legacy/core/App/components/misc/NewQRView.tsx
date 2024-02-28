a22174fbe0e2ce8f7f0575256b6a45a4e18f3a6d
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2024-02-28 14:15:14 -0800

    fix: add missing scan screen features in new scan screen (#1098)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    
    Signed-off-by: Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>

--- packages/legacy/core/App/components/misc/NewQRView.tsx
+++ packages/legacy/core/App/components/misc/NewQRView.tsx
@@ -3,10 +3,12 @@ import { useAgent } from '@aries-framework/react-hooks'
 import { StackScreenProps } from '@react-navigation/stack'
 import React, { useCallback, useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { View, StyleSheet, Text, ScrollView, useWindowDimensions } from 'react-native'
+import { View, Modal, StyleSheet, Text, ScrollView, useWindowDimensions, Pressable } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import Icon from 'react-native-vector-icons/MaterialIcons'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
+import { hitSlop } from '../../constants'
+import { useConfiguration } from '../../contexts/configuration'
 import { useStore } from '../../contexts/store'
 import { useTheme } from '../../contexts/theme'
 import { useConnectionByOutOfBandId } from '../../hooks/connections'
@@ -16,6 +18,7 @@ import { createConnectionInvitation } from '../../utils/helpers'
 import { testIdWithKey } from '../../utils/testable'
 import LoadingIndicator from '../animated/LoadingIndicator'
 import HeaderButton, { ButtonLocation } from '../buttons/HeaderButton'
+import InfoBox, { InfoBoxType } from '../misc/InfoBox'
 
 import QRRenderer from './QRRenderer'
 import QRScannerTorch from './QRScannerTorch'
@@ -35,6 +38,8 @@ const NewQRView: React.FC<Props> = ({ defaultToConnect, handleCodeScan, error, e
   const { width } = useWindowDimensions()
   const qrSize = width - 40
   const [store] = useStore()
+  const { showScanHelp, showScanButton } = useConfiguration()
+  const [showInfoBox, setShowInfoBox] = useState(false)
   const [torchActive, setTorchActive] = useState(false)
   const [firstTabActive, setFirstTabActive] = useState(!defaultToConnect)
   const [invitation, setInvitation] = useState<string | undefined>(undefined)
@@ -44,9 +49,13 @@ const NewQRView: React.FC<Props> = ({ defaultToConnect, handleCodeScan, error, e
   const { agent } = useAgent()
 
   const styles = StyleSheet.create({
-    container: {
+    mainSafeArea: {
       flex: 1,
       backgroundColor: ColorPallet.brand.secondaryBackground,
+    },
+    bottomSafeArea: {
+      flex: 0,
+      backgroundColor: ColorPallet.brand.primaryBackground,
     },
     camera: {
       flex: 1,
@@ -66,16 +75,16 @@ const NewQRView: React.FC<Props> = ({ defaultToConnect, handleCodeScan, error, e
       borderColor: ColorPallet.grayscale.white,
     },
     viewFinderContainer: {
-      flex: 1,
-      width: '100%',
+      flexGrow: 1,
       justifyContent: 'center',
       alignItems: 'center',
     },
-    errorContainer: {
+    messageContainer: {
+      marginHorizontal: 40,
       flexDirection: 'row',
       alignItems: 'center',
-      marginTop: 20,
-      paddingHorizontal: 20,
+      alignSelf: 'center',
+      paddingTop: 30,
     },
     icon: {
       color: ColorPallet.grayscale.white,
@@ -98,7 +107,18 @@ const NewQRView: React.FC<Props> = ({ defaultToConnect, handleCodeScan, error, e
       marginTop: 20,
       textAlign: 'center',
     },
-    editButton: { ...TextTheme.headingTwo, marginBottom: 20, marginLeft: 10, color: ColorPallet.brand.primary },
+    textStyle: {
+      ...TextTheme.title,
+      color: 'white',
+      marginHorizontal: 10,
+      textAlign: 'center',
+    },
+    editButton: {
+      ...TextTheme.headingTwo,
+      marginBottom: 20,
+      marginLeft: 10,
+      color: ColorPallet.brand.primary,
+    },
   })
 
   const createInvitation = useCallback(async () => {
@@ -132,89 +152,143 @@ const NewQRView: React.FC<Props> = ({ defaultToConnect, handleCodeScan, error, e
     }
   }, [record])
 
+  const styleForState = ({ pressed }: { pressed: boolean }) => [{ opacity: pressed ? 0.2 : 1 }]
+
+  const toggleShowInfoBox = () => setShowInfoBox(!showInfoBox)
+
   return (
-    <SafeAreaView edges={['bottom', 'left', 'right']} style={styles.container}>
-      {firstTabActive ? (
-        <>
-          <ScanCamera
-            handleCodeScan={handleCodeScan}
-            enableCameraOnError={enableCameraOnError}
-            error={error}
-            torchActive={torchActive}
-          />
-          <View style={styles.cameraViewContainer}>
-            <View style={styles.errorContainer}>
-              {error ? (
-                <>
-                  <Icon style={styles.icon} name="cancel" size={30}></Icon>
-                  <Text
-                    testID={testIdWithKey('ErrorMessage')}
-                    style={[TextTheme.normal, { color: ColorPallet.grayscale.white }]}
-                  >
-                    {error.message}
-                  </Text>
-                </>
-              ) : (
-                <Text style={[TextTheme.normal, { color: ColorPallet.grayscale.white }]}>
-                  {t('Scan.WillScanAutomatically')}
-                </Text>
-              )}
-            </View>
-            <View style={styles.viewFinderContainer}>
-              <View style={styles.viewFinder} />
-            </View>
-            <QRScannerTorch active={torchActive} onPress={() => setTorchActive(!torchActive)} />
-          </View>
-        </>
-      ) : (
-        <ScrollView>
-          <View style={{ alignItems: 'center' }}>
-            <View style={styles.qrContainer}>
-              {!invitation && <LoadingIndicator />}
-              {invitation && <QRRenderer value={invitation} size={qrSize} />}
-            </View>
-            <View style={{ paddingHorizontal: 20, flex: 1 }}>
+    <>
+      <SafeAreaView edges={['left', 'right']} style={styles.mainSafeArea}>
+        {firstTabActive ? (
+          <>
+            <Modal visible={showInfoBox} animationType="fade" transparent>
               <View
                 style={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'center',
+                  flex: 1,
+                  paddingHorizontal: 10,
                   justifyContent: 'center',
+                  alignItems: 'center',
+                  backgroundColor: 'rgba(0,0,0,0.6)',
                 }}
               >
-                <Text testID={testIdWithKey('WalletName')} style={[styles.walletName, { paddingHorizontal: 20 }]}>
-                  {store.preferences.walletName}
-                </Text>
-                <HeaderButton
-                  buttonLocation={ButtonLocation.Right}
-                  accessibilityLabel={t('NameWallet.EditWalletName')}
-                  testID={testIdWithKey('EditWalletName')}
-                  onPress={handleEdit}
-                  icon={'pencil'}
-                  iconTintColor={styles.walletName.color}
+                <InfoBox
+                  notificationType={InfoBoxType.Info}
+                  title={t('Scan.BadQRCode')}
+                  description={t('Scan.BadQRCodeDescription')}
+                  onCallToActionPressed={toggleShowInfoBox}
                 />
               </View>
-              <Text style={styles.secondaryText}>{t('Connection.ShareQR')}</Text>
-            </View>
-          </View>
-        </ScrollView>
-      )}
+            </Modal>
+            <ScanCamera
+              handleCodeScan={handleCodeScan}
+              enableCameraOnError={enableCameraOnError}
+              error={error}
+              torchActive={torchActive}
+            />
+            <View style={styles.cameraViewContainer}>
+              <View style={styles.messageContainer}>
+                {error ? (
+                  <>
+                    <Icon style={styles.icon} name="cancel" size={40} />
+                    <Text testID={testIdWithKey('ErrorMessage')} style={styles.textStyle}>
+                      {error.message}
+                    </Text>
+                  </>
+                ) : (
+                  <>
+                    <Icon name="qrcode-scan" size={40} style={styles.icon} />
+                    <Text style={styles.textStyle}>{t('Scan.WillScanAutomatically')}</Text>
+                  </>
+                )}
+              </View>
+              <View style={styles.viewFinderContainer}>
+                <View style={styles.viewFinder} />
+              </View>
+              {showScanButton && (
+                <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                  <Pressable
+                    accessibilityLabel={t('Scan.ScanNow')}
+                    accessibilityRole={'button'}
+                    testID={testIdWithKey('ScanNow')}
+                    onPress={toggleShowInfoBox}
+                    style={styleForState}
+                    hitSlop={hitSlop}
+                  >
+                    <Icon name="circle-outline" size={60} style={{ color: 'white', marginBottom: -15 }} />
+                  </Pressable>
+                </View>
+              )}
 
-      <View style={styles.tabContainer}>
-        <ScanTab
-          title={t('Scan.ScanQRCode')}
-          iconName={'crop-free'}
-          onPress={() => setFirstTabActive(true)}
-          active={firstTabActive}
-        />
-        <ScanTab
-          title={t('Scan.MyQRCode')}
-          iconName={'qr-code'}
-          onPress={() => setFirstTabActive(false)}
-          active={!firstTabActive}
-        />
-      </View>
-    </SafeAreaView>
+              <View style={{ width: '90%', marginHorizontal: 24, height: 24, marginBottom: 60, flexDirection: 'row' }}>
+                {showScanHelp && (
+                  <Pressable
+                    accessibilityLabel={t('Scan.ScanHelp')}
+                    accessibilityRole={'button'}
+                    testID={testIdWithKey('ScanHelp')}
+                    onPress={() => navigation.navigate(Screens.ScanHelp)}
+                    style={styleForState}
+                    hitSlop={hitSlop}
+                  >
+                    <Icon name="help-circle" size={24} style={{ color: 'white' }} />
+                  </Pressable>
+                )}
+
+                <View style={{ width: 10, marginLeft: 'auto' }} />
+                <QRScannerTorch active={torchActive} onPress={() => setTorchActive(!torchActive)} />
+              </View>
+            </View>
+          </>
+        ) : (
+          <ScrollView>
+            <View style={{ alignItems: 'center' }}>
+              <View style={styles.qrContainer}>
+                {!invitation && <LoadingIndicator />}
+                {invitation && <QRRenderer value={invitation} size={qrSize} />}
+              </View>
+              <View style={{ paddingHorizontal: 20, flex: 1 }}>
+                <View
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}
+                >
+                  <Text testID={testIdWithKey('WalletName')} style={[styles.walletName, { paddingHorizontal: 20 }]}>
+                    {store.preferences.walletName}
+                  </Text>
+                  <HeaderButton
+                    buttonLocation={ButtonLocation.Right}
+                    accessibilityLabel={t('NameWallet.EditWalletName')}
+                    testID={testIdWithKey('EditWalletName')}
+                    onPress={handleEdit}
+                    icon={'pencil'}
+                    iconTintColor={styles.walletName.color}
+                  />
+                </View>
+                <Text style={styles.secondaryText}>{t('Connection.ShareQR')}</Text>
+              </View>
+            </View>
+          </ScrollView>
+        )}
+
+        <View style={styles.tabContainer}>
+          <ScanTab
+            title={t('Scan.ScanQRCode')}
+            iconName={'crop-free'}
+            onPress={() => setFirstTabActive(true)}
+            active={firstTabActive}
+          />
+          <ScanTab
+            title={t('Scan.MyQRCode')}
+            iconName={'qr-code'}
+            onPress={() => setFirstTabActive(false)}
+            active={!firstTabActive}
+          />
+        </View>
+      </SafeAreaView>
+      <SafeAreaView edges={['bottom']} style={styles.bottomSafeArea} />
+    </>
   )
 }
 
