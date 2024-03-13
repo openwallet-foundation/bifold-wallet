531f6275ca1784d284f3da0d108eeb18e4854b89
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2024-03-13 10:14:34 -0700

    fix: developer mode not turning on in onboarding, missing header (#1113)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- packages/legacy/core/App/container-impl.ts
+++ packages/legacy/core/App/container-impl.ts
@@ -6,7 +6,9 @@ import Button from './components/buttons/Button'
 import { TOKENS, Container, TokenMapping } from './container-api'
 import { DispatchAction, ReducerAction } from './contexts/reducers/store'
 import OnboardingStack from './navigators/OnboardingStack'
+import Developer from './screens/Developer'
 import Onboarding from './screens/Onboarding'
+import Preface from './screens/Preface'
 import ScreenTerms from './screens/Terms'
 import { AuthenticateStackParams, Screens } from './types/navigators'
 
@@ -19,6 +21,8 @@ export class MainContainer implements Container {
   public init(): Container {
     // eslint-disable-next-line no-console
     console.log(`Initializing Bifold container`)
+    this.container.registerInstance(TOKENS.SCREEN_PREFACE, Preface)
+    this.container.registerInstance(TOKENS.SCREEN_DEVELOPER, Developer)
     this.container.registerInstance(TOKENS.SCREEN_TERMS, ScreenTerms)
     this.container.registerInstance(TOKENS.SCREEN_ONBOARDING, Onboarding)
     this.container.registerInstance(TOKENS.STACK_ONBOARDING, OnboardingStack)
