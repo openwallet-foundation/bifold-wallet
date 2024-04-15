965ccdd69718cb56f853bf8d8394b8e1dc60cd73
Martin Auer <martin.auer97@gmail.com>
2024-04-15 13:27:17 +0200

    fix: update deps
    fix: tests
    Signed-off-by: Martin Auer <martin.auer97@gmail.com>

--- packages/legacy/core/__tests__/screens/W3cProofRequest.test.tsx
+++ packages/legacy/core/__tests__/screens/W3cProofRequest.test.tsx
@@ -141,15 +141,12 @@ describe('displays a proof request screen', () => {
         timeTravel(1000)
       })
 
-      const shareButton = tree.getByTestId(testIdWithKey('Share'))
-      const declineButton = tree.getByTestId(testIdWithKey('Decline'))
+      const cancelButton = tree.getByTestId(testIdWithKey('Cancel'))
       const recordLoading = tree.getByTestId(testIdWithKey('RecordLoading'))
 
       expect(recordLoading).not.toBeNull()
-      expect(shareButton).not.toBeNull()
-      expect(shareButton).toBeDisabled()
-      expect(declineButton).not.toBeNull()
-      expect(declineButton).not.toBeDisabled()
+      expect(cancelButton).not.toBeNull()
+      expect(cancelButton).not.toBeDisabled()
     })
 
     test('displays a proof request with all claims available', async () => {
@@ -409,12 +406,10 @@ describe('displays a proof request screen', () => {
         timeTravel(1000)
       })
 
-      const shareButton = tree.getByTestId(testIdWithKey('Share'))
-      const declineButton = tree.getByTestId(testIdWithKey('Decline'))
+      const cancelButton = tree.getByTestId(testIdWithKey('Cancel'))
 
-      expect(shareButton).not.toBeNull()
-      expect(shareButton).toBeDisabled()
-      expect(declineButton).not.toBeNull()
+      expect(cancelButton).not.toBeNull()
+      expect(cancelButton).not.toBeDisabled()
     })
 
     test('displays a proof request with one or more predicates not satisfied', async () => {
@@ -514,8 +509,7 @@ describe('displays a proof request screen', () => {
       const ageLabel = getByText(/Age/, { exact: false })
       const ageValue = getByText(t('ProofRequest.PredicateLe') + ' 18')
       const ageNotSatisfied = getByText('ProofRequest.PredicateNotSatisfied', { exact: false })
-      const shareButton = getByTestId(testIdWithKey('Share'))
-      const declineButton = getByTestId(testIdWithKey('Decline'))
+      const cancelButton = getByTestId(testIdWithKey('Cancel'))
 
       expect(predicateMessage).not.toBeNull()
       expect(predicateMessage).toBeTruthy()
@@ -531,9 +525,8 @@ describe('displays a proof request screen', () => {
       expect(ageValue).toBeTruthy()
       expect(ageNotSatisfied).not.toBeNull()
       expect(ageNotSatisfied).toBeTruthy()
-      expect(shareButton).not.toBeNull()
-      expect(shareButton).toBeDisabled()
-      expect(declineButton).not.toBeNull()
+      expect(cancelButton).not.toBeNull()
+      expect(cancelButton).not.toBeDisabled()
     })
   })
 })
