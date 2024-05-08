bec55cde6397ce4fe2e877f0fab001ddd4505a67
Jason C. Leach <jason.leach@fullboar.ca>
2024-05-08 09:10:04 -0700

    feat: use cache for oca (#1128)
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ packages/oca/jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
+  },
+}
---
+++ packages/oca/src/constants.ts
@@ -0,0 +1,7 @@
+export const ocaBundleStorageDirectory = 'oca'
+
+export const ocaCacheDataFileName = 'oca.json'
+
+export const defaultBundleIndexFileName = 'ocabundles.json'
+
+export const defaultBundleLanguage = 'en'
