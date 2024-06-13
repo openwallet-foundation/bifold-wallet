b5e4ed64bb2cd3c9c2e47f7699ec4681357748b3
Bryce McMath <bryce.j.mcmath@gmail.com>
2024-06-13 14:52:34 -0700

    chore: update tests
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- packages/legacy/core/__tests__/screens/Terms.test.tsx
+++ packages/legacy/core/__tests__/screens/Terms.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, render } from '@testing-library/react-native'
 import React from 'react'
+import { container } from 'tsyringe'
 
-import Terms from '../../App/screens/Terms'
-import { testIdWithKey } from '../../App/utils/testable'
 import { ContainerProvider } from '../../App/container-api'
 import { MainContainer } from '../../App/container-impl'
-import { container } from 'tsyringe'
-import { StoreProvider, defaultState } from '../../App/contexts/store'
 import { AuthContext } from '../../App/contexts/auth'
-import authContext from '../contexts/auth'
 import { ConfigurationContext } from '../../App/contexts/configuration'
+import { StoreProvider, defaultState } from '../../App/contexts/store'
+import Terms from '../../App/screens/Terms'
+import { testIdWithKey } from '../../App/utils/testable'
+import authContext from '../contexts/auth'
 import configurationContext from '../contexts/configuration'
 
 jest.mock('@react-navigation/core', () => {
@@ -23,30 +23,37 @@ jest.mock('@hyperledger/anoncreds-react-native', () => ({}))
 jest.mock('@hyperledger/aries-askar-react-native', () => ({}))
 jest.mock('@hyperledger/indy-vdr-react-native', () => ({}))
 
-
 describe('Terms Screen', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
   test('Renders correctly', async () => {
     const main = new MainContainer(container.createChildContainer()).init()
-    const tree = render(<ContainerProvider value={main}>
-              <StoreProvider
+    const tree = render(
+      <ContainerProvider value={main}>
+        <StoreProvider
           initialState={{
             ...defaultState,
           }}
         >
           <ConfigurationContext.Provider value={configurationContext}>
-          <AuthContext.Provider value={authContext}>
-      <Terms />
-      </AuthContext.Provider>
-      </ConfigurationContext.Provider></StoreProvider></ContainerProvider>)
+            <AuthContext.Provider value={authContext}>
+              <Terms />
+            </AuthContext.Provider>
+          </ConfigurationContext.Provider>
+        </StoreProvider>
+      </ContainerProvider>
+    )
     expect(tree).toMatchSnapshot()
   })
 
   test('Button enabled by checkbox being checked', async () => {
     const main = new MainContainer(container.createChildContainer()).init()
-    const tree = render(<ContainerProvider value={main}><Terms /></ContainerProvider>)
+    const tree = render(
+      <ContainerProvider value={main}>
+        <Terms />
+      </ContainerProvider>
+    )
     const { getByTestId } = tree
     const checkbox = getByTestId(testIdWithKey('IAgree'))
     fireEvent(checkbox, 'press')
