08557bf73ffe20cbe6fe0dd5c667f922c8a97d60
Jason C. Leach <jason.leach@fullboar.ca>
2024-06-27 11:39:23 -0700

    fix: cleanup
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/legacy/core/App/container-impl.ts
+++ packages/legacy/core/App/container-impl.ts
@@ -54,7 +54,7 @@ export class MainContainer implements Container {
     this._container.registerInstance(TOKENS.COMP_BUTTON, Button)
     this._container.registerInstance(TOKENS.GROUP_BY_REFERENT, false)
     this._container.registerInstance(TOKENS.CRED_HELP_ACTION_OVERRIDES, [])
-    this._container.registerInstance(TOKENS.OBJECT_ONBOARDINGCONFIG, DefaultScreenOptionsDictionary)
+    this._container.registerInstance(TOKENS.OBJECT_ONBOARDING_CONFIG, DefaultScreenOptionsDictionary)
     this._container.registerInstance(TOKENS.UTIL_LOGGER, new ConsoleLogger())
     this._container.registerInstance(TOKENS.UTIL_OCA_RESOLVER, new DefaultOCABundleResolver(bundle))
     this._container.registerInstance(TOKENS.UTIL_LEDGERS, defaultIndyLedgers)
