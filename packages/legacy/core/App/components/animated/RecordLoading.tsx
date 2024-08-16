af7582d9a882002e4d581878e1d8e68f8a53fc61
Jason C. Leach <jason.leach@fullboar.ca>
2024-08-16 09:34:49 -0700

    feat: work on loading animation
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/legacy/core/App/components/animated/RecordLoading.tsx
+++ packages/legacy/core/App/components/animated/RecordLoading.tsx
@@ -4,52 +4,96 @@ import { View, StyleSheet, Animated } from 'react-native'
 import { useTheme } from '../../contexts/theme'
 import { testIdWithKey } from '../../utils/testable'
 
-const fadeTiming: Animated.TimingAnimationConfig = {
-  toValue: 0.2,
-  duration: 1100,
-  useNativeDriver: true,
-}
-
 const RecordLoading: React.FC = () => {
   const { ColorPallet } = useTheme()
-  const rowFadeAnim = useRef(new Animated.Value(1))
+  const rowFadeAnim = useRef(new Animated.Value(1)).current
+  const fadeTiming: Animated.TimingAnimationConfig = {
+    toValue: 0.4,
+    duration: 1100,
+    useNativeDriver: true,
+  }
   const style = StyleSheet.create({
     container: {
       flexDirection: 'column',
     },
     rectangle: {
-      backgroundColor: ColorPallet.grayscale.veryLightGrey,
-      height: 30,
-      marginVertical: 10,
-    },
-    line: {
       backgroundColor: ColorPallet.grayscale.lightGrey,
-      height: 1,
+      height: 30,
       marginVertical: 5,
+      borderRadius: 10,
     },
   })
 
   useEffect(() => {
-    Animated.loop(Animated.timing(rowFadeAnim.current, fadeTiming)).start()
-  }, [])
+    Animated.loop(Animated.timing(rowFadeAnim, fadeTiming)).start()
+  }, [rowFadeAnim, fadeTiming])
 
-  const makeARow = () => {
-    return (
-      <View style={{ flexDirection: 'column' }}>
-        <View style={[style.rectangle, { width: '35%' }]}></View>
-        <View style={[style.rectangle, { width: '85%' }]}></View>
-        <View style={style.line} />
-      </View>
-    )
-  }
+  // const skeletonLayout = () => {
+  //   return (
+  //     <View style={{ flexDirection: 'row' }}>
+  //       <View
+  //         style={{
+  //           marginLeft: 0,
+  //           backgroundColor: ColorPallet.grayscale.lightGrey,
+  //           width: 60,
+  //           borderTopLeftRadius: 15,
+  //           borderBottomLeftRadius: 15,
+  //         }}
+  //       />
+  //       <View
+  //         style={{
+  //           marginLeft: -22,
+  //           marginTop: 20,
+  //           backgroundColor: ColorPallet.grayscale.veryLightGrey,
+  //           height: 45,
+  //           width: 45,
+  //           borderRadius: 5,
+  //         }}
+  //       />
+  //       <View style={{ marginLeft: 15, marginTop: 15, marginBottom: 15 }}>
+  //         <View style={[style.rectangle, { width: 240, height: 20 }]} />
+  //         <View style={[style.rectangle, { width: 145, height: 25 }]} />
+  //         <View style={[style.rectangle, { width: 75, height: 20, marginTop: 20 }]} />
+  //         <View style={[style.rectangle, { width: 210, height: 25 }]} />
+  //       </View>
+  //     </View>
+  //   )
+  // }
+  // ColorPallet.brand.secondaryBackground
 
   return (
-    <View style={style.container} testID={testIdWithKey('RecordLoading')}>
-      <Animated.View style={{ opacity: rowFadeAnim.current }}>
-        {makeARow()}
-        {makeARow()}
-      </Animated.View>
-    </View>
+    <Animated.View style={[{ opacity: rowFadeAnim, backgroundColor: ColorPallet.grayscale.white, borderRadius: 15 }]}>
+      <View style={[style.container]} testID={testIdWithKey('RecordLoading')}>
+        <View style={{ flexDirection: 'row' }}>
+          <View
+            style={{
+              marginLeft: 0,
+              backgroundColor: ColorPallet.grayscale.lightGrey,
+              width: 60,
+              borderTopLeftRadius: 15,
+              borderBottomLeftRadius: 15,
+              // overflow: 'hidden',
+            }}
+          />
+          <View
+            style={{
+              marginLeft: -22,
+              marginTop: 20,
+              backgroundColor: ColorPallet.grayscale.lightGrey,
+              height: 45,
+              width: 45,
+              borderRadius: 5,
+            }}
+          />
+          <View style={{ marginLeft: 15, marginTop: 15, marginBottom: 15 }}>
+            <View style={[style.rectangle, { width: 240, height: 20 }]} />
+            <View style={[style.rectangle, { width: 145, height: 25 }]} />
+            <View style={[style.rectangle, { width: 75, height: 20, marginTop: 20 }]} />
+            <View style={[style.rectangle, { width: 210, height: 25 }]} />
+          </View>
+        </View>
+      </View>
+    </Animated.View>
   )
 }
 
