88718a21eab008001a37f0d66fb0036031dda62b
Jason C. Leach <jason.leach@fullboar.ca>
2024-08-22 11:39:04 -0700

    feat: work on loading placeholder
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/legacy/core/App/components/views/LoadingPlaceholder.tsx
+++ packages/legacy/core/App/components/views/LoadingPlaceholder.tsx
@@ -1,8 +1,7 @@
-import React, { useEffect, useMemo, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 
 import Button, { ButtonType } from '../../components/buttons/Button'
-import ConnectionAlert from '../../components/misc/ConnectionAlert'
 import { testIdWithKey } from '../../utils/testable'
 import { useTheme } from '../../contexts/theme'
 import { useTranslation } from 'react-i18next'
@@ -21,6 +20,29 @@ const LoadingPlaceholder: React.FC<LoadingPlaceholderProps> = ({ timeoutDuration
   const { RecordLoading } = useAnimatedComponents()
   const timerRef = useRef<NodeJS.Timeout | null>(null)
   const [overtime, setOvertime] = useState(false)
+  const styles = StyleSheet.create({
+    buttonContainer: {
+      marginVertical: 25,
+    },
+
+    link: {
+      ...ListItems.recordAttributeText,
+      ...ListItems.recordLink,
+      paddingVertical: 2,
+    },
+    loadingAnimationContainer: {
+      flex: 1,
+      flexGrow: 1,
+      borderRadius: 15,
+    },
+    noNewUpdatesText: {
+      ...HomeTheme.noNewUpdatesText,
+      flexWrap: 'wrap',
+    },
+    infoTextBoxContainer: {
+      flexShrink: 1,
+    },
+  })
 
   useEffect(() => {
     if (timeoutDurationInMs === 0) {
@@ -42,100 +64,46 @@ const LoadingPlaceholder: React.FC<LoadingPlaceholderProps> = ({ timeoutDuration
     }
   }, [timeoutDurationInMs])
 
-  const styles = StyleSheet.create({
-    pageContainer: {
-      flex: 1,
-    },
-    pageContent: {
-      flexGrow: 1,
-      justifyContent: 'space-between',
-    },
-    pageMargin: {
-      marginHorizontal: 20,
-    },
-    pageFooter: {
-      marginVertical: 15,
-    },
-    headerTextContainer: {
-      paddingVertical: 16,
-    },
-    headerText: {
-      ...ListItems.recordAttributeText,
-      flexShrink: 1,
-    },
-    footerButton: {
-      paddingTop: 10,
-    },
-    link: {
-      ...ListItems.recordAttributeText,
-      ...ListItems.recordLink,
-      paddingVertical: 2,
-    },
-    valueContainer: {
-      minHeight: ListItems.recordAttributeText.fontSize,
-      paddingVertical: 4,
-    },
-    detailsButton: {
-      ...ListItems.recordAttributeText,
-      color: ColorPallet.brand.link,
-      textDecorationLine: 'underline',
-    },
-    cardLoading: {
-      flex: 1,
-      flexGrow: 1,
-      borderRadius: 15,
-    },
-    noNewUpdatesText: {
-      ...HomeTheme.noNewUpdatesText,
-      flexWrap: 'wrap',
-    },
-  })
-
   return (
     <View>
       <Text>{'Some key step is going on'}</Text>
-      <View style={styles.pageMargin}>
+      <View>
         {overtime && (
-          <View style={{ paddingTop: 20, overflow: 'hidden' }}>
-            <InfoTextBox type={InfoBoxType.Info}>
-              <View style={{ backgroundColor: 'yellow' }}>
+          <>
+            <InfoTextBox type={InfoBoxType.Info} style={{ marginTop: 20 }}>
+              <View style={styles.infoTextBoxContainer}>
                 <Text
                   style={[styles.noNewUpdatesText, { fontWeight: 'bold', marginBottom: 20 }]}
                   testID={testIdWithKey('NoNewUpdates')}
                 >
                   {'This is slower than usual'}
                 </Text>
-                <Text
-                  style={[styles.noNewUpdatesText, { backgroundColor: 'red' }]}
-                  testID={testIdWithKey('NoNewUpdates')}
-                >
+                <Text style={[styles.noNewUpdatesText]} testID={testIdWithKey('NoNewUpdates')}>
                   {'Check your internet connection and try again. '}
                 </Text>
               </View>
             </InfoTextBox>
-          </View>
+          </>
         )}
 
         <Text style={[styles.noNewUpdatesText, { marginTop: 25, marginBottom: 10 }]}>
           {"You'll be requested to share the following information."}
         </Text>
 
-        <View style={styles.cardLoading}>
+        <View style={styles.loadingAnimationContainer}>
           <RecordLoading />
           <RecordLoading style={{ marginTop: 10 }} />
         </View>
       </View>
 
-      <View style={[styles.pageFooter, styles.pageMargin]}>
-        <View style={styles.footerButton}>
-          <Button
-            title={t('Global.Cancel')}
-            accessibilityLabel={t('Global.Cancel')}
-            testID={testIdWithKey('Cancel')}
-            buttonType={ButtonType.Primary}
-            onPress={onCancelTouched}
-          />
-        </View>
+      <View style={[styles.buttonContainer]}>
+        <Button
+          title={t('Global.Cancel')}
+          accessibilityLabel={t('Global.Cancel')}
+          testID={testIdWithKey('Cancel')}
+          buttonType={ButtonType.Primary}
+          onPress={onCancelTouched}
+        />
       </View>
     </View>
   )
