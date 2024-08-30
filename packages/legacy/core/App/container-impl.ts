4d96e658d5720fa53fc59fa002f1b179a5da6d99
Mostafa Gamal <moscd3@gmail.com>
2024-08-29 22:25:39 -0400

    Make history flag injectable
    Signed-off-by: Mostafa Gamal <moscd3@gmail.com>

--- packages/legacy/core/App/container-impl.ts
+++ packages/legacy/core/App/container-impl.ts
@@ -82,6 +82,7 @@ export class MainContainer implements Container {
     this._container.registerInstance(TOKENS.STACK_ONBOARDING, OnboardingStack)
     this._container.registerInstance(TOKENS.COMP_BUTTON, Button)
     this._container.registerInstance(TOKENS.GROUP_BY_REFERENT, false)
+    this._container.registerInstance(TOKENS.HISTORY_ENABLED, false)
     this._container.registerInstance(TOKENS.CRED_HELP_ACTION_OVERRIDES, [])
     this._container.registerInstance(TOKENS.OBJECT_ONBOARDING_CONFIG, DefaultScreenOptionsDictionary)
     this._container.registerInstance(TOKENS.UTIL_LOGGER, new ConsoleLogger())
