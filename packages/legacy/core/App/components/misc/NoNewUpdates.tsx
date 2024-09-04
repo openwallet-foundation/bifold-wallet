f0c77bbf638000195850d0b51900a1b90037459c
fc-santos <filipesantos@live.ca>
2024-09-04 10:07:45 -0400

    feat: make empty notifications list component injectable
    Signed-off-by: fc-santos <filipesantos@live.ca>

--- packages/legacy/core/App/components/misc/NoNewUpdates.tsx
+++ packages/legacy/core/App/components/misc/NoNewUpdates.tsx
@@ -5,11 +5,17 @@ import { StyleSheet, Text } from 'react-native'
 import { useTheme } from '../../contexts/theme'
 import { testIdWithKey } from '../../utils/testable'
 import InfoTextBox from '../texts/InfoTextBox'
+import { View } from 'react-native'
 
 const NoNewUpdates: React.FC = () => {
   const { t } = useTranslation()
-  const { HomeTheme } = useTheme()
+  const { HomeTheme, ColorPallet } = useTheme()
   const styles = StyleSheet.create({
+    noNewUpdatesContainer: {
+      paddingHorizontal: 20,
+      paddingVertical: 20,
+      backgroundColor: ColorPallet.brand.secondaryBackground,
+    },
     noNewUpdatesText: {
       ...HomeTheme.noNewUpdatesText,
       alignSelf: 'center',
@@ -19,11 +25,13 @@ const NoNewUpdates: React.FC = () => {
   })
 
   return (
-    <InfoTextBox>
-      <Text style={styles.noNewUpdatesText} testID={testIdWithKey('NoNewUpdates')}>
-        {t('Home.NoNewUpdates')}
-      </Text>
-    </InfoTextBox>
+    <View style={styles.noNewUpdatesContainer}>
+      <InfoTextBox>
+        <Text style={styles.noNewUpdatesText} testID={testIdWithKey('NoNewUpdates')}>
+          {t('Home.NoNewUpdates')}
+        </Text>
+      </InfoTextBox>
+    </View>
   )
 }
 
