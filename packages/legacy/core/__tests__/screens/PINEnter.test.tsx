c720d3d071aee3093ee1aaa90a65785de9d4e0ab
Bryce McMath <bryce.j.mcmath@gmail.com>
2024-09-09 16:44:16 -0700

    fix: double biometrics
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- packages/legacy/core/__tests__/screens/PINEnter.test.tsx
+++ packages/legacy/core/__tests__/screens/PINEnter.test.tsx
@@ -1,6 +1,9 @@
 import { render } from '@testing-library/react-native'
 import React from 'react'
+import { container } from 'tsyringe'
 
+import { ContainerProvider } from '../../App/container-api'
+import { MainContainer } from '../../App/container-impl'
 import { AuthContext } from '../../App/contexts/auth'
 import { StoreProvider, defaultState } from '../../App/contexts/store'
 import PINEnter from '../../App/screens/PINEnter'
@@ -9,34 +12,40 @@ import authContext from '../contexts/auth'
 
 describe('displays a PIN Enter screen', () => {
   test('PIN Enter renders correctly', () => {
+    const main = new MainContainer(container.createChildContainer()).init()
     const tree = render(
-      <StoreProvider
-        initialState={{
-          ...defaultState,
-        }}
-      >
-        <AuthContext.Provider value={authContext}>
-          <PINEnter setAuthenticated={jest.fn()} />
-        </AuthContext.Provider>
-      </StoreProvider>
+      <ContainerProvider value={main}>
+        <StoreProvider
+          initialState={{
+            ...defaultState,
+          }}
+        >
+          <AuthContext.Provider value={authContext}>
+            <PINEnter setAuthenticated={jest.fn()} />
+          </AuthContext.Provider>
+        </StoreProvider>
+      </ContainerProvider>
     )
     expect(tree).toMatchSnapshot()
   })
 
   test('PIN Enter renders correctly when logged out message is present', async () => {
+    const main = new MainContainer(container.createChildContainer()).init()
     const tree = render(
-      <StoreProvider
-        initialState={{
-          ...defaultState,
-          lockout: {
-            displayNotification: true,
-          },
-        }}
-      >
-        <AuthContext.Provider value={authContext}>
-          <PINEnter setAuthenticated={jest.fn()} />
-        </AuthContext.Provider>
-      </StoreProvider>
+      <ContainerProvider value={main}>
+        <StoreProvider
+          initialState={{
+            ...defaultState,
+            lockout: {
+              displayNotification: true,
+            },
+          }}
+        >
+          <AuthContext.Provider value={authContext}>
+            <PINEnter setAuthenticated={jest.fn()} />
+          </AuthContext.Provider>
+        </StoreProvider>
+      </ContainerProvider>
     )
     const textNotice = await tree.findByText('PINEnter.LockedOut')
     expect(textNotice).not.toBeNull()
@@ -44,16 +53,19 @@ describe('displays a PIN Enter screen', () => {
   })
 
   test('PIN Enter button exists', async () => {
+    const main = new MainContainer(container.createChildContainer()).init()
     const tree = render(
-      <StoreProvider
-        initialState={{
-          ...defaultState,
-        }}
-      >
-        <AuthContext.Provider value={authContext}>
-          <PINEnter setAuthenticated={jest.fn()} />
-        </AuthContext.Provider>
-      </StoreProvider>
+      <ContainerProvider value={main}>
+        <StoreProvider
+          initialState={{
+            ...defaultState,
+          }}
+        >
+          <AuthContext.Provider value={authContext}>
+            <PINEnter setAuthenticated={jest.fn()} />
+          </AuthContext.Provider>
+        </StoreProvider>
+      </ContainerProvider>
     )
     const EnterButton = await tree.getByTestId(testIdWithKey('Enter'))
     expect(EnterButton).not.toBeNull()
