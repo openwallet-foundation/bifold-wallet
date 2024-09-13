f1c8a3f983103289f8cabef9d7148558ed61e1bd
Jason C. Leach <jason.leach@fullboar.ca>
2024-09-13 13:00:16 -0700

    fix: manage agent state (#1258)
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/legacy/core/App/screens/Splash.tsx
+++ packages/legacy/core/App/screens/Splash.tsx
@@ -87,7 +87,7 @@ const resumeOnboardingAt = (
  * of this view.
  */
 const Splash: React.FC = () => {
-  const { setAgent } = useAgent()
+  const { agent, setAgent } = useAgent()
   const { t } = useTranslation()
   const [store, dispatch] = useStore()
   const navigation = useNavigation()
@@ -96,24 +96,24 @@ const Splash: React.FC = () => {
   const { LoadingIndicator } = useAnimatedComponents()
   const [mounted, setMounted] = useState(false)
   const [
-    cacheSchemas, 
-    cacheCredDefs, 
-    { version: TermsVersion }, 
-    logger, 
-    indyLedgers, 
-    { showPreface, enablePushNotifications }, 
+    cacheSchemas,
+    cacheCredDefs,
+    { version: TermsVersion },
+    logger,
+    indyLedgers,
+    { showPreface, enablePushNotifications },
     ocaBundleResolver,
     historyEnabled,
-  ] = useServices(
-    [TOKENS.CACHE_SCHEMAS, 
-    TOKENS.CACHE_CRED_DEFS, 
-    TOKENS.SCREEN_TERMS, 
-    TOKENS.UTIL_LOGGER, 
-    TOKENS.UTIL_LEDGERS, 
-    TOKENS.CONFIG, 
-    TOKENS.UTIL_OCA_RESOLVER, 
-    TOKENS.HISTORY_ENABLED]
-  )
+  ] = useServices([
+    TOKENS.CACHE_SCHEMAS,
+    TOKENS.CACHE_CRED_DEFS,
+    TOKENS.SCREEN_TERMS,
+    TOKENS.UTIL_LOGGER,
+    TOKENS.UTIL_LEDGERS,
+    TOKENS.CONFIG,
+    TOKENS.UTIL_OCA_RESOLVER,
+    TOKENS.HISTORY_ENABLED,
+  ])
   const styles = StyleSheet.create({
     container: {
       flex: 1,
@@ -232,6 +232,41 @@ const Splash: React.FC = () => {
 
         await (ocaBundleResolver as RemoteOCABundleResolver).checkForUpdates?.()
 
+        if (agent) {
+          logger.info('Agent already initialized, restarting...')
+
+          try {
+            await agent.wallet.open({
+              id: walletSecret.id,
+              key: walletSecret.key,
+            })
+
+            logger.info('Opened agent wallet')
+          } catch (error: unknown) {
+            logger.error('Error opening existing wallet', error as BifoldError)
+
+            throw new BifoldError(
+              'Wallet Service',
+              'There was a problem unlocking the wallet.',
+              (error as Error).message,
+              1047
+            )
+          }
+
+          await agent.mediationRecipient.initiateMessagePickup()
+
+          navigation.dispatch(
+            CommonActions.reset({
+              index: 0,
+              routes: [{ name: Stacks.TabStack }],
+            })
+          )
+
+          return
+        }
+
+        logger.info('No agent initialized, creating a new one')
+
         const newAgent = new Agent({
           config: {
             label: store.preferences.walletName || 'Aries Bifold',
@@ -310,7 +345,7 @@ const Splash: React.FC = () => {
     initAgent()
   }, [mounted, store.authentication.didAuthenticate, store.onboarding.didConsiderBiometry, walletSecret])
 
-  useEffect(()=>{
+  useEffect(() => {
     if (!mounted || !historyEnabled) {
       return
     }
@@ -318,7 +353,7 @@ const Splash: React.FC = () => {
       type: DispatchAction.HISTORY_CAPABILITY,
       payload: [true],
     })
-  },[mounted, historyEnabled])
+  }, [mounted, historyEnabled])
 
   return (
     <SafeAreaView style={styles.container}>
