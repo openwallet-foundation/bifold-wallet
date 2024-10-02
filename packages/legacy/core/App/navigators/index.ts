2b6fdf26231f9cea42a069285ba2b9afb6164a53
Filipe Santos <48060475+fc-santos@users.noreply.github.com>
2024-10-02 15:06:15 -0400

    chore: expose navigators stacks (#1269)
    Signed-off-by: fc-santos <filipesantos@live.ca>

---
+++ packages/legacy/core/App/navigators/index.ts
@@ -0,0 +1,27 @@
+import ConnectStack from './ConnectStack'
+import ContactStack from './ContactStack'
+import CredentialStack from './CredentialStack'
+import DeliveryStack from './DeliveryStack'
+import HomeStack from './HomeStack'
+import NotificationStack from './NotificationStack'
+import OnboardingStack from './OnboardingStack'
+import ProofRequestStack from './ProofRequestStack'
+import RootStack from './RootStack'
+import SettingStack from './SettingStack'
+import TabStack from './TabStack'
+import { useDefaultStackOptions } from './defaultStackOptions'
+
+export {
+  ConnectStack,
+  ContactStack,
+  CredentialStack,
+  DeliveryStack,
+  HomeStack,
+  NotificationStack,
+  OnboardingStack,
+  ProofRequestStack,
+  RootStack,
+  SettingStack,
+  TabStack,
+  useDefaultStackOptions,
+}
