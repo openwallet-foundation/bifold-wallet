0714887ec800ad6b411b3af69ba0e4597f84d41d
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2024-11-12 14:19:09 -0800

    fix: add check for reusable conn to oob hook (#1305)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    
    Signed-off-by: Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>

--- packages/legacy/core/App/hooks/connections.ts
+++ packages/legacy/core/App/hooks/connections.ts
@@ -2,15 +2,6 @@ import { ConnectionRecord, OutOfBandRecord } from '@credo-ts/core'
 import { useAgent, useConnectionById, useConnections } from '@credo-ts/react-hooks'
 import { useMemo, useState } from 'react'
 
-export const useConnectionByOutOfBandId = (outOfBandId: string): ConnectionRecord | undefined => {
-  const { records: connections } = useConnections()
-
-  return useMemo(
-    () => connections.find((connection: ConnectionRecord) => connection.outOfBandId === outOfBandId),
-    [connections, outOfBandId]
-  )
-}
-
 export const useOutOfBandById = (oobId: string): OutOfBandRecord | undefined => {
   const { agent } = useAgent()
   const [oob, setOob] = useState<OutOfBandRecord | undefined>(undefined)
@@ -22,6 +13,20 @@ export const useOutOfBandById = (oobId: string): OutOfBandRecord | undefined => 
     })
   }
   return oob
+}
+
+export const useConnectionByOutOfBandId = (outOfBandId: string): ConnectionRecord | undefined => {
+  const reuseConnectionId = useOutOfBandById(outOfBandId)?.reuseConnectionId
+  const { records: connections } = useConnections()
+
+  return useMemo(
+    () => connections.find((connection: ConnectionRecord) => (
+      connection.outOfBandId === outOfBandId ||
+      // Check for a reusable connection
+      (reuseConnectionId && connection.id === reuseConnectionId)
+    )),
+    [connections, outOfBandId, reuseConnectionId]
+  )
 }
 
 export const useOutOfBandByConnectionId = (connectionId: string): OutOfBandRecord | undefined => {
