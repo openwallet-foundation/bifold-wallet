01c6db3c8bf7317ae2c20c7b410f10fa1dc33e0e
FarjadGov14 <99842359+FarjadGov14@users.noreply.github.com>
2024-11-15 15:42:51 -0500

    feat: move the pin rules below the pin input (#1316)
    Signed-off-by: FarjadGov14 <syedmuhammadfarjad.abbas@ontario.ca>
    
    Signed-off-by: FarjadGov14 <99842359+FarjadGov14@users.noreply.github.com>

--- packages/legacy/core/App/screens/PINCreate.tsx
+++ packages/legacy/core/App/screens/PINCreate.tsx
@@ -281,24 +281,6 @@ const PINCreate: React.FC<PINCreateProps> = ({ setAuthenticated, explainedStatus
             autoFocus={false}
             inlineMessage={inlineMessageField1}
           />
-          {PINSecurity.displayHelper && (
-            <View style={{ marginBottom: 16 }}>
-              {PINOneValidations.map((validation, index) => {
-                return (
-                  <View style={{ flexDirection: 'row' }} key={index}>
-                    {validation.isInvalid ? (
-                      <Icon name="clear" size={iconSize} color={ColorPallet.notification.errorIcon} />
-                    ) : (
-                      <Icon name="check" size={iconSize} color={ColorPallet.notification.successIcon} />
-                    )}
-                    <Text style={[TextTheme.normal, { paddingLeft: 4 }]}>
-                      {t(`PINCreate.Helper.${validation.errorName}`)}
-                    </Text>
-                  </View>
-                )
-              })}
-            </View>
-          )}
           <PINInput
             label={t('PINCreate.ReenterPIN', { new: updatePin ? t('PINCreate.NewPIN') + ' ' : '' })}
             onPINChanged={(p: string) => {
@@ -321,6 +303,24 @@ const PINCreate: React.FC<PINCreateProps> = ({ setAuthenticated, explainedStatus
             ref={PINTwoInputRef}
             inlineMessage={inlineMessageField2}
           />
+          {PINSecurity.displayHelper && (
+            <View style={{ marginBottom: 16 }}>
+              {PINOneValidations.map((validation, index) => {
+                return (
+                  <View style={{ flexDirection: 'row' }} key={index}>
+                    {validation.isInvalid ? (
+                      <Icon name="clear" size={iconSize} color={ColorPallet.notification.errorIcon} />
+                    ) : (
+                      <Icon name="check" size={iconSize} color={ColorPallet.notification.successIcon} />
+                    )}
+                    <Text style={[TextTheme.normal, { paddingLeft: 4 }]}>
+                      {t(`PINCreate.Helper.${validation.errorName}`)}
+                    </Text>
+                  </View>
+                )
+              })}
+            </View>
+          )}
           {modalState.visible && (
             <AlertModal
               title={modalState.title}
