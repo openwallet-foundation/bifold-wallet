ef5ff392986197e6423a0e306345e74ecf4a8298
fc-santos <filipesantos@live.ca>
2024-11-20 13:57:34 -0500

    feat: update RecordLoading skeleton to display the new branding
    Signed-off-by: fc-santos <filipesantos@live.ca>

--- packages/legacy/core/App/components/animated/RecordLoading.tsx
+++ packages/legacy/core/App/components/animated/RecordLoading.tsx
@@ -3,6 +3,8 @@ import { StyleProp, ViewStyle, View, StyleSheet, Animated, useWindowDimensions }
 
 import { useTheme } from '../../contexts/theme'
 import { testIdWithKey } from '../../utils/testable'
+import { TOKENS, useServices } from '../../container-api'
+import { BrandingOverlayType } from '@hyperledger/aries-oca/build/legacy'
 
 type RecordLoadingProps = {
   style?: StyleProp<ViewStyle>
@@ -21,6 +23,7 @@ const RecordLoading: React.FC<RecordLoadingProps> = ({ style }) => {
   const padding = width * 0.05
   const logoHeight = width * 0.12
   const { ColorPallet } = useTheme()
+  const [bundleResolver] = useServices([TOKENS.UTIL_OCA_RESOLVER])
   const rowFadeAnim = useRef(new Animated.Value(1))
   const myStyle = StyleSheet.create({
     container: {
@@ -39,12 +42,21 @@ const RecordLoading: React.FC<RecordLoadingProps> = ({ style }) => {
       borderBottomLeftRadius: borderRadius,
     },
     logo: {
-      marginLeft: -1 * logoHeight + padding,
-      marginTop: padding,
       backgroundColor: ColorPallet.grayscale.lightGrey,
       height: logoHeight,
       width: logoHeight,
       borderRadius,
+    },
+    logoFloating: {
+      marginLeft: -1 * logoHeight + padding,
+      marginTop: padding,
+    },
+    logoWithIssuerContainer: {
+      flexDirection: 'row',
+      maxWidth: '70%',
+      gap: 4,
+      alignSelf: 'flex-end',
+      marginTop: 15,
     },
   })
 
@@ -59,12 +71,20 @@ const RecordLoading: React.FC<RecordLoadingProps> = ({ style }) => {
       <View style={myStyle.container} testID={testIdWithKey('RecordLoading')}>
         <View style={{ flexDirection: 'row' }}>
           <View style={myStyle.margin} />
-          <View style={myStyle.logo} />
-          <View style={{ flexGrow: 1, marginLeft: 15, marginTop: 15, marginBottom: 15 }}>
+          {bundleResolver.getBrandingOverlayType() !== BrandingOverlayType.Branding11 && (
+            <View style={[myStyle.logo, myStyle.logoFloating]} />
+          )}
+          <View style={{ flexGrow: 1, margin: 15 }}>
             <View style={[myStyle.rectangle, { width: '100%', height: 20 }]} />
             <View style={[myStyle.rectangle, { width: '75%', height: 25 }]} />
             <View style={[myStyle.rectangle, { width: '35%', height: 20, marginTop: 20 }]} />
             <View style={[myStyle.rectangle, { width: '90%', height: 25 }]} />
+            {bundleResolver.getBrandingOverlayType() === BrandingOverlayType.Branding11 && (
+              <View style={myStyle.logoWithIssuerContainer}>
+                <View style={myStyle.logo} />
+                <View style={[myStyle.rectangle, { width: '75%', height: logoHeight, marginVertical: 0 }]} />
+              </View>
+            )}
           </View>
         </View>
       </View>
