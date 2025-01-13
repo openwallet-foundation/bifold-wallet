a7d810ca32f7eb2c00de928f3d7a0248a4252f68
Jian Wang <Jian.Wang2@ontario.ca>
2025-01-13 14:26:13 -0500

    Restore the checking logic, wallet check isConnected as before when disableFirewallCheck not assert. But skip mediator connecting when it is disabled and check isInternetReachable instead
    Signed-off-by: Jian Wang <Jian.Wang2@ontario.ca>

--- packages/legacy/core/App/components/network/NetInfo.tsx
+++ packages/legacy/core/App/components/network/NetInfo.tsx
@@ -7,46 +7,51 @@ import { ToastType } from '../../components/toast/BaseToast'
 import { TOKENS, useServices } from '../../container-api'
 
 const NetInfo: React.FC = () => {
-  const { assertInternetReachable, assertMediatorReachable } = useNetwork()
+  const { silentAssertConnectedNetwork, assertInternetReachable, assertMediatorReachable } = useNetwork()
   const [{ disableFirewallCheck }] = useServices([TOKENS.CONFIG])
   const { t } = useTranslation()
   const [hasShown, setHasShown] = useState(false)
 
-  const isInternetReachable = true
+  const isInternetReachable = assertInternetReachable();
+  const isConnected = silentAssertConnectedNetwork()
   
   useEffect(() => {
     // Network is available, do further testing according to CFG.disableFirewallCheck
-    if (isInternetReachable) {
-      if (disableFirewallCheck) {
-        return
-      } else {
-        // Assert that internet is available by try connecting the socket of configured mediator
+    if (!disableFirewallCheck) {
+      // Network is connected
+      if (isConnected) {
+        // Assert that internet is available
         assertMediatorReachable().then((status) => {
           // Connected to a network, reset toast
           setHasShown(false)
           if (status) {
             return
           }
-
-          // User is connected to a network but has no internet, e.g. firewall problem, display the toast
+  
+          // User is connected to a network but has no internet, display toast
           Toast.show({
             type: ToastType.Error,
             autoHide: true,
-            text1: t('NetInfo.NoInternetConnectionTitle'),
+            text1: t('NetInfo.NoInternetConnectionMessage'),
           })
         })
         return
       }
-    } else if (!hasShown) {
-      // Only show the no-network toast if the user hasn't seen it before
+    } else if (isInternetReachable) {
+      return
+    }
+
+    // Only show the toast if the user hasn't seen it already
+    if (!hasShown) {
       setHasShown(true)
       Toast.show({
         type: ToastType.Error,
         autoHide: true,
-        text1: t('NetInfo.NoInternetConnectionTitle'),
+        text1: t('NetInfo.NoInternetConnectionMessage'),
       })
     }
-  }, [isInternetReachable, disableFirewallCheck, assertInternetReachable, assertMediatorReachable, t, hasShown])
+
+  }, [isConnected, isInternetReachable, disableFirewallCheck, assertInternetReachable, assertMediatorReachable, t, hasShown])
 
   return null
 }
