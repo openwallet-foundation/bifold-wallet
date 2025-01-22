c1f75ca2227f6e36150561428939407cd42ade81
ZouidiSamih <Samih.Zouidi3-ext@mcn.gouv.qc.ca>
2025-01-22 13:29:37 -0500

    Remove unnecessary type assertion
    Signed-off-by: ZouidiSamih <Samih.Zouidi3-ext@mcn.gouv.qc.ca>

--- packages/legacy/core/App/container-impl.ts
+++ packages/legacy/core/App/container-impl.ts
@@ -143,7 +143,7 @@ export class MainContainer implements Container {
     this._container.registerInstance(TOKENS.COMPONENT_RECORD, Record)
     this._container.registerInstance(TOKENS.COMPONENT_CONTACT_LIST_ITEM, ContactListItem)
     this._container.registerInstance(TOKENS.COMPONENT_CONTACT_DETAILS_CRED_LIST_ITEM, ContactCredentialListItem)
-    this._container.registerInstance(TOKENS.COMPONENT_CONNECTION_ALERT as any, ConnectionAlert)
+    this._container.registerInstance(TOKENS.COMPONENT_CONNECTION_ALERT, ConnectionAlert)
     this._container.registerInstance(TOKENS.CACHE_CRED_DEFS, [])
     this._container.registerInstance(TOKENS.CACHE_SCHEMAS, [])
     this._container.registerInstance(TOKENS.INLINE_ERRORS, {
