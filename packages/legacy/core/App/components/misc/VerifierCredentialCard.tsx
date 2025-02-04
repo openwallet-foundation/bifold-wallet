7eee3183acadc0eafce21b2850bfa83b06681c9d
fc-santos <filipesantos@live.ca>
2025-02-04 13:38:56 -0500

    fix: add credential card shadow for branding 1.1 in verifier credential card
    Signed-off-by: fc-santos <filipesantos@live.ca>

--- packages/legacy/core/App/components/misc/VerifierCredentialCard.tsx
+++ packages/legacy/core/App/components/misc/VerifierCredentialCard.tsx
@@ -349,14 +349,14 @@ const VerifierCredentialCard: React.FC<VerifierCredentialCardProps> = ({
         style,
         {
           elevation: elevated ? 5 : 0,
-          overflow: 'hidden',
+          overflow: elevated ? 'visible' : 'hidden',
         },
       ]}
       onLayout={(event) => {
         setDimensions({ cardHeight: event.nativeEvent.layout.height, cardWidth: event.nativeEvent.layout.width })
       }}
     >
-      <View testID={testIdWithKey('CredentialCard')}>
+      <View testID={testIdWithKey('CredentialCard')} style={{ overflow: 'hidden' }}>
         {overlay.metaOverlay?.watermark && (
           <CardWatermark
             width={dimensions.cardWidth}
