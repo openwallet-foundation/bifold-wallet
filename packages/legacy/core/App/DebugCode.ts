baa0369b5cfd6976c5e2ae1a5b5532043a4baca0
Mostafa Gamal <moscd3@gmail.com>
2025-02-18 15:27:11 -0500

    Update snapshots
    Signed-off-by: Mostafa Gamal <moscd3@gmail.com>

--- packages/legacy/core/App/DebugCode.ts
+++ packages/legacy/core/App/DebugCode.ts
@@ -7,14 +7,25 @@ const withRemoteDebugger = () => {
   }
 
   // Check if remote debugging is enabled
-  const isDebuggingRemotely = !!NativeModules.SettingsManager.settings.RCTDevMenu.isDebuggingRemotely
-  DevSettings.addMenuItem(isDebuggingRemotely ? message.stop : message.debug, () => {
-    // Toggle debugging state
-    NativeModules.DevSettings.setIsDebuggingRemotely(!isDebuggingRemotely)
+  const settingsManager = NativeModules.SettingsManager
+  const isDebuggingRemotely =
+    settingsManager && settingsManager.settings && settingsManager.settings.RCTDevMenu
+      ? !!settingsManager.settings.RCTDevMenu.isDebuggingRemotely
+      : false
+  if (DevSettings.addMenuItem) {
+    DevSettings.addMenuItem(isDebuggingRemotely ? message.stop : message.debug, () => {
+      // Toggle debugging state
+      if (settingsManager && settingsManager.settings && settingsManager.settings.RCTDevMenu) {
+        NativeModules.DevSettings.setIsDebuggingRemotely(!isDebuggingRemotely)
+      }
 
-    // Reload is needed for the changes to take effect
-    DevSettings.reload()
-  })
+      // Reload is needed for the changes to take effect
+      DevSettings.reload()
+    })
+  } else {
+    // eslint-disable-next-line no-console
+    console.warn('DevSettings.addMenuItem is not available in this environment.')
+  }
 }
 
 if (__DEV__) {
