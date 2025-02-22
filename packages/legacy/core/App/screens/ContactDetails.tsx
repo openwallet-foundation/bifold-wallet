95079b743ccf618f416380ed04cf3cedfae02f38
Jason C. Leach <jason.leach@fullboar.ca>
2025-02-21 16:33:54 -0800

    fix: delete contact nav
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

--- packages/legacy/core/App/screens/ContactDetails.tsx
+++ packages/legacy/core/App/screens/ContactDetails.tsx
@@ -164,10 +164,11 @@ const ContactDetails: React.FC<ContactDetailsProps> = ({ route }) => {
 
       await agent.connections.deleteById(connection.id)
 
-      navigation.navigate(Screens.Contacts)
+      navigation.popToTop()
 
-      // FIXME: This delay is a hack so that the toast doesn't appear until the modal is dismissed
-      await new Promise((resolve) => setTimeout(resolve, 1000))
+      // FIXME: This delay is a hack so that the toast doesn't
+      // appear until the modal is dismissed
+      // await new Promise((resolve) => setTimeout(resolve, 1000))
 
       Toast.show({
         type: ToastType.Success,
