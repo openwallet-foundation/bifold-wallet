8d39722ffbacd86b3ca059b088b8093a1a7c53a8
ClaudeArs <claudears2474@gmail.com>
2025-02-24 13:55:15 -0500

    fix: Add accessibilityLabel to PINInput Enter Old PIN
    Signed-off-by: ClaudeArs <claudears2474@gmail.com>

--- packages/legacy/core/App/screens/PINCreate.tsx
+++ packages/legacy/core/App/screens/PINCreate.tsx
@@ -296,6 +296,7 @@ const PINCreate: React.FC<PINCreateProps> = ({ setAuthenticated, explainedStatus
             <PINInput
               label={t('PINCreate.EnterOldPINTitle')}
               testID={testIdWithKey('EnterOldPIN')}
+              accessibilityLabel={t('PINCreate.EnterOldPIN')}
               onPINChanged={(p: string) => {
                 setPINOld(p)
               }}
@@ -352,9 +353,19 @@ const PINCreate: React.FC<PINCreateProps> = ({ setAuthenticated, explainedStatus
                 return (
                   <View style={{ flexDirection: 'row' }} key={index}>
                     {validation.isInvalid ? (
-                      <Icon accessibilityLabel={t("PINCreate.Helper.ClearIcon")} name="clear" size={iconSize} color={ColorPallet.notification.errorIcon} />
+                      <Icon
+                        accessibilityLabel={t('PINCreate.Helper.ClearIcon')}
+                        name="clear"
+                        size={iconSize}
+                        color={ColorPallet.notification.errorIcon}
+                      />
                     ) : (
-                      <Icon accessibilityLabel={t("PINCreate.Helper.CheckIcon")} name="check" size={iconSize} color={ColorPallet.notification.successIcon} />
+                      <Icon
+                        accessibilityLabel={t('PINCreate.Helper.CheckIcon')}
+                        name="check"
+                        size={iconSize}
+                        color={ColorPallet.notification.successIcon}
+                      />
                     )}
                     <Text style={[TextTheme.normal, { paddingLeft: 4 }]}>
                       {t(`PINCreate.Helper.${validation.errorName}`)}
