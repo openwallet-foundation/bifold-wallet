4fd13bf303abced14683e68ca6b1bed3c276e798
Jason C. Leach <jason.leach@fullboar.ca>
2025-03-11 11:37:48 -0700

    fefactor: onbording logic
    Signed-off-by: Jason C. Leach <jason.leach@fullboar.ca>

---
+++ packages/legacy/core/App/navigators/OnboardingScreens.ts
@@ -0,0 +1,130 @@
+import { TFunction } from 'i18next'
+import { TransitionPresets } from '@react-navigation/stack'
+import { ParamListBase, RouteConfig, StackNavigationState } from '@react-navigation/native'
+import { StackNavigationOptions, StackNavigationEventMap } from '@react-navigation/stack'
+import { Screens } from '../types/navigators'
+
+type ScreenOptions = RouteConfig<
+  ParamListBase,
+  Screens,
+  StackNavigationState<ParamListBase>,
+  StackNavigationOptions,
+  StackNavigationEventMap
+>
+
+interface ScreenComponents {
+  Preface: React.FC
+  Terms: React.FC
+  NameWallet: React.FC
+  useBiometry: React.FC
+  PushNotification: React.FC
+  Developer: React.FC
+  AttemptLockout: React.FC
+  OnBoardingScreen: React.FC
+  CreatePINScreen: React.FC
+  EnterPINScreen: React.FC
+}
+
+export const getOnboardingScreens = (
+  t: TFunction,
+  ScreenOptionsDictionary: Record<Screens, any>,
+  components: ScreenComponents
+): ScreenOptions[] => [
+  {
+    name: Screens.Preface,
+    component: components.Preface,
+    options: {
+      ...ScreenOptionsDictionary[Screens.Preface],
+      ...TransitionPresets.SlideFromRightIOS,
+      title: t('Screens.Preface'),
+    },
+  },
+  {
+    name: Screens.Onboarding,
+    children: components.OnBoardingScreen,
+    options: () => ({
+      ...ScreenOptionsDictionary[Screens.Onboarding],
+      ...TransitionPresets.SlideFromRightIOS,
+      title: t('Screens.Onboarding'),
+      headerLeft: () => false,
+    }),
+  },
+  {
+    name: Screens.Terms,
+    options: () => ({
+      ...ScreenOptionsDictionary[Screens.Terms],
+      ...TransitionPresets.SlideFromRightIOS,
+      title: t('Screens.Terms'),
+      headerLeft: () => false,
+    }),
+    component: components.Terms,
+  },
+  {
+    name: Screens.CreatePIN,
+    children: components.CreatePINScreen,
+    initialParams: {},
+    options: () => ({
+      ...ScreenOptionsDictionary[Screens.CreatePIN],
+      ...TransitionPresets.SlideFromRightIOS,
+      title: t('Screens.CreatePIN'),
+      headerLeft: () => false,
+    }),
+  },
+  {
+    name: Screens.NameWallet,
+    options: () => ({
+      ...ScreenOptionsDictionary[Screens.NameWallet],
+      title: t('Screens.NameWallet'),
+      headerLeft: () => false,
+    }),
+    component: components.NameWallet,
+  },
+  {
+    name: Screens.UseBiometry,
+    options: () => ({
+      ...ScreenOptionsDictionary[Screens.UseBiometry],
+      ...TransitionPresets.SlideFromRightIOS,
+      title: t('Screens.Biometry'),
+      headerLeft: () => false,
+    }),
+    component: components.useBiometry,
+  },
+  {
+    name: Screens.UsePushNotifications,
+    component: components.PushNotification,
+    options: () => ({
+      ...ScreenOptionsDictionary[Screens.UsePushNotifications],
+      ...TransitionPresets.SlideFromRightIOS,
+      title: t('Screens.UsePushNotifications'),
+      headerLeft: () => false,
+    }),
+  },
+  {
+    name: Screens.Developer,
+    component: components.Developer,
+    options: () => ({
+      ...ScreenOptionsDictionary[Screens.Developer],
+      title: t('Screens.Developer'),
+      headerBackAccessibilityLabel: t('Global.Back'),
+    }),
+  },
+  {
+    name: Screens.EnterPIN,
+    children: components.EnterPINScreen,
+    options: () => ({
+      title: t('Screens.EnterPIN'),
+      headerShown: true,
+      headerLeft: () => false,
+      rightLeft: () => false,
+    }),
+  },
+  {
+    name: Screens.AttemptLockout,
+    component: components.AttemptLockout,
+    options: () => ({
+      headerShown: true,
+      headerLeft: () => null,
+      title: t('Screens.AttemptLockout'),
+    }),
+  },
+]
