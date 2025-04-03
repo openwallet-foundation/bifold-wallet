f4177922055826448fe07ddf7f07c42ae5d4a340
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2025-04-03 11:26:17 -0700

    refactor: less deps for hook (#1494)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    
    Signed-off-by: Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>

--- packages/core/__tests__/hooks/screen-capture.test.tsx
+++ packages/core/__tests__/hooks/screen-capture.test.tsx
@@ -1,96 +1,51 @@
 import { renderHook } from '@testing-library/react-native'
 import { CaptureProtection } from 'react-native-capture-protection'
-import { container } from 'tsyringe'
 
-import { TOKENS } from '../../src/container-api'
-import { MainContainer } from '../../src/container-impl'
 import usePreventScreenCapture from '../../src/hooks/screen-capture'
-import { CustomBasicAppContext } from '../helpers/app'
 
 describe('usePreventScreenCapture', () => {
   beforeEach(() => {
     jest.resetAllMocks()
   })
 
-  it('should call preventScreenshot when preventScreenCapture is true', () => {
-    const context = new MainContainer(container.createChildContainer()).init()
-    const config = context.resolve(TOKENS.CONFIG)
-    context.container.registerInstance(TOKENS.CONFIG, { ...config, preventScreenCapture: true })
-    renderHook(() => usePreventScreenCapture(), {
-      wrapper: ({ children }) => (
-        <CustomBasicAppContext container={context}>{children}</CustomBasicAppContext>
-      ),  
-    })
+  it('with no `active` supplied, should call preventScreenshot', () => {
+    renderHook(() => usePreventScreenCapture())
 
     expect(CaptureProtection.preventScreenshot).toHaveBeenCalledTimes(1)
   })
   
-  it('should call allowScreenshot when preventScreenCapture is true but component is then unmounted', () => {
-    const context = new MainContainer(container.createChildContainer()).init()
-    const config = context.resolve(TOKENS.CONFIG)
-    context.container.registerInstance(TOKENS.CONFIG, { ...config, preventScreenCapture: true })
-    const { unmount } = renderHook(() => usePreventScreenCapture(true), {
-      wrapper: ({ children }) => (
-        <CustomBasicAppContext container={context}>{children}</CustomBasicAppContext>
-      ),  
-    })
+  it('with no `active` supplied, should then call allowScreenshot when component is unmounted', () => {
+    const { unmount } = renderHook(() => usePreventScreenCapture())
 
+    expect(CaptureProtection.preventScreenshot).toHaveBeenCalledTimes(1)
+    expect(CaptureProtection.allowScreenshot).not.toHaveBeenCalled()
+    
     unmount()
 
     expect(CaptureProtection.allowScreenshot).toHaveBeenCalledTimes(1)
     expect(CaptureProtection.allowScreenshot).toHaveBeenCalledWith(true)
   })
-  
-  it('should call preventScreenshot when preventScreenCapture is true and active is true', () => {
-    const context = new MainContainer(container.createChildContainer()).init()
-    const config = context.resolve(TOKENS.CONFIG)
-    context.container.registerInstance(TOKENS.CONFIG, { ...config, preventScreenCapture: true })
-    renderHook(() => usePreventScreenCapture(true), {
-      wrapper: ({ children }) => (
-        <CustomBasicAppContext container={context}>{children}</CustomBasicAppContext>
-      ),  
-    })
+
+  it('with `active` true, should call preventScreenshot', () => {
+    renderHook(() => usePreventScreenCapture(true))
 
     expect(CaptureProtection.preventScreenshot).toHaveBeenCalledTimes(1)
   })
   
-  it('should not call preventScreenshot when preventScreenCapture is true and active is false', () => {
-    const context = new MainContainer(container.createChildContainer()).init()
-    const config = context.resolve(TOKENS.CONFIG)
-    context.container.registerInstance(TOKENS.CONFIG, { ...config, preventScreenCapture: true })
-    renderHook(() => usePreventScreenCapture(false), {
-      wrapper: ({ children }) => (
-        <CustomBasicAppContext container={context}>{children}</CustomBasicAppContext>
-      ),  
-    })
+  it('with `active` true, should then call allowScreenshot when component is unmounted', () => {
+    const { unmount } = renderHook(() => usePreventScreenCapture(true))
 
-    expect(CaptureProtection.preventScreenshot).not.toHaveBeenCalled()
+    expect(CaptureProtection.preventScreenshot).toHaveBeenCalledTimes(1)
     expect(CaptureProtection.allowScreenshot).not.toHaveBeenCalled()
-  })
-  
-  it('should not call preventScreenshot when preventScreenCapture is false and active is false', () => {
-    const context = new MainContainer(container.createChildContainer()).init()
-    const config = context.resolve(TOKENS.CONFIG)
-    context.container.registerInstance(TOKENS.CONFIG, { ...config, preventScreenCapture: false })
-    renderHook(() => usePreventScreenCapture(false), {
-      wrapper: ({ children }) => (
-        <CustomBasicAppContext container={context}>{children}</CustomBasicAppContext>
-      ),  
-    })
+    
+    unmount()
 
-    expect(CaptureProtection.preventScreenshot).not.toHaveBeenCalled()
-    expect(CaptureProtection.allowScreenshot).not.toHaveBeenCalled()
+    expect(CaptureProtection.allowScreenshot).toHaveBeenCalledTimes(1)
+    expect(CaptureProtection.allowScreenshot).toHaveBeenCalledWith(true)
   })
-  
-  it('should not call preventScreenshot when preventScreenCapture is false and active is true', () => {
-    const context = new MainContainer(container.createChildContainer()).init()
-    const config = context.resolve(TOKENS.CONFIG)
-    context.container.registerInstance(TOKENS.CONFIG, { ...config, preventScreenCapture: false })
-    renderHook(() => usePreventScreenCapture(true), {
-      wrapper: ({ children }) => (
-        <CustomBasicAppContext container={context}>{children}</CustomBasicAppContext>
-      ),  
-    })
+
+  it('with `active` false, should not call screenshot methods', () => {
+    renderHook(() => usePreventScreenCapture(false))
 
     expect(CaptureProtection.preventScreenshot).not.toHaveBeenCalled()
     expect(CaptureProtection.allowScreenshot).not.toHaveBeenCalled()
