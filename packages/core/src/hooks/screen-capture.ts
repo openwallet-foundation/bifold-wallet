1b1603b4d24f391e4fd4e04758068ac64fa89330
Bryce McMath <bryce.j.mcmath@gmail.com>
2025-04-03 09:59:47 -0700

    refactor: less deps for hook
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- packages/core/src/hooks/screen-capture.ts
+++ packages/core/src/hooks/screen-capture.ts
@@ -1,15 +1,15 @@
 import { useEffect } from 'react'
 import { CaptureProtection } from 'react-native-capture-protection'
-import { TOKENS, useServices } from '../container-api'
 
 /**
  * Prevents screenshots when component is mounted. When unmounted, allows them again
  * 
  * @param {boolean} [active=true] - If `false`, hook does nothing
  * @remarks
- * - If the config boolean `preventScreenCapture` is `true` and the
- * `active` param is `true` it prevents screenshots when the component is mounted
- * - On component unmount, it restores the ability to take screenshots
+ * - If `active` param is `true`, it prevents screenshots when the
+ * component is mounted
+ * - On unmount, or when `active` is changed to false, it restores
+ * the ability to take screenshots
  * - Should be used at the screen level only
  * 
  * @example
@@ -25,17 +25,15 @@ import { TOKENS, useServices } from '../container-api'
  * @returns {void}
  */
 const usePreventScreenCapture = (active: boolean = true) => {
-  const [{ preventScreenCapture }] = useServices([TOKENS.CONFIG])
-
   useEffect(() => {
-    if (!active || !preventScreenCapture) return
+    if (!active) return
 
     CaptureProtection.preventScreenshot()
 
     return () => {
       CaptureProtection.allowScreenshot(true)
     }
-  }, [active, preventScreenCapture])
+  }, [active])
 }
 
 export default usePreventScreenCapture
