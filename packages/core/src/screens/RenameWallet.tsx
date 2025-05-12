564be0aa4b1be6de25ea6b32d81f337c0469312b
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2025-05-12 10:24:38 -0700

    refactor: wallet name screens (#1522)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>
    
    Signed-off-by: Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>

---
+++ packages/core/src/screens/RenameWallet.tsx
@@ -0,0 +1,25 @@
+import { useAgent } from '@credo-ts/react-hooks'
+import { useNavigation } from '@react-navigation/native'
+import React, { useCallback } from 'react'
+
+import WalletNameForm from '../components/forms/WalletNameForm'
+
+const RenameWallet: React.FC = () => {
+  const navigation = useNavigation()
+  const { agent } = useAgent()
+
+  const onCancel = useCallback(() => {
+    navigation.goBack()
+  }, [navigation])
+
+  const onSubmitSuccess = useCallback((name: string) => {
+    agent.config.label = name
+    navigation.goBack()
+  }, [navigation, agent])
+
+  return (
+    <WalletNameForm isRenaming onCancel={onCancel} onSubmitSuccess={onSubmitSuccess} />
+  )
+}
+
+export default RenameWallet
