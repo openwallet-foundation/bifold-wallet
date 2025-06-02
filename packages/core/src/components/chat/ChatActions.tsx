d1185bbbe88ed6ce9c52833ea4eb72d6d69eb1de
Travis Lee <travislee2k15@gmail.com>
2025-06-02 11:13:46 -0700

    fix: accessibility properties added directly to Icon instead of TouchableOpacity because latter caused Icon to not be pressable.
    Signed-off-by: Travis Lee <travislee2k15@gmail.com>

--- packages/core/src/components/chat/ChatActions.tsx
+++ packages/core/src/components/chat/ChatActions.tsx
@@ -2,7 +2,6 @@ import { t } from 'i18next'
 import React from 'react'
 import { Actions } from 'react-native-gifted-chat'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import { TouchableOpacity } from 'react-native'
 
 export const renderActions = (
   props: any,
@@ -19,9 +18,14 @@ export const renderActions = (
         marginLeft: 20,
       }}
       icon={() => (
-        <TouchableOpacity accessible={true} accessibilityLabel={t('Chat.Actions')} accessibilityRole="button">
-          <Icon name={'plus-box-outline'} size={40} color={theme.options} />
-        </TouchableOpacity>
+        <Icon
+          name={'plus-box-outline'}
+          size={40}
+          color={theme.options}
+          accessible={true}
+          accessibilityLabel={t('Chat.Actions')}
+          accessibilityRole="button"
+        />
       )}
       optionTintColor={theme.optionsText}
     />
