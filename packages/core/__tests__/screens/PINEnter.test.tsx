bab2ab3de37f3ed25de71f47989378371c04ef48
traviskhl <travislee2k15@gmail.com>
2025-07-15 09:57:17 -0700

    test: increased coverage for PINEnter.
    Signed-off-by: traviskhl <travislee2k15@gmail.com>

--- packages/core/__tests__/screens/PINEnter.test.tsx
+++ packages/core/__tests__/screens/PINEnter.test.tsx
@@ -1,6 +1,7 @@
 import { render, fireEvent } from '@testing-library/react-native'
 import React, { act } from 'react'
 import { container } from 'tsyringe'
+import { Keyboard } from 'react-native'
 
 import { ContainerProvider } from '../../src/container-api'
 import { MainContainer } from '../../src/container-impl'
@@ -156,5 +157,57 @@ describe('PINEnter Screen', () => {
     )
     const versionText = await tree.getByTestId(testIdWithKey('Version'))
     expect(versionText).not.toBeNull()
+  })
+
+  test('keyboard dismiss is called when pin input is filled', async () => {
+    jest.spyOn(Keyboard, 'dismiss').mockImplementation(() => {})
+
+    const main = new MainContainer(container.createChildContainer()).init()
+    const tree = render(
+      <ContainerProvider value={main}>
+        <StoreProvider
+          initialState={{
+            ...defaultState,
+          }}
+        >
+          <AuthContext.Provider value={authContext}>
+            <PINEnter setAuthenticated={jest.fn()} />
+          </AuthContext.Provider>
+        </StoreProvider>
+      </ContainerProvider>
+    )
+    const pinInput = tree.getByTestId(testIdWithKey('EnterPIN'))
+    await act(async () => {
+      fireEvent.changeText(pinInput, '123456') // minpinlength is 6
+    })
+    expect(Keyboard.dismiss).toHaveBeenCalled()
+  })
+
+  test('pin is submitted when pin input is filled', async () => {
+    const setAuthenticatedMock = jest.fn()
+    const mockAuthContext = {
+      ...authContext,
+      checkWalletPIN: jest.fn().mockResolvedValue(true),
+      setAuthenticated: setAuthenticatedMock,
+    }
+    const main = new MainContainer(container.createChildContainer()).init()
+    const tree = render(
+      <ContainerProvider value={main}>
+        <StoreProvider
+          initialState={{
+            ...defaultState,
+          }}
+        >
+          <AuthContext.Provider value={mockAuthContext}>
+            <PINEnter setAuthenticated={mockAuthContext.setAuthenticated} />
+          </AuthContext.Provider>
+        </StoreProvider>
+      </ContainerProvider>
+    )
+    const pinInput = tree.getByTestId(testIdWithKey('EnterPIN'))
+    await act(async () => {
+      fireEvent.changeText(pinInput, '123456') // minpinlength is 6
+    })
+    expect(setAuthenticatedMock).toHaveBeenCalled()
   })
 })
