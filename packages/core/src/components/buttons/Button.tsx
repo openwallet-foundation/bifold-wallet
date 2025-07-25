da487bb33cdea7ca3cc9ae4c85f3343cb6f08695
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2025-07-25 12:52:48 -0700

    chore(deps): synchronize and bump dependencies (#1591)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

--- packages/core/src/components/buttons/Button.tsx
+++ packages/core/src/components/buttons/Button.tsx
@@ -22,53 +22,53 @@ const ButtonImplComponent = (
 ) => {
   const { Buttons, heavyOpacity } = useTheme()
   const buttonStyles = {
-    [ButtonType.Critical]: { 
-      color: Buttons.critical, 
+    [ButtonType.Critical]: {
+      color: Buttons.critical,
       colorDisabled: Buttons.criticalTextDisabled,
       text: Buttons.criticalText,
-      textDisabled: Buttons.criticalTextDisabled
+      textDisabled: Buttons.criticalTextDisabled,
     },
-    [ButtonType.Primary]: { 
-      color: Buttons.primary, 
+    [ButtonType.Primary]: {
+      color: Buttons.primary,
       colorDisabled: Buttons.primaryDisabled,
       text: Buttons.primaryText,
-      textDisabled: Buttons.primaryTextDisabled
+      textDisabled: Buttons.primaryTextDisabled,
     },
-    [ButtonType.Secondary]: { 
-      color: Buttons.secondary, 
+    [ButtonType.Secondary]: {
+      color: Buttons.secondary,
       colorDisabled: Buttons.secondaryDisabled,
       text: Buttons.secondaryText,
-      textDisabled: Buttons.secondaryTextDisabled
+      textDisabled: Buttons.secondaryTextDisabled,
     },
-    [ButtonType.Tertiary]: { 
-      color: Buttons.tertiary, 
+    [ButtonType.Tertiary]: {
+      color: Buttons.tertiary,
       colorDisabled: Buttons.tertiaryDisabled,
       text: Buttons.tertiaryText,
-      textDisabled: Buttons.tertiaryTextDisabled
+      textDisabled: Buttons.tertiaryTextDisabled,
     },
-    [ButtonType.ModalCritical]: { 
-      color: Buttons.modalCritical, 
+    [ButtonType.ModalCritical]: {
+      color: Buttons.modalCritical,
       colorDisabled: Buttons.modalCriticalDisabled,
       text: Buttons.modalCriticalText,
-      textDisabled: Buttons.modalCriticalTextDisabled
+      textDisabled: Buttons.modalCriticalTextDisabled,
     },
-    [ButtonType.ModalPrimary]: { 
-      color: Buttons.modalPrimary, 
+    [ButtonType.ModalPrimary]: {
+      color: Buttons.modalPrimary,
       colorDisabled: Buttons.modalPrimaryDisabled,
       text: Buttons.modalPrimaryText,
-      textDisabled: Buttons.modalPrimaryTextDisabled
+      textDisabled: Buttons.modalPrimaryTextDisabled,
     },
-    [ButtonType.ModalSecondary]: { 
-      color: Buttons.modalSecondary, 
+    [ButtonType.ModalSecondary]: {
+      color: Buttons.modalSecondary,
       colorDisabled: Buttons.modalSecondaryDisabled,
       text: Buttons.modalSecondaryText,
-      textDisabled: Buttons.modalSecondaryTextDisabled
+      textDisabled: Buttons.modalSecondaryTextDisabled,
     },
-    [ButtonType.ModalTertiary]: { 
-      color: Buttons.modalTertiary, 
+    [ButtonType.ModalTertiary]: {
+      color: Buttons.modalTertiary,
       colorDisabled: Buttons.modalTertiaryDisabled,
       text: Buttons.modalTertiaryText,
-      textDisabled: Buttons.modalTertiaryTextDisabled
+      textDisabled: Buttons.modalTertiaryTextDisabled,
     },
   }
   const [isActive, setIsActive] = useState<boolean>(false)
