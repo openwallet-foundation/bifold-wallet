3fcf23b4bc367c501404389dca4cc7609362318b
Al Rosenthal <alfred.k.rosenthal@gmail.com>
2025-09-02 13:37:03 -0700

    feat: release build clean up (#1626)
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
    
    Signed-off-by: Al Rosenthal <alfred.k.rosenthal@gmail.com>

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -6,14 +6,18 @@ import AgentProvider from '@credo-ts/react-hooks'
 
 import createApp from './App'
 import * as components from './components'
-import { Button as IButton, ButtonImpl as Button, ButtonType } from './components/buttons/Button'
+import { ButtonImpl as Button, ButtonType, Button as IButton } from './components/buttons/Button'
 import IconButton, { ButtonLocation } from './components/buttons/IconButton'
 import BulletPoint from './components/inputs/BulletPoint'
 import CheckBoxRow from './components/inputs/CheckBoxRow'
+import LimitedTextInput from './components/inputs/LimitedTextInput'
+import NotificationListItem from './components/listItems/NotificationListItem'
 import ContentGradient from './components/misc/ContentGradient'
 import CredentialCard from './components/misc/CredentialCard'
 import ErrorBoundaryWrapper from './components/misc/ErrorBoundary'
+import FauxHeader from './components/misc/FauxHeader'
 import InfoBox, { InfoBoxType } from './components/misc/InfoBox'
+import DeveloperModal from './components/modals/DeveloperModal'
 import ErrorModal from './components/modals/ErrorModal'
 import SafeAreaModal from './components/modals/SafeAreaModal'
 import Record from './components/record/Record'
@@ -29,211 +33,182 @@ import { credentialsTourSteps } from './components/tour/CredentialsTourSteps'
 import { homeTourSteps } from './components/tour/HomeTourSteps'
 import { proofRequestTourSteps } from './components/tour/ProofRequestTourSteps'
 import { TourBox } from './components/tour/TourBox'
-import LimitedTextInput from './components/inputs/LimitedTextInput'
+import { Banner, BannerMessage, BannerSection } from './components/views/Banner'
 import HomeFooterView from './components/views/HomeFooterView'
 import KeyboardView from './components/views/KeyboardView'
-import NotificationListItem from './components/listItems/NotificationListItem'
+import { attemptLockoutConfig, PINRules, tours, walletTimeout } from './constants'
+import { defaultConfig, defaultHistoryEventsLogger } from './container-impl'
 import * as contexts from './contexts'
-import { AutoLockTime, ActivityProvider, useActivity } from './contexts/activity'
+import { ActivityProvider, AutoLockTime, useActivity } from './contexts/activity'
 import { AuthProvider } from './contexts/auth'
 import NavContainer from './contexts/navigation'
 import { NetworkProvider } from './contexts/network'
 import { useTour } from './contexts/tour/tour-context'
 import { TourProvider } from './contexts/tour/tour-provider'
-import AttemptLockout from './screens/AttemptLockout'
-import Developer from './screens/Developer'
-import DeveloperModal from './components/modals/DeveloperModal'
 import { useDeveloperMode } from './hooks/developer-mode'
-import FauxHeader from './components/misc/FauxHeader'
+import usePreventScreenCapture from './hooks/screen-capture'
+import useBifoldAgentSetup from './hooks/useBifoldAgentSetup'
+import { OpenIDCredentialRecordProvider } from './modules/openid/context/OpenIDCredentialRecordProvider'
+import { DefaultScreenLayoutOptions } from './navigators/defaultLayoutOptions'
+import { DefaultScreenOptionsDictionary, useDefaultStackOptions } from './navigators/defaultStackOptions'
+import AttemptLockout from './screens/AttemptLockout'
+import Biometry from './screens/Biometry'
+import Developer from './screens/Developer'
+import Onboarding from './screens/Onboarding'
 import OnboardingPages from './screens/OnboardingPages'
 import Preface from './screens/Preface'
+import Scan from './screens/Scan'
 import Splash from './screens/Splash'
 import Terms from './screens/Terms'
 import UpdateAvailable from './screens/UpdateAvailable'
-import Biometry from './screens/Biometry'
-import { loadLoginAttempt, isBiometricsActive } from './services/keychain'
-import { BifoldLogger } from './services/logger'
-import * as types from './types'
-import Scan from './screens/Scan'
-import Onboarding from './screens/Onboarding'
-import { DefaultScreenOptionsDictionary, useDefaultStackOptions } from './navigators/defaultStackOptions'
-import { PINRules, walletTimeout, tours, attemptLockoutConfig } from './constants'
-import { CredentialListFooterProps } from './types/credential-list-footer'
-import { OpenIDCredentialRecordProvider } from './modules/openid/context/OpenIDCredentialRecordProvider'
-import { defaultConfig, defaultHistoryEventsLogger } from './container-impl'
-import useBifoldAgentSetup from './hooks/useBifoldAgentSetup'
-import usePreventScreenCapture from './hooks/screen-capture'
-import { DefaultScreenLayoutOptions } from './navigators/defaultLayoutOptions'
-import { DeepPartial, ThemeBuilder } from './theme-builder'
-import { BannerMessage } from './components/views/Banner'
 import { bifoldLoggerInstance } from './services/bifoldLogger'
+import { isBiometricsActive, loadLoginAttempt } from './services/keychain'
+import { BifoldLogger } from './services/logger'
+import { DeepPartial, ThemeBuilder } from './theme-builder'
+import * as types from './types'
+import { CredentialListFooterProps } from './types/credential-list-footer'
 
-export * from './navigators'
-export * from './services/storage'
-export * from './types/attestation'
-export * from './types/version-check'
-export { LocalStorageKeys } from './constants'
-export * from './services/storage'
-export { initLanguages, initStoredLanguage, translationResources, Locales } from './localization'
-export { defaultState, mergeReducers, StoreProvider, StoreContext, useStore } from './contexts/store'
-export { default as Store, DispatchAction, reducer } from './contexts/reducers/store'
-export { useDeepLinks } from './hooks/deep-links'
-export { Assets as ImageAssets } from './theme'
-export { ThemeProvider, useTheme } from './contexts/theme'
-export { AnimatedComponentsProvider, useAnimatedComponents } from './contexts/animated-components'
-export { ColorPalette } from './theme'
 export { animatedComponents } from './animated-components'
-export { bifoldTheme } from './theme'
+export { EventTypes, LocalStorageKeys } from './constants'
+export { AnimatedComponentsProvider, useAnimatedComponents } from './contexts/animated-components'
 export { useAuth } from './contexts/auth'
 export { useNetwork } from './contexts/network'
-export { testIdWithKey, testIdForAccessabilityLabel } from './utils/testable'
-export { Screens, Stacks, TabStacks } from './types/navigators'
+export { DispatchAction, reducer, default as Store } from './contexts/reducers/store'
+export { defaultState, mergeReducers, StoreContext, StoreProvider, useStore } from './contexts/store'
+export { ThemeProvider, useTheme } from './contexts/theme'
+export { useDeepLinks } from './hooks/deep-links'
+export { initLanguages, initStoredLanguage, Locales, translationResources } from './localization'
+export * from './navigators'
 export { createStyles } from './screens/OnboardingPages'
-export { statusBarStyleForColor, StatusBarStyles } from './utils/luminance'
+export * from './services/storage'
+export { bifoldTheme, ColorPalette, Assets as ImageAssets } from './theme'
+export * from './types/attestation'
 export { BifoldError } from './types/error'
-export { EventTypes } from './constants'
-export { migrateToAskar } from './utils/migration'
+export { Screens, Stacks, TabStacks } from './types/navigators'
+export * from './types/version-check'
 export { createLinkSecretIfRequired, getAgentModules } from './utils/agent'
+export { getCredentialIdentifiers, isValidAnonCredsCredential } from './utils/credential'
 export {
-  removeExistingInvitationsById,
   connectFromScanOrDeepLink,
-  formatTime,
-  useCredentialConnectionLabel,
-  getConnectionName,
+  formatTime, getConnectionName, removeExistingInvitationsById, useCredentialConnectionLabel
 } from './utils/helpers'
-export { isValidAnonCredsCredential, getCredentialIdentifiers } from './utils/credential'
+export { getIndyLedgers, IndyLedger, readIndyLedgersFromFile, writeIndyLedgersToFile } from './utils/ledger'
+export { statusBarStyleForColor, StatusBarStyles } from './utils/luminance'
+export { migrateToAskar } from './utils/migration'
 export { buildFieldsFromAnonCredsCredential } from './utils/oca'
-export { IndyLedger, getIndyLedgers, readIndyLedgersFromFile, writeIndyLedgersToFile } from './utils/ledger'
+export { testIdForAccessabilityLabel, testIdWithKey } from './utils/testable'
 
-export type { IndyLedgerConfig, IndyLedgerJSON, IndyLedgersRecord, IndyLedgerFileSystem } from './utils/ledger'
 export type { AnimatedComponents } from './animated-components'
-export type {
-  ISVGAssets,
-  ISpacing,
-  IFontAttributes,
-  IInputAttributes,
-  IInlineInputMessage,
-  IInputs,
-  ITextTheme,
-  ITabTheme,
-  IBrandColors,
-  ISemanticColors,
-  INotificationColors,
-  IErrorColors,
-  IGrayscaleColors,
-  IColorPalette,
-  IAssets,
-  ITheme,
-} from './theme'
-export type { basicMessageCustomMetadata, credentialCustomMetadata } from './types/metadata'
-export { CredentialMetadata, BasicMessageMetadata } from './types/metadata'
-export type { VersionInfo, PersistentState } from './types/state'
-export type { BifoldAgent } from './utils/agent'
-export type { TourStep, RenderProps } from './contexts/tour/tour-context'
-export type { GenericFn } from './types/fn'
-export type { OnboardingStackParams, NotificationStackParams, ContactStackParams } from './types/navigators'
-export type { OnboardingStyleSheet }
-export type { WalletSecret } from './types/security'
 export type { ReducerAction } from './contexts/reducers/store'
+export type { RenderProps, TourStep } from './contexts/tour/tour-context'
 export type {
-  State,
-  Onboarding as OnboardingState,
-  LoginAttempt as LoginAttemptState,
-  Preferences as PreferencesState,
-  Migration as MigrationState,
-  Tours as ToursState,
-} from './types/state'
+  IAssets, IBrandColors, IColorPalette, IErrorColors, IFontAttributes, IGrayscaleColors, IInlineInputMessage, IInputAttributes, IInputs, INotificationColors, ISemanticColors, ISpacing, ISVGAssets, ITabTheme, ITextTheme, ITheme
+} from './theme'
+export type { GenericFn } from './types/fn'
+export { BasicMessageMetadata, CredentialMetadata } from './types/metadata'
+export type { basicMessageCustomMetadata, credentialCustomMetadata } from './types/metadata'
+export type { ContactStackParams, NotificationStackParams, OnboardingStackParams } from './types/navigators'
+export type { WalletSecret } from './types/security'
+export type { LoginAttempt as LoginAttemptState, Migration as MigrationState, Onboarding as OnboardingState, PersistentState, Preferences as PreferencesState, State, Tours as ToursState, VersionInfo } from './types/state'
+export type { BifoldAgent } from './utils/agent'
+export type { IndyLedgerConfig, IndyLedgerFileSystem, IndyLedgerJSON, IndyLedgersRecord } from './utils/ledger'
+export type { OnboardingStyleSheet }
 
 export type { InlineMessageProps } from './components/inputs/InlineErrorText'
 
 export { InlineErrorPosition } from './types/error'
 
-export type { CredentialListFooterProps }
 export * from './container-api'
 export { MainContainer } from './container-impl'
+export type { CredentialListFooterProps }
 
-export type { ScreenLayoutConfig, OnboardingTask } from './types/navigators'
 export type { Config, HistoryEventsLoggerConfig } from './types/config'
+export type { OnboardingTask, ScreenLayoutConfig } from './types/navigators'
 
-export { BaseTourID } from './types/tour'
-export type { SplashProps } from './screens/Splash'
-export type { OnboardingStackProps } from './navigators/OnboardingStack'
 export { LockoutReason } from './contexts/auth'
+export type { OnboardingStackProps } from './navigators/OnboardingStack'
+export type { SplashProps } from './screens/Splash'
+export { BaseTourID } from './types/tour'
+
+export type { BannerSectionProps } from './components/views/Banner'
 
 export {
-  createApp,
+  ActivityProvider,
   Agent,
   AgentProvider,
-  AuthProvider,
-  NetworkProvider,
-  TourProvider,
-  useTour,
   AttachTourStep,
-  TourBox,
+  AttemptLockout,
+  attemptLockoutConfig,
+  AuthProvider,
+  AutoLockTime,
+  Banner,
+  BannerSection,
+  BifoldLogger,
+  bifoldLoggerInstance,
+  Biometry,
+  BulletPoint,
+  Button,
+  ButtonLocation,
+  ButtonType,
+  CheckBoxRow,
+  components,
+  ContentGradient,
+  contexts,
+  createApp,
+  CredentialCard,
+  credentialOfferTourSteps,
+  credentialsTourSteps,
   defaultConfig,
   defaultHistoryEventsLogger,
-  homeTourSteps,
-  credentialsTourSteps,
-  credentialOfferTourSteps,
-  proofRequestTourSteps,
-  ButtonType,
-  ButtonLocation,
-  CheckBoxRow,
-  CredentialCard,
-  ErrorBoundaryWrapper,
-  bifoldLoggerInstance,
-  ContentGradient,
-  ErrorModal,
-  SafeAreaModal,
-  IconButton,
-  ActivityProvider,
-  useActivity,
-  InfoTextBox,
-  InfoBox,
-  InfoBoxType,
-  Link,
-  AutoLockTime,
-  ToastType,
-  toastConfig,
-  NavContainer,
-  OnboardingPages,
-  OpenIDCredentialRecordProvider,
-  NotificationListItem,
-  useDefaultStackOptions,
-  useBifoldAgentSetup,
-  usePreventScreenCapture,
-  Splash,
+  DefaultScreenLayoutOptions,
+  DefaultScreenOptionsDictionary,
   Developer,
   DeveloperModal,
-  useDeveloperMode,
+  ErrorBoundaryWrapper,
+  ErrorModal,
   FauxHeader,
-  Terms,
-  Preface,
-  UpdateAvailable,
   HomeFooterView as HomeContentView,
-  Biometry,
-  AttemptLockout,
-  Record,
-  Scan,
-  Onboarding,
-  types,
-  components,
-  contexts,
-  Text,
-  ThemedText,
-  loadLoginAttempt,
+  homeTourSteps,
+  IconButton,
+  InfoBox,
+  InfoBoxType,
+  InfoTextBox,
   isBiometricsActive,
-  Button,
-  BifoldLogger,
-  LimitedTextInput,
   KeyboardView,
-  BulletPoint,
+  LimitedTextInput,
+  Link,
+  loadLoginAttempt,
+  NavContainer,
+  NetworkProvider,
+  NotificationListItem,
+  Onboarding,
+  OnboardingPages,
+  OpenIDCredentialRecordProvider,
   PINRules,
-  walletTimeout,
-  attemptLockoutConfig,
-  tours,
-  DefaultScreenOptionsDictionary,
-  DefaultScreenLayoutOptions,
+  Preface,
+  proofRequestTourSteps,
+  Record,
+  SafeAreaModal,
+  Scan,
+  Splash,
+  Terms,
+  Text,
   ThemeBuilder,
+  ThemedText,
+  toastConfig,
+  ToastType,
+  TourBox,
+  TourProvider,
+  tours,
+  types,
+  UpdateAvailable,
+  useActivity,
+  useBifoldAgentSetup,
+  useDefaultStackOptions,
+  useDeveloperMode,
+  usePreventScreenCapture,
+  useTour,
+  walletTimeout
 }
-export type { IButton, DeepPartial, BannerMessage }
+export type { BannerMessage, DeepPartial, IButton }
+
