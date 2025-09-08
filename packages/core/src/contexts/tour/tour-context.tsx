cdf541e3e3c01b5f312d636cfe8291b3e16edb68
Alfred Rosenthal <alfred.k.rosenthal@gmail.com>
2025-09-08 16:03:27 -0700

    chore: fixed linting issue
    Signed-off-by: Alfred Rosenthal <alfred.k.rosenthal@gmail.com>

--- packages/core/src/contexts/tour/tour-context.tsx
+++ packages/core/src/contexts/tour/tour-context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactElement, useContext } from 'react'
+import { createContext, ReactElement, useContext } from 'react'
 import { LayoutRectangle } from 'react-native'
 
 import { BaseTourID, TourID } from '../../types/tour'
