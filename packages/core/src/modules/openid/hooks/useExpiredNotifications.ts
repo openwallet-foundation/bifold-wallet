6c05c4761faf95074b4d129c2a7fc827e4811b14
Mostafa Gamal <moscd3@gmail.com>
2025-11-21 09:41:49 -0500

    fix rmove cred
    Signed-off-by: Mostafa Gamal <moscd3@gmail.com>

--- packages/core/src/modules/openid/hooks/useExpiredNotifications.ts
+++ packages/core/src/modules/openid/hooks/useExpiredNotifications.ts
@@ -1,38 +1,45 @@
 // modules/openid/hooks/useExpiredNotifications.ts
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { credentialRegistry, RegistryStore } from '../refresh/registery'
 import { CustomNotification } from '../../../types/notification'
 import { OpenIDCustomNotificationType } from '../refresh/types'
+import { TOKENS, useServices } from '../../../container-api'
+import { useDeclineReplacement } from './useDeclineReplacement'
 
 export const useExpiredNotifications = (): CustomNotification[] => {
   const [items, setItems] = useState<CustomNotification[]>([])
+  const [logger] = useServices([TOKENS.UTIL_LOGGER])
+  const { declineByOldId } = useDeclineReplacement({ logger })
 
-  const build = (s: RegistryStore): CustomNotification[] =>
-    s.expired.map((oldId) => {
-      const lite = s.byId[oldId]
-      const n: CustomNotification = {
-        type: OpenIDCustomNotificationType.CredentialExpired,
-        title: 'Credential expired',
-        pageTitle: 'Credential Expired',
-        buttonTitle: 'Review',
-        description: 'This credential is no longer valid. You can attempt to obtain an updated version.',
-        createdAt: new Date(),
-        onPressAction: () => {},
-        onCloseAction: () => {},
-        component: () => null,
-        metadata: {
-          oldId,
-          format: lite?.format,
-        },
-      }
-      return n
-    })
+  const build = useCallback(
+    (s: RegistryStore): CustomNotification[] =>
+      s.expired.map((oldId) => {
+        const lite = s.byId[oldId]
+        const n: CustomNotification = {
+          type: OpenIDCustomNotificationType.CredentialExpired,
+          title: 'Credential expired',
+          pageTitle: 'Credential Expired',
+          buttonTitle: 'Review',
+          description: 'This credential is no longer valid. You can attempt to obtain an updated version.',
+          createdAt: new Date(),
+          onPressAction: () => {},
+          onCloseAction: () => declineByOldId(oldId),
+          component: () => null,
+          metadata: {
+            oldId,
+            format: lite?.format,
+          },
+        }
+        return n
+      }),
+    [declineByOldId]
+  )
 
   useEffect(() => {
     setItems(build(credentialRegistry.getState()))
     const unsub = credentialRegistry.subscribe((s) => setItems(build(s)))
     return unsub
-  }, [])
+  }, [build])
 
   return items
 }
