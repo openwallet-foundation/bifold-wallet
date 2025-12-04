86181d8d6ab94729d9934f4039c980fb504d1611
Mac Deluca <Mac.Deluca@quartech.com>
2025-12-04 12:23:46 -0800

    fix: using basicappcontext
    Signed-off-by: Mac Deluca <Mac.Deluca@quartech.com>

--- packages/core/__tests__/components/HeaderRightHome.test.tsx
+++ packages/core/__tests__/components/HeaderRightHome.test.tsx
@@ -1,25 +1,15 @@
 import { render } from '@testing-library/react-native'
 import React from 'react'
 import HeaderRightHome from '../../src/components/buttons/HeaderHome'
-import * as container from '../../src/container-api'
-
-jest.mock('../../src/container-api', () => ({
-  useServices: jest.fn(),
-  TOKENS: {
-    UTIL_LOGGER: 'UTIL_LOGGER',
-  },
-}))
+import { BasicAppContext } from '../helpers/app'
 
 describe('HeaderRightHome Component', () => {
-  beforeEach(() => {
-    jest.resetAllMocks()
-  })
-
   test('Renders correctly', () => {
-    const containerMock = jest.mocked(container)
-    containerMock.useServices.mockReturnValue([{ warn: jest.fn() }] as any)
-
-    const tree = render(<HeaderRightHome />)
+    const tree = render(
+      <BasicAppContext>
+        <HeaderRightHome />
+      </BasicAppContext>
+    )
 
     expect(tree).toMatchSnapshot()
   })
--- packages/core/__tests__/components/IconButton.test.tsx
+++ packages/core/__tests__/components/IconButton.test.tsx
@@ -3,85 +3,70 @@ import React from 'react'
 
 import IconButton, { ButtonLocation } from '../../src/components/buttons/IconButton'
 import { testIdWithKey } from '../../src/utils/testable'
-import * as container from '../../src/container-api'
-
-jest.mock('../../src/container-api', () => ({
-  useServices: jest.fn(),
-  TOKENS: {
-    UTIL_LOGGER: 'UTIL_LOGGER',
-  },
-}))
+import { BasicAppContext } from '../helpers/app'
 
 describe('IconButton Component', () => {
-  beforeEach(() => {
-    jest.resetAllMocks()
-  })
-
   test('Left alignment renders correctly', () => {
-    const containerMock = jest.mocked(container)
-    containerMock.useServices.mockReturnValue([{ warn: jest.fn() }] as any)
-
     const tree = render(
-      <IconButton
-        buttonLocation={ButtonLocation.Left}
-        accessibilityLabel={'LeftButton'}
-        testID={testIdWithKey('LeftButton')}
-        onPress={jest.fn()}
-        icon="information"
-      />
+      <BasicAppContext>
+        <IconButton
+          buttonLocation={ButtonLocation.Left}
+          accessibilityLabel={'LeftButton'}
+          testID={testIdWithKey('LeftButton')}
+          onPress={jest.fn()}
+          icon="information"
+        />
+      </BasicAppContext>
     )
 
     expect(tree).toMatchSnapshot()
   })
 
   test('Right alignment renders correctly', () => {
-    const containerMock = jest.mocked(container)
-    containerMock.useServices.mockReturnValue([{ warn: jest.fn() }] as any)
-
     const tree = render(
-      <IconButton
-        buttonLocation={ButtonLocation.Right}
-        accessibilityLabel={'RightButton'}
-        testID={testIdWithKey('RightButton')}
-        onPress={jest.fn()}
-        icon="information"
-      />
+      <BasicAppContext>
+        <IconButton
+          buttonLocation={ButtonLocation.Right}
+          accessibilityLabel={'RightButton'}
+          testID={testIdWithKey('RightButton')}
+          onPress={jest.fn()}
+          icon="information"
+        />
+      </BasicAppContext>
     )
 
     expect(tree).toMatchSnapshot()
   })
 
   test('Right alignment with text renders correctly', () => {
-    const containerMock = jest.mocked(container)
-    containerMock.useServices.mockReturnValue([{ warn: jest.fn() }] as any)
-
     const tree = render(
-      <IconButton
-        buttonLocation={ButtonLocation.Right}
-        accessibilityLabel={'RightButton'}
-        testID={testIdWithKey('RightButton')}
-        onPress={jest.fn()}
-        text="RightButton"
-        icon="information"
-      />
+      <BasicAppContext>
+        <IconButton
+          buttonLocation={ButtonLocation.Right}
+          accessibilityLabel={'RightButton'}
+          testID={testIdWithKey('RightButton')}
+          onPress={jest.fn()}
+          text="RightButton"
+          icon="information"
+        />
+      </BasicAppContext>
     )
 
     expect(tree).toMatchSnapshot()
   })
 
   test('Button onPress triggers on press', () => {
-    const containerMock = jest.mocked(container)
-    containerMock.useServices.mockReturnValue([{ warn: jest.fn() }] as any)
-
     const callback = jest.fn()
     const { getByTestId } = render(
-      <IconButton
-        buttonLocation={ButtonLocation.Left}
-        accessibilityLabel={'LeftButton'}
-        testID={testIdWithKey('LeftButton')}
-        onPress={callback}
-        icon="information"
-      />
+      <BasicAppContext>
+        <IconButton
+          buttonLocation={ButtonLocation.Left}
+          accessibilityLabel={'LeftButton'}
+          testID={testIdWithKey('LeftButton')}
+          onPress={callback}
+          icon="information"
+        />
+      </BasicAppContext>
     )
 
     const button = getByTestId(testIdWithKey('LeftButton'))
