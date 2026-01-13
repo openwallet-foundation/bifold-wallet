7fe30c149009d3c89e50e80d5fbaf8d71f520579
Bryce McMath <32586431+bryce-mcmath@users.noreply.github.com>
2026-01-13 15:45:49 -0800

    fix: styling (#1752)
    Signed-off-by: Bryce McMath <bryce.j.mcmath@gmail.com>

---
+++ .changeset/thin-tigers-fall.md
@@ -0,0 +1,5 @@
+---
+'@bifold/core': patch
+---
+
+styling fixes, better ScreenWrapper usage
--- packages/core/src/screens/TogglePushNotifications.tsx
+++ packages/core/src/screens/TogglePushNotifications.tsx
@@ -1,8 +1,7 @@
 import { useAgent } from '@credo-ts/react-hooks'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { AppState, Linking, ScrollView, StyleSheet, Switch, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { AppState, Linking, StyleSheet, Switch, View } from 'react-native'
 
 import Button, { ButtonType } from '../components/buttons/Button'
 import { ThemedText } from '../components/texts/ThemedText'
@@ -13,6 +12,7 @@ import { DispatchAction } from '../contexts/reducers/store'
 import { useStore } from '../contexts/store'
 import { useTheme } from '../contexts/theme'
 import { testIdWithKey } from '../utils/testable'
+import ScreenWrapper from '../components/views/ScreenWrapper'
 
 // Screen accesible through settings that allows the user to
 // enable or disable push notifications
@@ -20,7 +20,7 @@ const TogglePushNotifications: React.FC = () => {
   const { t } = useTranslation()
   const [store, dispatch] = useStore()
   const { agent } = useAgent()
-  const { ColorPalette } = useTheme()
+  const { ColorPalette, Spacing } = useTheme()
   const [{ enablePushNotifications }] = useServices([TOKENS.CONFIG])
   const [notificationState, setNotificationState] = useState<boolean>(store.preferences.usePushNotifications)
   const [notificationStatus, setNotificationStatus] = useState<'denied' | 'granted' | 'unknown'>('unknown')
@@ -30,18 +30,12 @@ const TogglePushNotifications: React.FC = () => {
   }
 
   const styles = StyleSheet.create({
-    screenContainer: {
-      flex: 1,
-      padding: 30,
-    },
     toggleContainer: {
-      marginTop: 25,
       display: 'flex',
       flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    controlsContainer: {
-      marginTop: 'auto',
+      gap: Spacing.md,
+      alignItems: 'center',
+      justifyContent: 'center',
     },
   })
 
@@ -74,44 +68,34 @@ const TogglePushNotifications: React.FC = () => {
     setNotificationState(!notificationState)
   }
 
+  const controls = hasNotificationsDisabled ? (
+    <Button
+      buttonType={ButtonType.Primary}
+      title={t('PushNotifications.OpenSettings')}
+      accessibilityLabel={t('PushNotifications.OpenSettings')}
+      testID={testIdWithKey('PushNotificationSettings')}
+      onPress={() => Linking.openSettings()}
+    />
+  ) : (
+    <View style={styles.toggleContainer}>
+      <ThemedText>{t('PushNotifications.ReceiveNotifications')}</ThemedText>
+      <Switch
+        trackColor={{ false: ColorPalette.grayscale.lightGrey, true: ColorPalette.brand.primaryDisabled }}
+        thumbColor={notificationState ? ColorPalette.brand.primary : ColorPalette.grayscale.mediumGrey}
+        ios_backgroundColor={ColorPalette.grayscale.lightGrey}
+        onValueChange={toggleSwitch}
+        accessibilityLabel={t('PushNotifications.ReceiveNotifications')}
+        accessibilityRole="switch"
+        testID={testIdWithKey('PushNotificationSwitch')}
+        value={notificationState}
+      />
+    </View>
+  )
+
   return (
-    <SafeAreaView style={{ flex: 1 }} edges={['left', 'right', 'bottom']}>
-      <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-        <View style={styles.screenContainer}>
-          {hasNotificationsDisabled ? (
-            <>
-              <PushNotificationsDisabledContent />
-              <View style={styles.controlsContainer}>
-                <Button
-                  buttonType={ButtonType.Primary}
-                  title={t('PushNotifications.OpenSettings')}
-                  accessibilityLabel={t('PushNotifications.OpenSettings')}
-                  testID={testIdWithKey('PushNotificationSettings')}
-                  onPress={() => Linking.openSettings()}
-                />
-              </View>
-            </>
-          ) : (
-            <>
-              <PushNotificationsContent />
-              <View style={styles.toggleContainer}>
-                <ThemedText>{t('PushNotifications.ReceiveNotifications')}</ThemedText>
-                <Switch
-                  trackColor={{ false: ColorPalette.grayscale.lightGrey, true: ColorPalette.brand.primaryDisabled }}
-                  thumbColor={notificationState ? ColorPalette.brand.primary : ColorPalette.grayscale.mediumGrey}
-                  ios_backgroundColor={ColorPalette.grayscale.lightGrey}
-                  onValueChange={toggleSwitch}
-                  accessibilityLabel={t('PushNotifications.ReceiveNotifications')}
-                  accessibilityRole="switch"
-                  testID={testIdWithKey('PushNotificationSwitch')}
-                  value={notificationState}
-                />
-              </View>
-            </>
-          )}
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ScreenWrapper controls={controls} padded>
+      {hasNotificationsDisabled ? <PushNotificationsDisabledContent /> : <PushNotificationsContent />}
+    </ScreenWrapper>
   )
 }
 
