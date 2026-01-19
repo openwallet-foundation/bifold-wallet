d327c999f3abdee92f3760cf43e0957f997751fa
Graham Stewart <graham.stewart@quartech.com>
2026-01-19 14:44:15 -0800

    Fixed some peer dependencies for didcomm classes to be loaded correctly and some bad references to didcomm when handling oob invitations
    Signed-off-by: Graham Stewart <graham.stewart@quartech.com>

--- packages/core/src/utils/agent.ts
+++ packages/core/src/utils/agent.ts
@@ -114,10 +114,10 @@ export function getAgentModules({ indyNetworks, mediatorInvitationUrl, txnCache 
           }),
         ],
       },
-      // mediationRecipient: {
-      //   mediatorInvitationUrl: mediatorInvitationUrl,
-      //   mediatorPickupStrategy: DidCommMediatorPickupStrategy.PickUpV2,
-      // },
+      mediationRecipient: {
+        mediatorInvitationUrl: mediatorInvitationUrl,
+        mediatorPickupStrategy: DidCommMediatorPickupStrategy.PickUpV2,
+      },
     }),
     openid4vc: new OpenId4VcModule(),
     // oob: new DidCommOutOfBandModule(),
