33d41a28f31b19466ab02da6f08ca04b49a2006f
Graham Stewart <graham.stewart@quartech.com>
2026-01-20 13:59:02 -0800

    Fixed some instances of mediatorRecipient not being called correctly

--- packages/core/src/utils/agent.ts
+++ packages/core/src/utils/agent.ts
@@ -111,7 +111,7 @@ export function getAgentModules({ indyNetworks, mediatorInvitationUrl, txnCache 
       },
       mediationRecipient: {
         mediatorInvitationUrl: mediatorInvitationUrl,
-        mediatorPickupStrategy: DidCommMediatorPickupStrategy.PickUpV2,
+        mediatorPickupStrategy: DidCommMediatorPickupStrategy.Implicit,
       },
     }),
     openid4vc: new OpenId4VcModule(),
