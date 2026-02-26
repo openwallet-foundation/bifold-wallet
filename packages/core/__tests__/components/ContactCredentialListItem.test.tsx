5843a1b184189e91c280ace4ca0eacfc1d6bf160
Mostafa Gamal <moscd3@gmail.com>
2026-02-26 15:16:23 -0500

    Fix Contact credential list item test
    Signed-off-by: Mostafa Gamal <moscd3@gmail.com>

--- packages/core/__tests__/components/ContactCredentialListItem.test.tsx
+++ packages/core/__tests__/components/ContactCredentialListItem.test.tsx
@@ -1,3 +1,31 @@
+import { render, waitFor } from '@testing-library/react-native'
+import fs from 'fs'
+import path from 'path'
+import React from 'react'
+
+import ContactCredentialListItem from '../../src/components/listItems/ContactCredentialListItem'
+import { BasicAppContext } from '../helpers/app'
+import { DidCommCredentialExchangeRecord } from '@credo-ts/didcomm'
+
+const credentialPath = path.join(__dirname, '../fixtures/degree-credential.json')
+const credential = JSON.parse(fs.readFileSync(credentialPath, 'utf8'))
+
 describe('ContactCredentialListItem Component', () => {
-  test.todo('Restore component tests for credential list item')
+  test('Renders correctly', async () => {
+    const credentialRecord = new DidCommCredentialExchangeRecord(credential)
+    credentialRecord.credentials.push({
+      credentialRecordType: 'anoncreds',
+      credentialRecordId: '',
+    })
+    credentialRecord.createdAt = new Date(credentialRecord.createdAt)
+    const tree = render(
+      <BasicAppContext>
+        <ContactCredentialListItem credential={credentialRecord} onPress={() => {}} />
+      </BasicAppContext>
+    )
+
+    await waitFor(async () => {
+      expect(tree).toMatchSnapshot()
+    })
+  })
 })
